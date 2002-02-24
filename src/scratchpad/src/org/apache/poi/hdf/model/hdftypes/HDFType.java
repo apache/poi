04f68b855476c26eea5e67db6f3e8ba805bd23f5
Andrew C. Oliver <acoliver@apache.org>
2002-02-24 20:29:45 +0000

    start of new Word file support refactorization
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/poi/trunk@352119 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/scratchpad/src/org/apache/poi/hdf/model/hdftypes/HDFType.java
@@ -0,0 +1,16 @@
+/*
+ * HDFType.java
+ *
+ * Created on February 24, 2002, 2:37 PM
+ */
+
+package org.apache.poi.hdf.model.hdftypes;
+
+/**
+ *
+ * @author  andy
+ */
+public interface HDFType {
+
+}
+
