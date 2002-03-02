75f016af73f5521b83192b32991a6661ce9a2163
Andrew C. Oliver <acoliver@apache.org>
2002-03-02 20:57:54 +0000

    more fields.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/poi/trunk@352129 13f79535-47bb-0310-9956-ffa450edef68

--- src/scratchpad/src/org/apache/poi/hdf/model/hdftypes/FileInformationBlock.java
+++ src/scratchpad/src/org/apache/poi/hdf/model/hdftypes/FileInformationBlock.java
@@ -36,11 +36,79 @@ public class FileInformationBlock implements HDFType {
         private static final BitField crypto            = new BitField(0x8000);
    
     private short field_7_minversion;
-    //private short field_8_
+    private short field_8_encrypted_key;
+    private short field_9_environment; // 0 or 1 - windows or mac
+    private short field_10_history;
         
+        private static final BitField history_mac   = new BitField(0x01);
+        private static final BitField empty_special = new BitField(0x02);
+        private static final BitField load_override = new BitField(0x04);
+        private static final BitField future_undo   = new BitField(0x08);
+        private static final BitField w97_saved     = new BitField(0x10);
+        private static final BitField spare         = new BitField(0xfe); 
+        
+    private short field_11_default_charset;
+    private short field_12_default_extcharset;
+    private int  field_13_offset_first_char;
+    private int  field_14_offset_last_char;
+    private short field_15_count_shorts;
+    
+    private short field_16_beg_shorts; //why same offset?
+    
+    private short field_16_creator_id;
+    private short field_17_revisor_id;
+    private short field_18_creator_private;
+    private short field_19_revisor_private;
+    
+    private short field_20_unused;
+    private short field_21_unused;    
+    private short field_22_unused;
+    private short field_23_unused;
+    private short field_24_unused;
+    private short field_25_unused;
+    private short field_26_unused;
+    private short field_27_unused;
+    private short field_28_unused;
+    
+    private short field_29_fareastid; 
+    private short field_30_count_ints;
+    
+    private int field_31_beg_ints; //why same offset?
+    
+    private int field_31_last_byte;
+    
+    private int field_32_creator_build_date;
+    private int field_33_revisor_build_date;
+    private int field_34_main_streamlen;
+    private int field_35_footnote_streamlen;
+    private int field_36_header_streamlen;
+    private int field_37_macro_streamlen;
+    private int field_38_annotation_streamlen;
+    private int field_39_endnote_streamlen;
+    private int field_40_textbox_streamlen;
+    private int field_41_headbox_streamlen;
+    private int field_42_pointer_to_plc_list_chp; //rename me!
+    private int field_43_first_chp; //rename me
+    private int field_44_count_chps; //rename me
+    private int field_45_pointer_to_plc_list_pap; //rename me.
+    private int field_46_first_pap; //rename me
+    private int field_47_count_paps; //rename me
+    private int field_48_pointer_to_plc_list_lvc; //rename me
+    private int field_49_first_lvc; //rename me
+    private int field_50_count_lvc; //rename me
+    
+    private int field_51_unknown;
+    private int field_52_unknown;
+    
+    
+    
+    
+    
+    
         
     /** Creates a new instance of FileInformationBlock */
     public FileInformationBlock() {
     }
 
 }
+
