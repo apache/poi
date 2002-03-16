612d2d4c6417b34e07e23ef736b2bfbadea9e33b
Nicola Ken Barozzi <nicolaken@apache.org>
2002-03-16 17:08:00 +0000

    Update of the build system to make the script cleaner and make ./lib/endorsed libs automatically used in both script and build classpaths.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/poi/trunk@352231 13f79535-47bb-0310-9956-ffa450edef68

--- build.bat
+++ build.bat
@@ -4,10 +4,10 @@ set OLD_ANT_HOME=%ANT_HOME%
 set ANT_HOME=tools\ant
 
 set OLD_CLASSPATH=%CLASSPATH%
-set CLASSPATH=tools\centipede\lib\xml-apis.jar;tools\centipede\lib\xerces.jar;tools\centipede\lib\xalan.jar;tools\centipede\lib\junit.jar;tools\centipede\lib\jIzPress.jar;tools\centipede\lib\jtidy.jar
+for %%i in (.\lib\endorsed\*.jar) do call appendcp.bat %%i
+for %%i in (.\tools\centipede\lib\*.jar) do call appendcp.bat %%i
 
 call %ANT_HOME%\bin\ant -listener org.apache.tools.ant.XmlLogger %1 %2 %3 %4 %5 %6 %7 %8 %9
 
 set ANT_HOME=%OLD_ANT_HOME%
 set CLASSPATH=%OLD_CLASSPATH%
-
