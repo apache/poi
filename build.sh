91f2aab60a2abc64173db9d79f9f74e874f6f739
Nicola Ken Barozzi <nicolaken@apache.org>
2002-03-16 17:08:00 +0000

    Update of the build system to make the script cleaner and make ./lib/endorsed libs automatically used in both script and build classpaths.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/poi/trunk@352231 13f79535-47bb-0310-9956-ffa450edef68

--- build.sh
+++ build.sh
@@ -1,8 +1,23 @@
 #!/bin/sh
+# ----- Verify and Set Required Environment Variables -------------------------
+
+if [ "$JAVA_HOME" = "" ] ; then
+  echo You must set JAVA_HOME to point at your Java Development Kit installation
+  exit 1
+fi
 
 chmod u+x ./tools/ant/bin/antRun
 chmod u+x ./tools/ant/bin/ant
 
+# ----- Verify and Set Required Environment Variables -------------------------
+
+if [ "$TERM" = "cygwin" ] ; then
+  S=';'
+else
+  S=':'
+fi
+
+# ----- Set Up The Runtime Classpath ------------------------------------------
 
 OLD_ANT_HOME=$ANT_HOME
 unset ANT_HOME
@@ -11,7 +26,7 @@ CP=$CLASSPATH
 export CP
 unset CLASSPATH
 
-CLASSPATH="./tools/centipede/lib/xml-apis.jar:./tools/centipede/lib/xerces.jar:./tools/centipede/lib/xalan.jar:./tools/centipede/lib/junit.jar:./tools/centipede/lib/jIzPress.jar:./tools/centipede/lib/jtidy.jar"
+CLASSPATH="`echo ./lib/endorsed/*.jar | tr ' ' $S``echo ./tools/centipede/lib/*.jar | tr ' ' $S`"
 export CLASSPATH
 
 $PWD/./tools/ant/bin/ant -listener org.apache.tools.ant.XmlLogger $@ 
