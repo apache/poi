45c5a3899ed38a49969476f3afe4b090f00aeb69
Andrew C. Oliver <acoliver@apache.org>
2002-04-07 17:29:33 +0000

    made it official (deprecation)
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/poi/trunk@352349 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/poi/hssf/usermodel/HSSFColorConstants.java
+++ src/java/org/apache/poi/hssf/usermodel/HSSFColorConstants.java
@@ -66,6 +66,8 @@ package org.apache.poi.hssf.usermodel;
  * NOTE WILL MOST LIKELY BE DEPRECATED BY THE END OF THE 2.0 CYCLE IN FAVOR
  * OF nsph.util.HSSFColor...
  * @author  Andrew C. Oliver (acoliver at apache dot org)
+ * @deprecated use org.apache.poi.hssf.util.HSSFColor instead
+ * @see org.apache.poi.hssf.util.HSSFColor
  */
 
 public interface HSSFColorConstants
