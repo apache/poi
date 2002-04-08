79b292b2744158146cce95e5495c81a4125329e0
Nicola Ken Barozzi <nicolaken@apache.org>
2002-04-08 07:21:16 +0000

    Update of the build system. Gump target works ok. All docs generate ok. Jar task is ok.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/poi/trunk@352353 13f79535-47bb-0310-9956-ffa450edef68

--- build.bat
+++ build.bat
@@ -6,7 +6,7 @@ set ANT_HOME=tools\antipede
 set OLD_CLASSPATH=%CLASSPATH%
 for %%i in (.\lib\endorsed\*.jar) do call appendcp.bat %%i
 
-call %ANT_HOME%\bin\ant -listener org.apache.tools.ant.XmlLogger %1 %2 %3 %4 %5 %6 %7 %8 %9
+call %ANT_HOME%\bin\ant -emacs -listener org.apache.tools.ant.XmlLogger %1 %2 %3 %4 %5 %6 %7 %8 %9
 
 set ANT_HOME=%OLD_ANT_HOME%
 set CLASSPATH=%OLD_CLASSPATH%
