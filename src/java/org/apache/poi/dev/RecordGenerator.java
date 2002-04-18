986dd8a3e48b852b56cbc57b6a6fa222a07459b1
Glen Stampoultzis <glens@apache.org>
2002-04-18 12:54:29 +0000

    New record plus added a check in the record generator.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/poi/trunk@352445 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/poi/dev/RecordGenerator.java
+++ src/java/org/apache/poi/dev/RecordGenerator.java
@@ -56,8 +56,6 @@ package org.apache.poi.dev;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
-import org.w3c.dom.Node;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -66,14 +64,16 @@ import java.io.File;
 public class RecordGenerator
 {
     public static void main(String[] args)
-        throws Exception
+            throws Exception
     {
+        // Force load so that we don't start generating records and realise this hasn't compiled yet.
+        Class.forName("org.apache.poi.generator.FieldIterator");
+
         if (args.length != 4)
         {
             System.out.println("Usage:");
             System.out.println("  java org.apache.poi.hssf.util.RecordGenerator RECORD_DEFINTIONS RECORD_STYLES DEST_SRC_PATH TEST_SRC_PATH");
-        }
-        else
+        } else
         {
             generateRecords(args[0], args[1], args[2], args[3]);
         }
@@ -87,11 +87,11 @@ public class RecordGenerator
         for (int i = 0; i < definitionsFile.listFiles().length; i++)
         {
             File file = definitionsFile.listFiles()[i];
-            if (file.isFile() && 
-                (file.getName().endsWith("_record.xml") ||
-                 file.getName().endsWith("_type.xml")
-                )
-               )
+            if (file.isFile() &&
+                    (file.getName().endsWith("_record.xml") ||
+                    file.getName().endsWith("_type.xml")
+                    )
+            )
             {
                 // Get record name and package
                 DocumentBuilderFactory factory =
@@ -103,36 +103,35 @@ public class RecordGenerator
                 String suffix = record.getElementsByTagName("suffix").item(0).getFirstChild().getNodeValue();
                 String recordName = record.getAttributes().getNamedItem("name").getNodeValue();
                 String packageName = record.getAttributes().getNamedItem("package").getNodeValue();
-                packageName = packageName.replace('.','/');
+                packageName = packageName.replace('.', '/');
 
                 // Generate record
-                String destinationPath = destSrcPathDir + "/" + packageName ;
+                String destinationPath = destSrcPathDir + "/" + packageName;
                 File destinationPathFile = new File(destinationPath);
                 destinationPathFile.mkdirs();
-                String destinationFilepath = destinationPath + "/" + recordName + suffix+".java";
-                String args[] = new String [] { "-in", file.getAbsolutePath(), "-xsl", recordStyleDir + "/" + extendstg.toLowerCase() + ".xsl",
-                                                "-out", destinationFilepath,
-                                                "-TEXT"};
+                String destinationFilepath = destinationPath + "/" + recordName + suffix + ".java";
+                String args[] = new String[]{"-in", file.getAbsolutePath(), "-xsl", recordStyleDir + "/" + extendstg.toLowerCase() + ".xsl",
+                                             "-out", destinationFilepath,
+                                             "-TEXT"};
 
-                org.apache.xalan.xslt.Process.main( args );
-                System.out.println("Generated "+suffix+": " + destinationFilepath);
+                org.apache.xalan.xslt.Process.main(args);
+                System.out.println("Generated " + suffix + ": " + destinationFilepath);
 
                 // Generate test (if not already generated)
-                destinationPath = testSrcPathDir + "/" + packageName ;
+                destinationPath = testSrcPathDir + "/" + packageName;
                 destinationPathFile = new File(destinationPath);
                 destinationPathFile.mkdirs();
-                destinationFilepath = destinationPath + "/Test" + recordName + suffix+".java";
+                destinationFilepath = destinationPath + "/Test" + recordName + suffix + ".java";
                 if (new File(destinationFilepath).exists() == false)
                 {
-                String temp = (recordStyleDir + "/" + extendstg.toLowerCase() + "_test.xsl");
-                    args = new String [] { "-in", file.getAbsolutePath(), "-xsl",
-					    temp,
-                                                    "-out", destinationFilepath,
-                                                    "-TEXT"};
-                    org.apache.xalan.xslt.Process.main( args );
+                    String temp = (recordStyleDir + "/" + extendstg.toLowerCase() + "_test.xsl");
+                    args = new String[]{"-in", file.getAbsolutePath(), "-xsl",
+                                        temp,
+                                        "-out", destinationFilepath,
+                                        "-TEXT"};
+                    org.apache.xalan.xslt.Process.main(args);
                     System.out.println("Generated test: " + destinationFilepath);
-                }
-                else
+                } else
                 {
                     System.out.println("Skipped test generation: " + destinationFilepath);
                 }
