97633bf7205eee031c66b97069a1383f441efd26
Nicola Ken Barozzi <nicolaken@apache.org>
2002-04-25 22:52:14 +0000

    Made the build output less cluttered with skipped tasks that were shown.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/poi/trunk@352483 13f79535-47bb-0310-9956-ffa450edef68

--- build.sh
+++ build.sh
@@ -30,7 +30,7 @@ CLASSPATH="`echo ./lib/endorsed/*.jar | tr ' ' $S`"
 export CLASSPATH
 
 echo Using classpath: \"$CLASSPATH\"
-$PWD/./tools/antipede/bin/ant -emacs $@ 
+$PWD/./tools/antipede/bin/ant -emacs -logger org.apache.tools.ant.NoBannerLogger $@ 
 
 unset CLASSPATH
 
