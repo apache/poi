ca589e267bd6192a5a1d31f25503826420dd8e71
Nicola Ken Barozzi <nicolaken@apache.org>
2002-04-25 22:52:14 +0000

    Made the build output less cluttered with skipped tasks that were shown.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/poi/trunk@352483 13f79535-47bb-0310-9956-ffa450edef68

--- build.bat
+++ build.bat
@@ -7,7 +7,7 @@ set OLD_CLASSPATH=%CLASSPATH%
 for %%i in (.\lib\endorsed\*.jar) do call appendcp.bat %%i
 
 echo Using classpath: "%CLASSPATH%"
-call %ANT_HOME%\bin\ant -emacs %1 %2 %3 %4 %5 %6 %7 %8 %9
+call %ANT_HOME%\bin\ant -emacs -logger org.apache.tools.ant.NoBannerLogger %1 %2 %3 %4 %5 %6 %7 %8 %9
 
 set ANT_HOME=%OLD_ANT_HOME%
 set CLASSPATH=%OLD_CLASSPATH%
