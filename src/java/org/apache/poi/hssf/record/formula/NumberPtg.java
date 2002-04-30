84d8c0a13396195778128f494844bc652673b14a
Andrew C. Oliver <acoliver@apache.org>
2002-04-30 00:18:29 +0000

    avik's patches and stuff.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/poi/trunk@352560 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/poi/hssf/record/formula/NumberPtg.java
@@ -0,0 +1,118 @@
+/* ====================================================================
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2002 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "Apache" and "Apache Software Foundation" and
+ *    "Apache POI" must not be used to endorse or promote products
+ *    derived from this software without prior written permission. For
+ *    written permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    "Apache POI", nor may "Apache" appear in their name, without
+ *    prior written permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ */
+
+package org.apache.poi.hssf.record.formula;
+
+import org.apache.poi.util.LittleEndian;
+
+/**
+ * Integer (short intger)
+ * Stores a (java) short value in a formula
+ * @author  Avik Sengupta
+ */
+
+public class NumberPtg
+    extends Ptg
+{
+    public final static int  SIZE = 9;
+    public final static byte sid  = 0x1f;
+    private double            field_1_value;
+
+    /** Creates new NumberPtg */
+
+    public NumberPtg()
+    {
+    }
+
+    public NumberPtg(byte [] data, int offset)
+    {
+        setValue(LittleEndian.getDouble(data, offset + 1));
+    }
+    
+    protected NumberPtg(String value) {
+        setValue(Double.parseDouble(value));
+    }
+    public void setValue(double value)
+    {
+        field_1_value = value;
+    }
+
+    public double getValue()
+    {
+        return field_1_value;
+    }
+
+    public void writeBytes(byte [] array, int offset)
+    {
+        array[ offset + 0 ] = sid;
+        LittleEndian.putDouble(array, offset + 1, getValue());
+    }
+
+    public int getSize()
+    {
+        return SIZE;
+    }
+
+    public String toFormulaString()
+    {
+        return "" + getValue();
+    }
+    
+    
+    //TODO: do we really need this method??    
+    public int getStringLength() {
+        return 1;
+    }    
+}
+
