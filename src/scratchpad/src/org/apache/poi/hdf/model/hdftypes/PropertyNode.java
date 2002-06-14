df966f48bb155078b9a6bd88e934e98f9b1402cb
Said Ryan Ackley <sackley@apache.org>
2002-06-14 02:03:20 +0000

    Added comments
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/poi/trunk@352696 13f79535-47bb-0310-9956-ffa450edef68

--- src/scratchpad/src/org/apache/poi/hdf/model/hdftypes/PropertyNode.java
+++ src/scratchpad/src/org/apache/poi/hdf/model/hdftypes/PropertyNode.java
@@ -54,39 +54,61 @@
 package org.apache.poi.hdf.model.hdftypes;
 
 
-
+/**
+ * Represents a lightweight node in the Trees used to store formatting
+ * properties.
+ *
+ * @author Ryan Ackley
+ */
 public class PropertyNode implements Comparable
 {
   private byte[] _grpprl;
   private int _fcStart;
   private int _fcEnd;
 
+  /**
+   * @param fcStart The start of the text for this property.
+   * @param fcEnd The end of the text for this property.
+   * @param grpprl The property description in compressed form.
+   */
   public PropertyNode(int fcStart, int fcEnd, byte[] grpprl)
   {
       _fcStart = fcStart;
       _fcEnd = fcEnd;
       _grpprl = grpprl;
   }
+  /**
+   * @return The offset of this property's text.
+   */
   public int getStart()
   {
       return _fcStart;
   }
+  /**
+   * @retrun The offset of the end of this property's text.
+   */
   public int getEnd()
   {
     return _fcEnd;
   }
+  /**
+   * @return This property's property in copmpressed form.
+   */
   protected byte[] getGrpprl()
   {
     return _grpprl;
   }
+  /**
+   * Used for sorting in collections.
+   */
   public int compareTo(Object o)
   {
-      int fcStart = ((PropertyNode)o).getStart();
-      if(_fcStart == fcStart)
+      int fcEnd = ((PropertyNode)o).getEnd();
+      if(_fcEnd == fcEnd)
       {
         return 0;
       }
-      else if(_fcStart < fcStart)
+      else if(_fcEnd < fcEnd)
       {
         return -1;
       }
