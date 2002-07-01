0403a7cdfd24a746a28198db21eb2eb7d93d8cf6
Said Ryan Ackley <sackley@apache.org>
2002-07-01 20:30:59 +0000

    initial check in , changing around the object model
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/poi/trunk@352739 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/scratchpad/src/org/apache/poi/hdf/model/HDFObjectModel.java
@@ -0,0 +1,73 @@
+package org.apache.poi.hdf.model;
+
+import org.apache.poi.hdf.event.HDFLowLevelParsingListener;
+import org.apache.poi.hdf.model.util.BTreeSet;
+
+import org.apache.poi.hdf.model.hdftypes.ChpxNode;
+import org.apache.poi.hdf.model.hdftypes.PapxNode;
+import org.apache.poi.hdf.model.hdftypes.SepxNode;
+import org.apache.poi.hdf.model.hdftypes.TextPiece;
+import org.apache.poi.hdf.model.hdftypes.DocumentProperties;
+import org.apache.poi.hdf.model.hdftypes.FontTable;
+import org.apache.poi.hdf.model.hdftypes.ListTables;
+import org.apache.poi.hdf.model.hdftypes.StyleSheet;
+
+
+public class HDFObjectModel implements HDFLowLevelParsingListener
+{
+
+    /** The DOP*/
+    private DocumentProperties _dop;
+    /**the StyleSheet*/
+    private StyleSheet _styleSheet;
+    /**list info */
+    private ListTables _listTables;
+    /** Font info */
+    private FontTable _fonts;
+
+    /** text pieces */
+    BTreeSet _text = new BTreeSet();
+    /** document sections */
+    BTreeSet _sections = new BTreeSet();
+    /** document paragraphs */
+    BTreeSet _paragraphs = new BTreeSet();
+    /** document character runs */
+    BTreeSet _characterRuns = new BTreeSet();
+
+    public HDFObjectModel()
+    {
+    }
+
+    public void document(DocumentProperties dop)
+    {
+      _dop = dop;
+    }
+    public void section(SepxNode sepx)
+    {
+      _sections.add(sepx);
+    }
+    public void paragraph(PapxNode papx)
+    {
+      _paragraphs.add(papx);
+    }
+    public void characterRun(ChpxNode chpx)
+    {
+      _characterRuns.add(chpx);
+    }
+    public void text(TextPiece t)
+    {
+      _text.add(t);
+    }
+    public void fonts(FontTable fontTbl)
+    {
+      _fonts = fontTbl;
+    }
+    public void lists(ListTables listTbl)
+    {
+      _listTables = listTbl;
+    }
+    public void styleSheet(StyleSheet stsh)
+    {
+      _styleSheet = stsh;
+    }
+}
