be7ce20c13f4564a918718927ddb169b002be28e
Said Ryan Ackley <sackley@apache.org>
2002-07-03 19:29:35 +0000

    added some functions
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/poi/trunk@352747 13f79535-47bb-0310-9956-ffa450edef68

--- src/scratchpad/src/org/apache/poi/hdf/event/HDFLowLevelParsingListener.java
+++ src/scratchpad/src/org/apache/poi/hdf/event/HDFLowLevelParsingListener.java
@@ -11,12 +11,17 @@ import org.apache.poi.hdf.model.hdftypes.StyleSheet;
 
 public interface HDFLowLevelParsingListener
 {
+  public void mainDocument(byte[] mainDocument);
+  public void tableStream(byte[] tableStream);
   public void document(DocumentProperties dop);
-  public void section(SepxNode sepx);
+  public void bodySection(SepxNode sepx);
   public void paragraph(PapxNode papx);
   public void characterRun(ChpxNode chpx);
+  public void hdrSection(SepxNode sepx);
+  public void endSections();
   public void text(TextPiece t);
   public void fonts(FontTable fontTbl);
   public void lists(ListTables listTbl);
   public void styleSheet(StyleSheet stsh);
+  public void miscellaneous(int fcMin, int ccpText, int ccpFtn, int fcPlcfhdd, int lcbPlcfhdd);
 }
