f59894d3755a8ab5b9fe5e9f90731a8803c950ec
Said Ryan Ackley <sackley@apache.org>
2003-06-09 01:57:43 +0000

    stuff for writing
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/poi/trunk@353126 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/scratchpad/src/org/apache/poi/hwpf/model/io/HWPFFileSystem.java
@@ -0,0 +1,31 @@
+package org.apache.poi.hwpf.model.io;
+
+/**
+ * <p>Title: </p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2003</p>
+ * <p>Company: </p>
+ * @author not attributable
+ * @version 1.0
+ */
+
+
+import java.io.ByteArrayOutputStream;
+import java.util.HashMap;
+
+public class HWPFFileSystem
+{
+  HashMap _streams;
+
+  public HWPFFileSystem()
+  {
+    _streams.put("WordDocument", new HWPFOutputStream());
+    _streams.put("1Table", new HWPFOutputStream());
+  }
+
+  public HWPFOutputStream getStream(String name)
+  {
+    return (HWPFOutputStream)_streams.get(name);
+  }
+
+}
