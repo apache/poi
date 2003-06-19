c9c639ac5367f0c5450ea63e0167029df29eea92
tetsuya <tetsuya@unknown>
2003-06-19 13:14:14 +0000

    Fixed line formatting. (2)
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/poi/trunk@353148 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/poi/hssf/record/LeftMarginRecord.java
+++ src/java/org/apache/poi/hssf/record/LeftMarginRecord.java
@@ -57,6 +57,7 @@ package org.apache.poi.hssf.record;
 
 
 import org.apache.poi.util.*;
+
 /**
  * Record for the left margin.
  * NOTE: This source was automatically generated.
@@ -67,10 +68,12 @@ public class LeftMarginRecord
     extends Record implements Margin{
     public final static short      sid                             = 0x26;
     private  double     field_1_margin;
+
     public LeftMarginRecord()
     {
 
     }
+
     /**
      * Constructs a LeftMargin record and sets its fields appropriately.
      *
@@ -79,10 +82,12 @@ public class LeftMarginRecord
      * @param size  size the size of the data area of the record
      * @param data  data of the record (should not contain sid/len)
      */
+
     public LeftMarginRecord(short id, short size, byte [] data)
     {
         super(id, size, data);
     }
+
     /**
      * Constructs a LeftMargin record and sets its fields appropriately.
      *
@@ -92,15 +97,18 @@ public class LeftMarginRecord
      * @param data  data of the record (should not contain sid/len)
      * @param offset of the record's data
      */
+
     public LeftMarginRecord(short id, short size, byte [] data, int offset)
     {
         super(id, size, data, offset);
     }
+
     /**
      * Checks the sid matches the expected side for this record
      *
      * @param id   the expected sid.
      */
+
     protected void validateSid(short id)
     {
         if (id != sid)
@@ -108,10 +116,12 @@ public class LeftMarginRecord
             throw new RecordFormatException("Not a LeftMargin record");
         }
     }
+
     protected void fillFields(byte [] data, short size, int offset)
     {
         field_1_margin                  = LittleEndian.getDouble(data, 0x0 + offset);
     }
+
     public String toString()
     {
         StringBuffer buffer = new StringBuffer();
@@ -120,6 +130,7 @@ public class LeftMarginRecord
         buffer.append("[/LeftMargin]\n");
         return buffer.toString();
     }
+
     public int serialize(int offset, byte[] data)
     {
         LittleEndian.putShort(data, 0 + offset, sid);
@@ -127,34 +138,43 @@ public class LeftMarginRecord
         LittleEndian.putDouble(data, 4 + offset, field_1_margin);
         return getRecordSize();
     }
+
     /**
      * Size of record (exluding 4 byte header)
      */
+
     public int getRecordSize()
     {
         return 4  + 8;
     }
+
     public short getSid()
     {
         return this.sid;
     }
+
     /**
      * Get the margin field for the LeftMargin record.
      */
+
     public double getMargin()
     {
         return field_1_margin;
     }
+
     /**
      * Set the margin field for the LeftMargin record.
      */
+
     public void setMargin(double field_1_margin)
     {
         this.field_1_margin = field_1_margin;
     }
+
     public Object clone() {
         LeftMarginRecord rec = new LeftMarginRecord();
         rec.field_1_margin = this.field_1_margin;
         return rec;
     }
+
 }
