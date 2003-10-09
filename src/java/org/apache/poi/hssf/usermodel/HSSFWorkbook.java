fd2cc4b7d160cda5e4d85efd8f0f7e1164b77d2c
Avik Sengupta <avik@apache.org>
2003-10-09 19:21:57 +0000

    PR: 13921 Sheet name should not be greater than 31 chars and should not contain \/?*[]
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/poi/branches/REL_2_BRANCH@353385 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/poi/hssf/usermodel/HSSFWorkbook.java
+++ src/java/org/apache/poi/hssf/usermodel/HSSFWorkbook.java
@@ -281,7 +281,9 @@ public class HSSFWorkbook
     
      
     /**
-     * set the sheet name.
+     * set the sheet name. 
+     * Will throw IllegalArgumentException if the name is greater than 31 chars
+     * or contains /\?*[]
      * @param sheet number (0 based)
      * @param sheet name
      */
