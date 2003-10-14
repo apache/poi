4f8a7a6ee90420cb64fd8d21d277d65b1ad41511
Jason Height <jheight@apache.org>
2003-10-14 22:44:55 +0000

    Changed initial palette pos to -1 so we know when it has been set.
    Part of fix for 16559
    
    
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/poi/branches/REL_2_BRANCH@353401 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/poi/hssf/model/WorkbookRecordList.java
+++ src/java/org/apache/poi/hssf/model/WorkbookRecordList.java
@@ -74,7 +74,7 @@ public class WorkbookRecordList
     private int  namepos     = 0;   // holds the position of last name record
     private int  supbookpos  = 0;   // holds the position of sup book
     private int  externsheetPos = 0;// holds the position of the extern sheet
-    private int  palettepos  = 0;   // hold the position of the palette, if applicable
+    private int  palettepos  = -1;   // hold the position of the palette, if applicable
 
 
     public void setRecords( List records )
