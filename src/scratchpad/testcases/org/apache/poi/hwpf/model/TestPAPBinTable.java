65f4bf6e54fb1f622402dd986f4f7412a86430f1
Said Ryan Ackley <sackley@apache.org>
2004-03-02 06:23:44 +0000

    *** empty log message ***
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/poi/trunk@353517 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/scratchpad/testcases/org/apache/poi/hwpf/model/TestPAPBinTable.java
@@ -0,0 +1,134 @@
+/* ====================================================================
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2003 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "Apache" and "Apache Software Foundation" and
+ *    "Apache POI" must not be used to endorse or promote products
+ *    derived from this software without prior written permission. For
+ *    written permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    "Apache POI", nor may "Apache" appear in their name, without
+ *    prior written permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ */
+
+package org.apache.poi.hwpf.model;
+
+import junit.framework.*;
+import org.apache.poi.hwpf.*;
+import org.apache.poi.hwpf.model.io.*;
+
+import java.io.*;
+import java.util.*;
+
+public class TestPAPBinTable
+  extends TestCase
+{
+  private PAPBinTable _pAPBinTable = null;
+  private HWPFDocFixture _hWPFDocFixture;
+
+  public TestPAPBinTable(String name)
+  {
+    super(name);
+  }
+
+  public void testReadWrite()
+    throws Exception
+  {
+    FileInformationBlock fib = _hWPFDocFixture._fib;
+    byte[] mainStream = _hWPFDocFixture._mainStream;
+    byte[] tableStream = _hWPFDocFixture._tableStream;
+    int fcMin = fib.getFcMin();
+
+    _pAPBinTable = new PAPBinTable(mainStream, tableStream, fib.getFcPlcfbtePapx(), fib.getLcbPlcfbtePapx(), fcMin);
+
+    HWPFFileSystem fileSys = new HWPFFileSystem();
+
+    _pAPBinTable.writeTo(fileSys, 0);
+    ByteArrayOutputStream tableOut = fileSys.getStream("1Table");
+    ByteArrayOutputStream mainOut =  fileSys.getStream("WordDocument");
+
+    byte[] newTableStream = tableOut.toByteArray();
+    byte[] newMainStream = mainOut.toByteArray();
+
+    PAPBinTable newBinTable = new PAPBinTable(newMainStream, newTableStream, 0, newTableStream.length, 0);
+
+    ArrayList oldTextRuns = _pAPBinTable.getParagraphs();
+    ArrayList newTextRuns = newBinTable.getParagraphs();
+
+    assertEquals(oldTextRuns.size(), newTextRuns.size());
+
+    int size = oldTextRuns.size();
+    for (int x = 0; x < size; x++)
+    {
+     PropertyNode oldNode = (PropertyNode)oldTextRuns.get(x);
+     PropertyNode newNode = (PropertyNode)newTextRuns.get(x);
+
+     assertTrue(oldNode.equals(newNode));
+    }
+
+
+
+
+  }
+
+  protected void setUp()
+    throws Exception
+  {
+    super.setUp();
+    /**@todo verify the constructors*/
+    _hWPFDocFixture = new HWPFDocFixture(this);
+
+    _hWPFDocFixture.setUp();
+  }
+
+  protected void tearDown()
+    throws Exception
+  {
+    _hWPFDocFixture.tearDown();
+
+    _hWPFDocFixture = null;
+    super.tearDown();
+  }
+
+}
