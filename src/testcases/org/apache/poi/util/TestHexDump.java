3a59e361ff066a5d3d25fbbf05fab8d848f1f6e0
Glen Stampoultzis <glens@apache.org>
2004-08-10 02:59:52 +0000

    Empty byte array case for HexDump
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/poi/trunk@353579 13f79535-47bb-0310-9956-ffa450edef68

--- src/testcases/org/apache/poi/util/TestHexDump.java
+++ src/testcases/org/apache/poi/util/TestHexDump.java
@@ -277,6 +277,11 @@ public class TestHexDump
 
             // as expected
         }
+
+        // verify proper behaviour with empty byte array
+        ByteArrayOutputStream os = new ByteArrayOutputStream( );
+        HexDump.dump( new byte[0], 0, os, 0 );
+        assertEquals( "No Data", os.toString() );
     }
 
     public void testToHex()
