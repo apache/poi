f1382ee5ebcef196f771e11a0a7847e611950779
Glen Stampoultzis <glens@apache.org>
2005-05-01 22:00:12 +0000

    Moving to head
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/poi/trunk@353663 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/poi/hssf/usermodel/HSSFPicture.java
@@ -0,0 +1,39 @@
+package org.apache.poi.hssf.usermodel;
+
+/**
+ * Represents a escher picture.  Eg. A GIF, JPEG etc...
+ *
+ * @author Glen Stampoultzis
+ * @version $Id$
+ */
+public class HSSFPicture
+        extends HSSFSimpleShape
+{
+    public static final int PICTURE_TYPE_EMF = 0;                // Windows Enhanced Metafile
+    public static final int PICTURE_TYPE_WMF = 1;                // Windows Metafile
+    public static final int PICTURE_TYPE_PICT = 2;               // Macintosh PICT
+    public static final int PICTURE_TYPE_JPEG = 3;               // JFIF
+    public static final int PICTURE_TYPE_PNG = 4;                // PNG
+    public static final int PICTURE_TYPE_DIB = 5;                // Windows DIB
+
+    int pictureIndex;
+
+    /**
+     * Constructs a picture object.
+     */
+    HSSFPicture( HSSFShape parent, HSSFAnchor anchor )
+    {
+        super( parent, anchor );
+        setShapeType(OBJECT_TYPE_PICTURE);
+    }
+
+    public int getPictureIndex()
+    {
+        return pictureIndex;
+    }
+
+    public void setPictureIndex( int pictureIndex )
+    {
+        this.pictureIndex = pictureIndex;
+    }
+}
