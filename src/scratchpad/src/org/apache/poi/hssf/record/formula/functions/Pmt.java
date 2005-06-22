d8ea7f98e3f70cf8cbc516b35c37152dabe47892
Amol S. Deshmukh <amol@apache.org>
2005-06-22 20:43:30 +0000

    Implemented this finance function evaluation
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/poi/trunk@353723 13f79535-47bb-0310-9956-ffa450edef68

--- src/scratchpad/src/org/apache/poi/hssf/record/formula/functions/Pmt.java
+++ src/scratchpad/src/org/apache/poi/hssf/record/formula/functions/Pmt.java
@@ -4,10 +4,59 @@
  */
 package org.apache.poi.hssf.record.formula.functions;
 
+import org.apache.poi.hssf.record.formula.eval.BoolEval;
+import org.apache.poi.hssf.record.formula.eval.ErrorEval;
+import org.apache.poi.hssf.record.formula.eval.Eval;
+import org.apache.poi.hssf.record.formula.eval.NumberEval;
+import org.apache.poi.hssf.record.formula.eval.NumericValueEval;
+import org.apache.poi.hssf.record.formula.eval.ValueEval;
+
 /**
  * @author 
  *
  */
-public class Pmt extends DefaultFunctionImpl {
+public class Pmt extends FinanceFunction {
 
+    public Eval evaluate(Eval[] operands, int srcRow, short srcCol) {
+        double rate = 0, fv = 0, nper = 0, pv = 0, d = 0;
+        boolean type = false;
+        ValueEval retval = null;
+        ValueEval ve = null;
+        
+        switch (operands.length) {
+        default:
+            retval = ErrorEval.VALUE_INVALID;
+            break;
+        case 5:
+            ve = singleOperandNumericAsBoolean(operands[4], srcRow, srcCol);
+            if (ve instanceof ErrorEval) { retval = ErrorEval.VALUE_INVALID; break; }
+            type = ((BoolEval) ve).getBooleanValue();
+        case 4:
+            ve = singleOperandEvaluate(operands[0], srcRow, srcCol);
+            if (ve instanceof NumericValueEval) rate = ((NumericValueEval) ve).getNumberValue();
+            else { retval = ErrorEval.VALUE_INVALID; break; }
+            
+            ve = singleOperandEvaluate(operands[1], srcRow, srcCol);
+            if (ve instanceof NumericValueEval) nper  = ((NumericValueEval) ve).getNumberValue();
+            else { retval = ErrorEval.VALUE_INVALID; break; }
+            
+            ve = singleOperandEvaluate(operands[2], srcRow, srcCol);
+            if (ve instanceof NumericValueEval) pv   = ((NumericValueEval) ve).getNumberValue();
+            else { retval = ErrorEval.VALUE_INVALID; break; }
+            
+            ve = singleOperandEvaluate(operands[3], srcRow, srcCol);
+            if (ve instanceof NumericValueEval) fv = ((NumericValueEval) ve).getNumberValue();
+            else { retval = ErrorEval.VALUE_INVALID; break; }
+        }
+        
+        if (retval == null) {
+            d = FinanceLib.pmt(rate, nper, pv, fv, type);
+            retval = (Double.isNaN(d))
+                    ? (ValueEval) ErrorEval.VALUE_INVALID
+                    : (Double.isInfinite(d)) 
+                        ? (ValueEval) ErrorEval.NUM_ERROR 
+                        : new NumberEval(d);
+        }
+        return retval;
+    }
 }
