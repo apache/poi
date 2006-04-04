4850277501b9118514742c17434f709625e78d11
Nick Burch <nick@apache.org>
2006-04-04 17:02:14 +0000

    Convert HWPFDocument to having POIDocument as its parent
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/poi/trunk@391365 13f79535-47bb-0310-9956-ffa450edef68

--- src/scratchpad/testcases/org/apache/poi/TestPOIDocument.java
+++ src/scratchpad/testcases/org/apache/poi/TestPOIDocument.java
@@ -24,6 +24,7 @@ import junit.framework.TestCase;
 import java.io.*;
 
 import org.apache.poi.hslf.HSLFSlideShow;
+import org.apache.poi.hwpf.HWPFDocument;
 import org.apache.poi.poifs.filesystem.*;
 
 /**
@@ -33,20 +34,30 @@ import org.apache.poi.poifs.filesystem.*;
  * @author Nick Burch (nick at torchbox dot com)
  */
 public class TestPOIDocument extends TestCase {
-	// The POI Document to work on
+	// The POI Documents to work on
 	private POIDocument doc;
-	// POIFS primed on the test (powerpoint) data
+	private POIDocument doc2;
+	// POIFS primed on the test (powerpoint and word) data
 	private POIFSFileSystem pfs;
+	private POIFSFileSystem pfs2;
 
 	/**
-	 * Set things up, using a PowerPoint document for our testing
+	 * Set things up, using a PowerPoint document and 
+	 *  a Word Document for our testing
 	 */
     public void setUp() throws Exception {
-		String dirname = System.getProperty("HSLF.testdata.path");
-		String filename = dirname + "/basic_test_ppt_file.ppt";
-		FileInputStream fis = new FileInputStream(filename);
-		pfs = new POIFSFileSystem(fis);
+		String dirnameHSLF = System.getProperty("HSLF.testdata.path");
+		String filenameHSLF = dirnameHSLF + "/basic_test_ppt_file.ppt";
+		String dirnameHWPF = System.getProperty("HWPF.testdata.path");
+		String filenameHWPF = dirnameHWPF + "/test2.doc";
+		
+		FileInputStream fisHSLF = new FileInputStream(filenameHSLF);
+		pfs = new POIFSFileSystem(fisHSLF);
 		doc = new HSLFSlideShow(pfs);
+		
+		FileInputStream fisHWPF = new FileInputStream(filenameHWPF);
+		pfs2 = new POIFSFileSystem(fisHWPF);
+		doc2 = new HWPFDocument(pfs2);
 	}
     
     public void testReadProperties() throws Exception {
@@ -57,6 +68,16 @@ public class TestPOIDocument extends TestCase {
     	// Check they are as expected for the test doc
     	assertEquals("Hogwarts", doc.getSummaryInformation().getAuthor());
     	assertEquals(10598, doc.getDocumentSummaryInformation().getByteCount());
+    }
+    	
+    public void testReadProperties2() throws Exception {	
+    	// Check again on the word one
+    	assertNotNull(doc2.getDocumentSummaryInformation());
+    	assertNotNull(doc2.getSummaryInformation());
+    	
+    	assertEquals("Hogwarts", doc2.getSummaryInformation().getAuthor());
+    	assertEquals("", doc2.getSummaryInformation().getKeywords());
+    	assertEquals(0, doc2.getDocumentSummaryInformation().getByteCount());
     }
 
     public void testWriteProperties() throws Exception {
