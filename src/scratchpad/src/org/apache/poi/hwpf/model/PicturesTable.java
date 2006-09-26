34878d8f0147696fda0c4690f93d3933c2df1db0
Nick Burch <nick@apache.org>
2006-09-26 14:46:39 +0000

    From bug #38616 - support for extracting images from word files, plus tests for this
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/poi/trunk@450066 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/scratchpad/src/org/apache/poi/hwpf/model/PicturesTable.java
@@ -0,0 +1,152 @@
+/* ====================================================================
+   Copyright 2002-2006   Apache Software Foundation
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+==================================================================== */
+
+
+package org.apache.poi.hwpf.model;
+
+import org.apache.poi.util.LittleEndian;
+import org.apache.poi.hwpf.usermodel.CharacterRun;
+import org.apache.poi.hwpf.usermodel.Picture;
+
+import java.util.List;
+import java.util.ArrayList;
+
+
+/**
+ * Holds information about all pictures embedded in Word Document either via "Insert -> Picture -> From File" or via
+ * clipboard. Responsible for images extraction and determining whether some document�s piece contains embedded image.
+ * Analyzes raw data bytestream �Data� (where Word stores all embedded objects) provided by HWPFDocument.
+ *
+ * Word stores images as is within so called "Data stream" - the stream within a Word docfile containing various data
+ * that hang off of characters in the main stream. For example, binary data describing in-line pictures and/or
+ * formfields an also embedded objects-native data. Word picture structures are concatenated one after the other in
+ * the data stream if the document contains pictures.
+ * Data stream is easily reachable via HWPFDocument._dataStream property.
+ * A picture is represented in the document text stream as a special character, an Unicode \u0001 whose
+ * CharacterRun.isSpecial() returns true. The file location of the picture in the Word binary file is accessed
+ * via CharacterRun.getPicOffset(). The CharacterRun.getPicOffset() is a byte offset into the data stream.
+ * Beginning at the position recorded in picOffset, a header data structure, will be stored.
+ *
+ * @author Dmitry Romanov
+ */
+public class PicturesTable
+{
+  static final int TYPE_IMAGE = 0x08;
+  static final int TYPE_IMAGE_WORD2000 = 0x00;
+  static final int TYPE_IMAGE_PASTED_FROM_CLIPBOARD = 0xA;
+  static final int TYPE_IMAGE_PASTED_FROM_CLIPBOARD_WORD2000 = 0x2;
+  static final int TYPE_HORIZONTAL_LINE = 0xE;
+  static final int BLOCK_TYPE_OFFSET = 0xE;
+  static final int MM_MODE_TYPE_OFFSET = 0x6;
+
+  private byte[] _dataStream;
+
+  /** @link dependency
+   * @stereotype instantiate*/
+  /*# Picture lnkPicture; */
+
+  /**
+   *
+   * @param _dataStream
+   */
+  public PicturesTable(byte[] _dataStream)
+  {
+    this._dataStream = _dataStream;
+  }
+
+  /**
+   * determines whether specified CharacterRun contains reference to a picture
+   * @param run
+   */
+  public boolean hasPicture(CharacterRun run) {
+    if (run.isSpecialCharacter() && !run.isObj() && !run.isOle2() && !run.isData() && "\u0001".equals(run.text())) {
+      return isBlockContainsImage(run.getPicOffset());
+    }
+    return false;
+  }
+
+  /**
+   * determines whether specified CharacterRun contains reference to a picture
+   * @param run
+  */
+  public boolean hasHorizontalLine(CharacterRun run) {
+    if (run.isSpecialCharacter() && "\u0001".equals(run.text())) {
+      return isBlockContainsHorizontalLine(run.getPicOffset());
+    }
+    return false;
+  }
+
+  private boolean isPictureRecognized(short blockType, short mappingModeOfMETAFILEPICT) {
+    return (blockType == TYPE_IMAGE || blockType == TYPE_IMAGE_PASTED_FROM_CLIPBOARD || (blockType==TYPE_IMAGE_WORD2000 && mappingModeOfMETAFILEPICT==0x64) || (blockType==TYPE_IMAGE_PASTED_FROM_CLIPBOARD_WORD2000 && mappingModeOfMETAFILEPICT==0x64));
+  }
+
+  private static short getBlockType(byte[] dataStream, int pictOffset) {
+    return LittleEndian.getShort(dataStream, pictOffset + BLOCK_TYPE_OFFSET);
+  }
+
+  private static short getMmMode(byte[] dataStream, int pictOffset) {
+    return LittleEndian.getShort(dataStream, pictOffset + MM_MODE_TYPE_OFFSET);
+  }
+
+  /**
+   * Returns picture object tied to specified CharacterRun
+   * @param run
+   * @param fillBytes if true, Picture will be returned with filled byte array that represent picture's contents. If you don't want
+   * to have that byte array in memory but only write picture's contents to stream, pass false and then use Picture.writeImageContent
+   * @see Picture#writeImageContent(java.io.OutputStream)
+   * @return a Picture object if picture exists for specified CharacterRun, null otherwise. PicturesTable.hasPicture is used to determine this.
+   * @see #hasPicture(org.apache.poi.hwpf.usermodel.CharacterRun) 
+   */
+  public Picture extractPicture(CharacterRun run, boolean fillBytes) {
+    if (hasPicture(run)) {
+      return new Picture(run.getPicOffset(), _dataStream, fillBytes);
+    }
+    return null;
+  }
+
+  /**
+   * @return a list of Picture objects found in current document
+   */
+  public List getAllPictures() {
+    ArrayList pictures = new ArrayList();
+    
+    int pos = 0;
+    boolean atEnd = false;
+    
+    while(pos<_dataStream.length && !atEnd) {
+      if (isBlockContainsImage(pos)) {
+        pictures.add(new Picture(pos, _dataStream, false));
+      }
+      
+      int skipOn = LittleEndian.getInt(_dataStream, pos);
+      if(skipOn <= 0) { atEnd = true; }
+      pos += skipOn;
+    }
+    
+    return pictures;
+  }
+
+  private boolean isBlockContainsImage(int i)
+  {
+    return isPictureRecognized(getBlockType(_dataStream, i), getMmMode(_dataStream, i));
+  }
+
+  private boolean isBlockContainsHorizontalLine(int i)
+  {
+    return getBlockType(_dataStream, i)==TYPE_HORIZONTAL_LINE && getMmMode(_dataStream, i)==0x64;
+  }
+
+}
---
+++ src/scratchpad/testcases/org/apache/poi/hwpf/TestHWPFPictures.java
@@ -0,0 +1,130 @@
+
+/* ====================================================================
+   Copyright 2002-2004   Apache Software Foundation
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+==================================================================== */
+        
+package org.apache.poi.hwpf;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.util.List;
+
+import org.apache.poi.hwpf.model.PicturesTable;
+import org.apache.poi.hwpf.usermodel.Picture;
+
+import junit.framework.TestCase;
+
+/**
+ * Test picture support in HWPF
+ * @author nick
+ */
+public class TestHWPFPictures extends TestCase {
+	private HWPFDocument docA;
+	private HWPFDocument docB;
+	private String docAFile;
+	private String docBFile;
+	
+	private String imgAFile;
+	private String imgBFile;
+	
+	protected void setUp() throws Exception {
+		String dirname = System.getProperty("HWPF.testdata.path");
+		
+		docAFile = dirname + "/testPictures.doc";
+		docBFile = dirname + "/two_images.doc";
+		
+		imgAFile = dirname + "/simple_image.jpg";
+		imgBFile = dirname + "/simple_image.png";
+	}
+	
+	/**
+	 * Test just opening the files
+	 */
+	public void testOpen() throws Exception {
+		docA = new HWPFDocument(new FileInputStream(docAFile));
+		docB = new HWPFDocument(new FileInputStream(docBFile));
+	}
+	
+	/**
+	 * Test that we have the right numbers of images in each file
+	 */
+	public void testImageCount() throws Exception {
+		docA = new HWPFDocument(new FileInputStream(docAFile));
+		docB = new HWPFDocument(new FileInputStream(docBFile));
+		
+		assertNotNull(docA.getPicturesTable());
+		assertNotNull(docB.getPicturesTable());
+		
+		PicturesTable picA = docA.getPicturesTable();
+		PicturesTable picB = docB.getPicturesTable();
+		
+		List picturesA = picA.getAllPictures();
+		List picturesB = picB.getAllPictures();
+		
+		assertEquals(7, picturesA.size());
+		assertEquals(2, picturesB.size());
+	}
+	
+	/**
+	 * Test that we have the right images in at least one file
+	 */
+	public void testImageData() throws Exception {
+		docB = new HWPFDocument(new FileInputStream(docBFile));
+		PicturesTable picB = docB.getPicturesTable();
+		List picturesB = picB.getAllPictures();
+		
+		assertEquals(2, picturesB.size());
+		
+		Picture pic1 = (Picture)picturesB.get(0);
+		Picture pic2 = (Picture)picturesB.get(1);
+		
+		assertNotNull(pic1);
+		assertNotNull(pic2);
+		
+		// Check the same
+		byte[] pic1B = readFile(imgAFile);
+		byte[] pic2B = readFile(imgBFile);
+		
+		assertEquals(pic1B.length, pic1.getContent().length);
+		assertEquals(pic2B.length, pic2.getContent().length);
+
+		assertBytesSame(pic1B, pic1.getContent());
+		assertBytesSame(pic2B, pic2.getContent());
+	}
+	
+	
+	private void assertBytesSame(byte[] a, byte[] b) {
+		assertEquals(a.length, b.length);
+		for(int i=0; i<a.length; i++) {
+			assertEquals(a[i],b[i]);
+		}
+	}
+	
+	private byte[] readFile(String file) throws Exception {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		FileInputStream fis = new FileInputStream(file);
+		byte[] buffer = new byte[1024];
+		
+		int read = 0;
+		while(read > -1) {
+			read = fis.read(buffer);
+			if(read > 0) {
+				baos.write(buffer,0,read);
+			}
+		}
+		
+		return baos.toByteArray();
+	}
+}
