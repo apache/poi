4a42f6a765840cea6beeef02b18056b27091f5dc
Amol S. Deshmukh <amol@apache.org>
2006-11-26 06:14:11 +0000

    initial version
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/poi/trunk@479290 13f79535-47bb-0310-9956-ffa450edef68

--- src/scratchpad/src/org/apache/poi/hssf/record/formula/functions/Isref.java
+++ src/scratchpad/src/org/apache/poi/hssf/record/formula/functions/Isref.java
@@ -4,10 +4,31 @@
  */
 package org.apache.poi.hssf.record.formula.functions;
 
+import org.apache.poi.hssf.record.formula.eval.AreaEval;
+import org.apache.poi.hssf.record.formula.eval.BoolEval;
+import org.apache.poi.hssf.record.formula.eval.ErrorEval;
+import org.apache.poi.hssf.record.formula.eval.Eval;
+import org.apache.poi.hssf.record.formula.eval.RefEval;
+
 /**
- * @author 
+ * @author Amol S. Deshmukh &lt; amolweb at ya hoo dot com &gt;
  *
  */
-public class Isref extends NotImplementedFunction {
-
+public class Isref implements Function {
+    public Eval evaluate(Eval[] operands, int srcCellRow, short srcCellCol) {
+        Eval retval = BoolEval.FALSE;
+        
+        switch (operands.length) {
+        default:
+            retval = ErrorEval.VALUE_INVALID;
+            break;
+        case 1:
+            Eval eval = operands[0];
+            if (eval instanceof RefEval || eval instanceof AreaEval) {
+                retval = BoolEval.TRUE;
+            }
+        }
+        
+        return retval;
+    }
 }
