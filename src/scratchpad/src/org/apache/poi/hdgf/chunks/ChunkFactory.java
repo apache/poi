2a0276c665240b65edade0e89634c26cad5b6eea
Nick Burch <nick@apache.org>
2007-06-21 19:28:04 +0000

    Start on processing the commands stored within a HDGF chunk, plus initial tests for it
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/poi/trunk@549588 13f79535-47bb-0310-9956-ffa450edef68

--- src/scratchpad/src/org/apache/poi/hdgf/chunks/ChunkFactory.java
+++ src/scratchpad/src/org/apache/poi/hdgf/chunks/ChunkFactory.java
@@ -16,18 +16,80 @@
 ==================================================================== */
 package org.apache.poi.hdgf.chunks;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.StringTokenizer;
+
 /**
  * Factor class to create the appropriate chunks, which
  *  needs the version of the file to process the chunk header
  *  and trailer areas.
  * Makes use of chunks_parse_cmds.tbl from vsdump to be able
- *  to c
+ *  to process the chunk value area
  */
 public class ChunkFactory {
+	/** The version of the currently open document */
 	private int version;
-	public ChunkFactory(int version) {
+	/**
+	 * Key is a Chunk's type, value is an array of its CommandDefinitions
+	 */
+	private Hashtable chunkCommandDefinitions = new Hashtable();
+	/** What the name is of the chunk table */
+	private static String chunkTableName = 
+		"/org/apache/poi/hdgf/chunks/chunks_parse_cmds.tbl";
+	
+	public ChunkFactory(int version) throws IOException {
 		this.version = version;
+		
+		processChunkParseCommands();
 	}
+	
+	/** 
+	 * Open chunks_parse_cmds.tbl and process it, to get the definitions
+	 *  of all the different possible chunk commands. 
+	 */
+	private void processChunkParseCommands() throws IOException {
+		String line;
+		InputStream cpd = ChunkFactory.class.getResourceAsStream(chunkTableName);
+		BufferedReader inp = new BufferedReader(new InputStreamReader(cpd));
+		while( (line = inp.readLine()) != null ) {
+			if(line.startsWith("#")) continue;
+			if(line.startsWith(" ")) continue;
+			if(line.startsWith("\t")) continue;
+			if(line.length() == 0) continue;
+
+			// Start xxx
+			if(!line.startsWith("start")) {
+				throw new IllegalStateException("Expecting start xxx, found " + line);
+			}
+			int chunkType = Integer.parseInt(line.substring(6));
+			ArrayList defsL = new ArrayList();
+			
+			// Data entries
+			while( ! (line = inp.readLine()).startsWith("end") ) {
+				StringTokenizer st = new StringTokenizer(line, " ");
+				int defType = Integer.parseInt(st.nextToken());
+				int offset = Integer.parseInt(st.nextToken());
+				String name = st.nextToken("\uffff").substring(1);
+				
+				CommandDefinition def = new CommandDefinition(defType,offset,name);
+				defsL.add(def);
+			}
+			
+			CommandDefinition[] defs = (CommandDefinition[])
+				defsL.toArray(new CommandDefinition[defsL.size()]);
+			
+			// Add to the hashtable
+			chunkCommandDefinitions.put(new Integer(chunkType), defs);
+		}
+		inp.close();
+		cpd.close();
+	}
+	
 	public int getVersion() { return version; }
 
 	/**
@@ -88,9 +150,36 @@ public class ChunkFactory {
 		Chunk chunk = new Chunk(header, trailer, separator, contents);
 		
 		// Feed in the stuff from  chunks_parse_cmds.tbl
-		// TODO
+		CommandDefinition[] defs = (CommandDefinition[])
+			chunkCommandDefinitions.get(new Integer(header.getType()));
+		chunk.commandDefinitions = defs;
 		
 		// All done
 		return chunk;
+	}
+	
+	/**
+	 * The definition of a Command, which a chunk may hold.
+	 * The Command holds the value, this describes it.
+	 */
+	public class CommandDefinition {
+		private int type;
+		private int offset;
+		private String name;
+		public CommandDefinition(int type, int offset, String name) {
+			this.type = type;
+			this.offset = offset;
+			this.name = name;
+		}
+		
+		public String getName() {
+			return name;
+		}
+		public int getOffset() {
+			return offset;
+		}
+		public int getType() {
+			return type;
+		}
 	}
 }
