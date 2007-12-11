f6a7cb7483105b68b6c2f470adf4ca172ebefd1e
Nick Burch <nick@apache.org>
2007-12-11 13:03:53 +0000

    Support for the Trim function, and a little enhancement to the formula evaluation test
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@603233 13f79535-47bb-0310-9956-ffa450edef68

--- src/scratchpad/src/org/apache/poi/hssf/record/formula/functions/Trim.java
+++ src/scratchpad/src/org/apache/poi/hssf/record/formula/functions/Trim.java
@@ -14,12 +14,62 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
-/*
- * Created on May 15, 2005
- *
- */
 package org.apache.poi.hssf.record.formula.functions;
 
-public class Trim extends NotImplementedFunction {
+import org.apache.poi.hssf.record.formula.eval.BlankEval;
+import org.apache.poi.hssf.record.formula.eval.ErrorEval;
+import org.apache.poi.hssf.record.formula.eval.Eval;
+import org.apache.poi.hssf.record.formula.eval.NumberEval;
+import org.apache.poi.hssf.record.formula.eval.StringEval;
+import org.apache.poi.hssf.record.formula.eval.StringValueEval;
+import org.apache.poi.hssf.record.formula.eval.ValueEval;
 
+/**
+ * An implementation of the TRIM function:
+ * Removes leading and trailing spaces from value if evaluated operand
+ *  value is string.
+ * @author Manda Wilson &lt; wilson at c bio dot msk cc dot org &gt;
+ */
+public class Trim extends TextFunction {
+
+	/**
+	 * Removes leading and trailing spaces from value if evaluated 
+	 *  operand value is string.
+	 * Returns StringEval only if evaluated operand is of type string 
+	 *  (and is not blank or null) or number. If evaluated operand is 
+	 *  of type string and is blank or null, or if evaluated operand is 
+	 *  of type blank, returns BlankEval.  Otherwise returns ErrorEval.
+	 * 
+	 * @see org.apache.poi.hssf.record.formula.eval.Eval
+	 */
+    public Eval evaluate(Eval[] operands, int srcCellRow, short srcCellCol) {
+    	Eval retval = ErrorEval.VALUE_INVALID;
+        String str = null;
+        
+        switch (operands.length) {
+	        default:
+	            break;
+	        case 1:
+	            ValueEval veval = singleOperandEvaluate(operands[0], srcCellRow, srcCellCol);
+	            if (veval instanceof StringValueEval) {
+	                StringValueEval sve = (StringValueEval) veval;
+	                str = sve.getStringValue();
+	                if (str == null || str.trim().equals("")) {
+	                	return BlankEval.INSTANCE;
+	                }
+	            }
+	            else if (veval instanceof NumberEval) {
+	                NumberEval neval = (NumberEval) veval;
+	                str = neval.getStringValue();
+	            } 
+	            else if (veval instanceof BlankEval) {
+	            	return BlankEval.INSTANCE;
+	            }
+	    }
+	        
+        if (str != null) {
+            retval = new StringEval(str.trim());
+        } 
+        return retval;
+    }
 }
