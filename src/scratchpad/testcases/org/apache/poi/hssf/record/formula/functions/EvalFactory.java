1f4c3cc7503fc20a89bfc9332d33801d61e13117
Nick Burch <nick@apache.org>
2008-02-04 10:48:29 +0000

    Implement CountA, CountIf, Index, Rows and Columns functions. Patch from Josh Micich in bug #44345
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@618230 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/scratchpad/testcases/org/apache/poi/hssf/record/formula/functions/EvalFactory.java
@@ -0,0 +1,63 @@
+/* ====================================================================
+   Licensed to the Apache Software Foundation (ASF) under one or more
+   contributor license agreements.  See the NOTICE file distributed with
+   this work for additional information regarding copyright ownership.
+   The ASF licenses this file to You under the Apache License, Version 2.0
+   (the "License"); you may not use this file except in compliance with
+   the License.  You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+==================================================================== */
+        
+
+package org.apache.poi.hssf.record.formula.functions;
+
+import org.apache.poi.hssf.record.formula.AreaPtg;
+import org.apache.poi.hssf.record.formula.ReferencePtg;
+import org.apache.poi.hssf.record.formula.eval.Area2DEval;
+import org.apache.poi.hssf.record.formula.eval.AreaEval;
+import org.apache.poi.hssf.record.formula.eval.NumberEval;
+import org.apache.poi.hssf.record.formula.eval.Ref2DEval;
+import org.apache.poi.hssf.record.formula.eval.RefEval;
+import org.apache.poi.hssf.record.formula.eval.ValueEval;
+
+/**
+ * Test helper class for creating mock <code>Eval</code> objects
+ * 
+ * @author Josh Micich
+ */
+final class EvalFactory {
+	private static final NumberEval ZERO = new NumberEval(0);
+
+	private EvalFactory() {
+		// no instances of this class
+	}
+
+	/**
+	 * Creates a dummy AreaEval (filled with zeros)
+	 * <p/>
+	 * nCols and nRows could have been derived
+	 */
+	public static AreaEval createAreaEval(String areaRefStr, int nCols, int nRows) {
+		int nValues = nCols * nRows;
+		ValueEval[] values = new ValueEval[nValues];
+		for (int i = 0; i < nValues; i++) {
+			values[i] = ZERO;
+		}
+		
+		return new Area2DEval(new AreaPtg(areaRefStr), values);
+	}
+
+	/**
+	 * Creates a single RefEval (with value zero)
+	 */
+	public static RefEval createRefEval(String refStr) {
+		return new Ref2DEval(new ReferencePtg(refStr), ZERO, true);
+	}
+}
---
+++ src/scratchpad/testcases/org/apache/poi/hssf/record/formula/functions/TestCountFuncs.java
@@ -0,0 +1,150 @@
+/* ====================================================================
+   Licensed to the Apache Software Foundation (ASF) under one or more
+   contributor license agreements.  See the NOTICE file distributed with
+   this work for additional information regarding copyright ownership.
+   The ASF licenses this file to You under the Apache License, Version 2.0
+   (the "License"); you may not use this file except in compliance with
+   the License.  You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+==================================================================== */
+        
+
+package org.apache.poi.hssf.record.formula.functions;
+
+import junit.framework.TestCase;
+
+import org.apache.poi.hssf.record.formula.AreaPtg;
+import org.apache.poi.hssf.record.formula.ReferencePtg;
+import org.apache.poi.hssf.record.formula.eval.Area2DEval;
+import org.apache.poi.hssf.record.formula.eval.AreaEval;
+import org.apache.poi.hssf.record.formula.eval.BlankEval;
+import org.apache.poi.hssf.record.formula.eval.BoolEval;
+import org.apache.poi.hssf.record.formula.eval.Eval;
+import org.apache.poi.hssf.record.formula.eval.NumberEval;
+import org.apache.poi.hssf.record.formula.eval.Ref2DEval;
+import org.apache.poi.hssf.record.formula.eval.StringEval;
+import org.apache.poi.hssf.record.formula.eval.ValueEval;
+
+/**
+ * Test cases for COUNT(), COUNTA() COUNTIF(), COUNTBLANK()
+ * 
+ * @author Josh Micich
+ */
+public final class TestCountFuncs extends TestCase {
+
+	public TestCountFuncs(String testName) {
+		super(testName);
+	}
+	
+	public void testCountA() {
+		
+		Eval[] args;
+		
+		args = new Eval[] {
+			new NumberEval(0),	 
+		};
+		confirmCountA(1, args);
+		
+		args = new Eval[] {
+			new NumberEval(0),	
+			new NumberEval(0),
+			new StringEval(""),
+		};
+		confirmCountA(3, args);
+		
+		args = new Eval[] {
+			EvalFactory.createAreaEval("D2:F5", 3, 4),	
+		};
+		confirmCountA(12, args);
+		
+		args = new Eval[] {
+			EvalFactory.createAreaEval("D1:F5", 3, 5),	// 15
+			EvalFactory.createRefEval("A1"),	
+			EvalFactory.createAreaEval("A1:F6", 7, 6),	// 42
+			new NumberEval(0),
+		};
+		confirmCountA(59, args);
+	}
+
+	public void testCountIf() {
+		
+		AreaEval range;
+		ValueEval[] values;
+		
+		// when criteria is a boolean value
+		values = new ValueEval[] {
+				new NumberEval(0),	
+				new StringEval("TRUE"),	// note - does not match boolean TRUE
+				BoolEval.TRUE,
+				BoolEval.FALSE,
+				BoolEval.TRUE,
+				BlankEval.INSTANCE,
+		};
+		range = createAreaEval("A1:B2", values);
+		confirmCountIf(2, range, BoolEval.TRUE);
+		
+		// when criteria is numeric
+		values = new ValueEval[] {
+				new NumberEval(0),	
+				new StringEval("2"),	
+				new StringEval("2.001"),	
+				new NumberEval(2),	
+				new NumberEval(2),	
+				BoolEval.TRUE,
+				BlankEval.INSTANCE,
+		};
+		range = createAreaEval("A1:B2", values);
+		confirmCountIf(3, range, new NumberEval(2));
+		// note - same results when criteria is a string that parses as the number with the same value
+		confirmCountIf(3, range, new StringEval("2.00"));
+		
+		if (false) { // not supported yet: 
+			// when criteria is an expression (starting with a comparison operator)
+			confirmCountIf(4, range, new StringEval(">1"));
+		}
+	}
+	/**
+	 * special case where the criteria argument is a cell reference
+	 */
+	public void testCountIfWithCriteriaReference() {
+
+		ValueEval[] values = { 
+				new NumberEval(22),
+				new NumberEval(25),
+				new NumberEval(21),
+				new NumberEval(25),
+				new NumberEval(25),
+				new NumberEval(25),
+		};
+		Area2DEval arg0 = new Area2DEval(new AreaPtg("C1:C6"), values);
+		
+		Ref2DEval criteriaArg = new Ref2DEval(new ReferencePtg("A1"), new NumberEval(25), true);
+		Eval[] args=  { arg0, criteriaArg, };
+		
+		double actual = NumericFunctionInvoker.invoke(new Countif(), args);
+		assertEquals(4, actual, 0D);
+	}
+	
+
+	private static AreaEval createAreaEval(String areaRefStr, ValueEval[] values) {
+		return new Area2DEval(new AreaPtg(areaRefStr), values);
+	}
+
+	private static void confirmCountA(int expected, Eval[] args) {
+		double result = NumericFunctionInvoker.invoke(new Counta(), args);
+		assertEquals(expected, result, 0);
+	}
+	private static void confirmCountIf(int expected, AreaEval range, Eval criteria) {
+		
+		Eval[] args = { range, criteria, };
+		double result = NumericFunctionInvoker.invoke(new Countif(), args);
+		assertEquals(expected, result, 0);
+	}
+}
