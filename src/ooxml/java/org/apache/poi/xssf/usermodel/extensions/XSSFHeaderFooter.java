154979d07d0c9167ddd7c1425d5e9c6963d4b132
Ugo Cei <ugo@apache.org>
2008-02-07 09:24:49 +0000

    XSSF implementation of header and footer. Contributed by Paolo Mottadelli <p.mottadelli@sourcesense.com> as bugzilla issue #44330.
    git-svn-id: https://svn.apache.org/repos/asf/poi/branches/ooxml@619316 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/ooxml/java/org/apache/poi/xssf/usermodel/extensions/XSSFHeaderFooter.java
@@ -0,0 +1,73 @@
+/* ====================================================================
+   Licensed to the Apache Software Foundation (ASF) under one or more
+   contributor license agreements.  See the NOTICE file distributed with
+   this work for additional information regarding copyright ownership.
+   The ASF licenses this file to You under the Apache License, Version 2.0
+   (the "License"); you may not use this file except in compliance with
+   the License.  You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+==================================================================== */
+
+package org.apache.poi.xssf.usermodel.extensions;
+
+import org.apache.poi.ss.usermodel.Footer;
+import org.apache.poi.xssf.usermodel.helpers.HeaderFooterHelper;
+import org.openxmlformats.schemas.spreadsheetml.x2006.main.CTHeaderFooter;
+
+public abstract class XSSFHeaderFooter implements Footer{
+    
+    private HeaderFooterHelper helper;
+    private CTHeaderFooter headerFooter;
+    private String value;
+
+    public XSSFHeaderFooter(CTHeaderFooter headerFooter) {
+       this.headerFooter = headerFooter;
+       this.value = getText();
+       this.value = this.value != null ? this.value : "";
+       this.helper = new HeaderFooterHelper();
+    }
+    
+    public CTHeaderFooter getHeaderFooter() {
+        return this.headerFooter;
+    }
+    
+    public String getValue() {
+        return this.value;
+    }
+    
+    public abstract String getText();
+    
+    protected abstract void setText(String text);
+
+    public String getCenter() {
+        return helper.getCenterSection(getText());
+    }
+
+    public String getLeft() {
+        return helper.getLeftSection(getText());
+    }
+
+    public String getRight() {
+        return helper.getRightSection(getText());
+    }
+
+    public void setCenter(String newCenter) {
+        setText(helper.setCenterSection(getText(), newCenter));
+    }
+
+    public void setLeft(String newLeft) {
+        setText(helper.setLeftSection(getText(), newLeft));
+    }
+
+    public void setRight(String newRight) {
+        setText(helper.setRightSection(getText(), newRight));
+    }
+
+}
--- src/ooxml/java/org/apache/poi/xssf/usermodel/helpers/ColumnHelper.java
+++ src/ooxml/java/org/apache/poi/xssf/usermodel/helpers/ColumnHelper.java
@@ -1,99 +1,90 @@
+/* ====================================================================
+   Licensed to the Apache Software Foundation (ASF) under one or more
+   contributor license agreements.  See the NOTICE file distributed with
+   this work for additional information regarding copyright ownership.
+   The ASF licenses this file to You under the Apache License, Version 2.0
+   (the "License"); you may not use this file except in compliance with
+   the License.  You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+==================================================================== */
+
 package org.apache.poi.xssf.usermodel.helpers;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 
 import org.openxmlformats.schemas.spreadsheetml.x2006.main.CTCol;
 import org.openxmlformats.schemas.spreadsheetml.x2006.main.CTCols;
 import org.openxmlformats.schemas.spreadsheetml.x2006.main.CTWorksheet;
 
+
 public class ColumnHelper {
-	
-	private List<CTCol> columns;
-	
-    public ColumnHelper(CTWorksheet worksheet) {
-		super();
-		setColumns(worksheet);
-	}
-
-	public List<CTCol> getColumns() {
-    	return columns;
-    }
-	
-	public void setColumns(CTWorksheet worksheet) {
-    	columns = new ArrayList<CTCol>();
-    	CTCols[] colsArray = worksheet.getColsArray();
-    	for (int i = 0 ; i < colsArray.length ; i++) {
-    		CTCols cols = colsArray[i];
-    		CTCol[] colArray = cols.getColArray();
-    		for (int y = 0 ; y < colArray.length ; y++) {
-    		 	CTCol col = colArray[y];
-    		 	for (long k = col.getMin() ; k <= col.getMax() ; k++) {
-    		 		setColumn(columns, col, k);
-    		 	}
-    		}
-    	}
-	}
-
-	private void setColumn(List<CTCol> columns, CTCol col, long k) {
-		CTCol column = getColumn(columns, k);
-		if (column == null) {
-			column = CTCol.Factory.newInstance();
-			column.setMin(k);
-			column.setMax(k);
-			setColumnAttributes(col, column);
-			columns.add(column);
-		}
-		else {
-			setColumnAttributes(col, column);
-		}
-	}
-
-	private void setColumnAttributes(CTCol col, CTCol column) {
-		if (col.getWidth() > 0) {
-			column.setWidth(col.getWidth());
-		}
-		// TODO set all col attributes
-	}
     
-    public CTCol getColumn(List<CTCol> columns, long k) {
-    	for (Iterator<CTCol> it = columns.iterator() ; it.hasNext() ; ) {
-    		CTCol column = it.next();
-    		if (column.getMin() == k) {
-    			return column;
-    		}
-    	}
-    	return null;
+    private CTWorksheet worksheet;
+    
+
+    public ColumnHelper(CTWorksheet worksheet) {
+        super();
+        this.worksheet = worksheet;
+        cleanColumns();
+    }
+
+    public void cleanColumns() {
+        CTCols newCols = CTCols.Factory.newInstance();
+        CTCols[] colsArray = worksheet.getColsArray();
+        int i = 0;
+        for (i = 0 ; i < colsArray.length ; i++) {
+            CTCols cols = colsArray[i];
+            CTCol[] colArray = cols.getColArray();
+            for (int y = 0 ; y < colArray.length ; y++) {
+                 CTCol col = colArray[y];
+                 for (long k = col.getMin() ; k <= col.getMax() ; k++) {
+                     if (!columnExists(newCols, k)) {
+                         CTCol newCol = newCols.addNewCol();
+                         newCol.setMin(k);
+                         newCol.setMax(k);
+                         setColumnAttributes(col, newCol);
+                     }
+                 }
+            }
+        }
+        for (int y = i-1 ; y >= 0 ; y--) {
+            worksheet.removeCols(y);
+        }
+        worksheet.addNewCols();
+        worksheet.setColsArray(0, newCols);
     }
     
     public CTCol getColumn(long index) {
-    	for (Iterator<CTCol> it = columns.iterator() ; it.hasNext() ; ) {
-    		CTCol column = it.next();
-    		if (getColumnIndex(column) == index) {
-    			return column;
-    		}
-    	}
-    	return null;
+        for (int i = 0 ; i < worksheet.getColsArray(0).sizeOfColArray() ; i++) {
+            if (worksheet.getColsArray(0).getColArray(i).getMin() == index) {
+                return worksheet.getColsArray(0).getColArray(i);
+            }
+        }
+        return  null;
     }
     
-    public long getColumnIndex(CTCol column) {
-    	if (column.getMin() == column.getMax()) {
-    		return column.getMin();
-    	}
-    	return -1;
+    public boolean columnExists(CTCols cols, long index) {
+        for (int i = 0 ; i < cols.sizeOfColArray() ; i++) {
+            if (cols.getColArray(i).getMin() == index) {
+                return true;
+            }
+        }
+        return false;
     }
     
-    public CTCol createColumn(long index) {
-    	CTCol column = CTCol.Factory.newInstance();
-    	setIndex(column, index);
-    	columns.add(column);
-    	return column;
+    public void setColumnAttributes(CTCol col, CTCol newCol) {
+        if (col.getWidth() != 0) {
+            newCol.setWidth(col.getWidth());
+        }
+        if (col.getHidden()) {
+            newCol.setHidden(true);
+        }
     }
-
-	private void setIndex(CTCol column, long index) {
-		column.setMin(index);
-		column.setMax(index);
-	}
-
+    
 }
