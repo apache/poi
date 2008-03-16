f60c47e3b22e2a0cb001da3d55cafb0794be9a1f
Nick Burch <nick@apache.org>
2008-03-16 15:38:09 +0000

    Patch from Josh from bug #44608 - Support for PercentPtg in the formula evaluator
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@637598 13f79535-47bb-0310-9956-ffa450edef68

--- src/scratchpad/testcases/org/apache/poi/hssf/record/formula/eval/TestFormulasFromSpreadsheet.java
+++ src/scratchpad/testcases/org/apache/poi/hssf/record/formula/eval/TestFormulasFromSpreadsheet.java
@@ -15,7 +15,6 @@
 * limitations under the License.
 */
 
-
 package org.apache.poi.hssf.record.formula.eval;
 
 import java.io.FileInputStream;
@@ -59,36 +58,36 @@ public final class TestFormulasFromSpreadsheet extends TestCase {
 		 * Name of the test spreadsheet (found in the standard test data folder)
 		 */
 		public final static String FILENAME = "FormulaEvalTestData.xls";
-	    /**
-	     * Row (zero-based) in the test spreadsheet where the operator examples start.
-	     */
+		/**
+		 * Row (zero-based) in the test spreadsheet where the operator examples start.
+		 */
 		public static final int START_OPERATORS_ROW_INDEX = 22; // Row '23'
-	    /**
-	     * Row (zero-based) in the test spreadsheet where the function examples start.
-	     */
-		public static final int START_FUNCTIONS_ROW_INDEX = 83; // Row '84' 
+		/**
+		 * Row (zero-based) in the test spreadsheet where the function examples start.
+		 */
+		public static final int START_FUNCTIONS_ROW_INDEX = 87; // Row '88' 
 		/** 
 		 * Index of the column that contains the function names
 		 */
-	    public static final short COLUMN_INDEX_FUNCTION_NAME = 1; // Column 'B'
+		public static final short COLUMN_INDEX_FUNCTION_NAME = 1; // Column 'B'
 	
-	    /**
-	     * Used to indicate when there are no more functions left
-	     */
+		/**
+		 * Used to indicate when there are no more functions left
+		 */
 		public static final String FUNCTION_NAMES_END_SENTINEL = "<END-OF-FUNCTIONS>";
 	
 		/**
 		 * Index of the column where the test values start (for each function)
 		 */
-	    public static final short COLUMN_INDEX_FIRST_TEST_VALUE = 3; // Column 'D'
-	    
-	    /**
-	     * Each function takes 4 rows in the test spreadsheet 
-	     */
+		public static final short COLUMN_INDEX_FIRST_TEST_VALUE = 3; // Column 'D'
+		
+		/**
+		 * Each function takes 4 rows in the test spreadsheet 
+		 */
 		public static final int NUMBER_OF_ROWS_PER_FUNCTION = 4;
 	}
 
-    private HSSFWorkbook workbook;
+	private HSSFWorkbook workbook;
 	private HSSFSheet sheet;
 	// Note - multiple failures are aggregated before ending.  
 	// If one or more functions fail, a single AssertionFailedError is thrown at the end
@@ -97,138 +96,138 @@ public final class TestFormulasFromSpreadsheet extends TestCase {
 	private int _evaluationFailureCount;
 	private int _evaluationSuccessCount;
 
-    private static final HSSFCell getExpectedValueCell(HSSFRow row, short columnIndex) {
-    	if (row == null) {
-    		return null;
-    	}
-    	return row.getCell(columnIndex);
-    }
+	private static final HSSFCell getExpectedValueCell(HSSFRow row, short columnIndex) {
+		if (row == null) {
+			return null;
+		}
+		return row.getCell(columnIndex);
+	}
 
 
-    private static void confirmExpectedResult(String msg, HSSFCell expected, HSSFFormulaEvaluator.CellValue actual) {
-        if (expected == null) {
+	private static void confirmExpectedResult(String msg, HSSFCell expected, HSSFFormulaEvaluator.CellValue actual) {
+		if (expected == null) {
 			throw new AssertionFailedError(msg + " - Bad setup data expected value is null");
 		}
 		if(actual == null) {
 			throw new AssertionFailedError(msg + " - actual value was null");
 		}
-        
+		
 		if (expected.getCellType() == HSSFCell.CELL_TYPE_STRING) {
-		    String value = expected.getRichStringCellValue().getString();
-		    if (value.startsWith("#")) {
-		    	// TODO - this code never called
-		        expected.setCellType(HSSFCell.CELL_TYPE_ERROR);
-		        // expected.setCellErrorValue(...?);
-		    }
+			String value = expected.getRichStringCellValue().getString();
+			if (value.startsWith("#")) {
+				// TODO - this code never called
+				expected.setCellType(HSSFCell.CELL_TYPE_ERROR);
+				// expected.setCellErrorValue(...?);
+			}
 		}
 		
 		switch (expected.getCellType()) {
 			case HSSFCell.CELL_TYPE_BLANK:
-			    assertEquals(msg, HSSFCell.CELL_TYPE_BLANK, actual.getCellType());
-			    break;
+				assertEquals(msg, HSSFCell.CELL_TYPE_BLANK, actual.getCellType());
+				break;
 			case HSSFCell.CELL_TYPE_BOOLEAN:
-			    assertEquals(msg, HSSFCell.CELL_TYPE_BOOLEAN, actual.getCellType());
-			    assertEquals(msg, expected.getBooleanCellValue(), actual.getBooleanValue());
-			    break;
+				assertEquals(msg, HSSFCell.CELL_TYPE_BOOLEAN, actual.getCellType());
+				assertEquals(msg, expected.getBooleanCellValue(), actual.getBooleanValue());
+				break;
 			case HSSFCell.CELL_TYPE_ERROR:
-			    assertEquals(msg, HSSFCell.CELL_TYPE_ERROR, actual.getCellType());
-			    if(false) { // TODO: fix ~45 functions which are currently returning incorrect error values
-			    	assertEquals(msg, expected.getErrorCellValue(), actual.getErrorValue());
-			    }
-			    break;
+				assertEquals(msg, HSSFCell.CELL_TYPE_ERROR, actual.getCellType());
+				if(false) { // TODO: fix ~45 functions which are currently returning incorrect error values
+					assertEquals(msg, expected.getErrorCellValue(), actual.getErrorValue());
+				}
+				break;
 			case HSSFCell.CELL_TYPE_FORMULA: // will never be used, since we will call method after formula evaluation
-			    throw new AssertionFailedError("Cannot expect formula as result of formula evaluation: " + msg);
+				throw new AssertionFailedError("Cannot expect formula as result of formula evaluation: " + msg);
 			case HSSFCell.CELL_TYPE_NUMERIC:
-			    assertEquals(msg, HSSFCell.CELL_TYPE_NUMERIC, actual.getCellType());
-			    TestMathX.assertEquals(msg, expected.getNumericCellValue(), actual.getNumberValue(), TestMathX.POS_ZERO, TestMathX.DIFF_TOLERANCE_FACTOR);
-//	            double delta = Math.abs(expected.getNumericCellValue()-actual.getNumberValue());
-//	            double pctExpected = Math.abs(0.00001*expected.getNumericCellValue());
-//	            assertTrue(msg, delta <= pctExpected);
-			    break;
+				assertEquals(msg, HSSFCell.CELL_TYPE_NUMERIC, actual.getCellType());
+				TestMathX.assertEquals(msg, expected.getNumericCellValue(), actual.getNumberValue(), TestMathX.POS_ZERO, TestMathX.DIFF_TOLERANCE_FACTOR);
+//				double delta = Math.abs(expected.getNumericCellValue()-actual.getNumberValue());
+//				double pctExpected = Math.abs(0.00001*expected.getNumericCellValue());
+//				assertTrue(msg, delta <= pctExpected);
+				break;
 			case HSSFCell.CELL_TYPE_STRING:
-			    assertEquals(msg, HSSFCell.CELL_TYPE_STRING, actual.getCellType());
-			    assertEquals(msg, expected.getRichStringCellValue().getString(), actual.getRichTextStringValue().getString());
-			    break;
+				assertEquals(msg, HSSFCell.CELL_TYPE_STRING, actual.getCellType());
+				assertEquals(msg, expected.getRichStringCellValue().getString(), actual.getRichTextStringValue().getString());
+				break;
 		}
-    }
+	}
 
 
 	protected void setUp() throws Exception {
-        if (workbook == null) {
-        	String filePath = System.getProperty("HSSF.testdata.path")+ "/" + SS.FILENAME;
-            FileInputStream fin = new FileInputStream( filePath );
-            workbook = new HSSFWorkbook( fin );
-            sheet = workbook.getSheetAt( 0 );
-          }
-        _functionFailureCount = 0;
-        _functionSuccessCount = 0;
-        _evaluationFailureCount = 0;
-        _evaluationSuccessCount = 0;
-    }
-    
-    public void testFunctionsFromTestSpreadsheet() {
-        
-        processFunctionGroup(SS.START_OPERATORS_ROW_INDEX, null);
-        processFunctionGroup(SS.START_FUNCTIONS_ROW_INDEX, null);
-        // example for debugging individual functions/operators:
-//        processFunctionGroup(SS.START_OPERATORS_ROW_INDEX, "ConcatEval");
-//        processFunctionGroup(SS.START_FUNCTIONS_ROW_INDEX, "AVERAGE");
-        
-        // confirm results
-    	String successMsg = "There were " 
-    			+ _evaluationSuccessCount + " successful evaluation(s) and "
+		if (workbook == null) {
+			String filePath = System.getProperty("HSSF.testdata.path")+ "/" + SS.FILENAME;
+			FileInputStream fin = new FileInputStream( filePath );
+			workbook = new HSSFWorkbook( fin );
+			sheet = workbook.getSheetAt( 0 );
+		  }
+		_functionFailureCount = 0;
+		_functionSuccessCount = 0;
+		_evaluationFailureCount = 0;
+		_evaluationSuccessCount = 0;
+	}
+	
+	public void testFunctionsFromTestSpreadsheet() {
+		
+		processFunctionGroup(SS.START_OPERATORS_ROW_INDEX, null);
+		processFunctionGroup(SS.START_FUNCTIONS_ROW_INDEX, null);
+		// example for debugging individual functions/operators:
+//		processFunctionGroup(SS.START_OPERATORS_ROW_INDEX, "ConcatEval");
+//		processFunctionGroup(SS.START_FUNCTIONS_ROW_INDEX, "AVERAGE");
+		
+		// confirm results
+		String successMsg = "There were " 
+				+ _evaluationSuccessCount + " successful evaluation(s) and "
 				+ _functionSuccessCount + " function(s) without error";
  		if(_functionFailureCount > 0) {
 			String msg = _functionFailureCount + " function(s) failed in "
 			+ _evaluationFailureCount + " evaluation(s).  " + successMsg;
-        	throw new AssertionFailedError(msg);
-        }
+			throw new AssertionFailedError(msg);
+		}
  		if(false) { // normally no output for successful tests
  			System.out.println(getClass().getName() + ": " + successMsg);
  		}
 	}
 
-    /**
-     * @param startRowIndex row index in the spreadsheet where the first function/operator is found 
-     * @param testFocusFunctionName name of a single function/operator to test alone. 
-     * Typically pass <code>null</code> to test all functions
-     */
+	/**
+	 * @param startRowIndex row index in the spreadsheet where the first function/operator is found 
+	 * @param testFocusFunctionName name of a single function/operator to test alone. 
+	 * Typically pass <code>null</code> to test all functions
+	 */
 	private void processFunctionGroup(int startRowIndex, String testFocusFunctionName) {
  
 		HSSFFormulaEvaluator evaluator = new HSSFFormulaEvaluator(sheet, workbook);
 
-        int rowIndex = startRowIndex;
-        while (true) {
-            HSSFRow r = sheet.getRow(rowIndex);
-            String targetFunctionName = getTargetFunctionName(r);
-        	if(targetFunctionName == null) {
-        		throw new AssertionFailedError("Test spreadsheet cell empty on row (" 
-        				+ (rowIndex+1) + "). Expected function name or '"
-        				+ SS.FUNCTION_NAMES_END_SENTINEL + "'");
-        	}
-        	if(targetFunctionName.equals(SS.FUNCTION_NAMES_END_SENTINEL)) {
-        		// found end of functions list
-        		break;
-        	}
-        	if(testFocusFunctionName == null || targetFunctionName.equalsIgnoreCase(testFocusFunctionName)) {
-        		
-	        	// expected results are on the row below
-	            HSSFRow expectedValuesRow = sheet.getRow(rowIndex + 1);
-	            if(expectedValuesRow == null) {
-	            	int missingRowNum = rowIndex + 2; //+1 for 1-based, +1 for next row
-	            	throw new AssertionFailedError("Missing expected values row for function '" 
-	            			+ targetFunctionName + " (row " + missingRowNum + ")"); 
-	            }
-	            switch(processFunctionRow(evaluator, targetFunctionName, r, expectedValuesRow)) {
-	            	case Result.ALL_EVALUATIONS_SUCCEEDED: _functionSuccessCount++; break;
-	            	case Result.SOME_EVALUATIONS_FAILED: _functionFailureCount++; break;
-	            	default:
-	            		throw new RuntimeException("unexpected result");
-	            	case Result.NO_EVALUATIONS_FOUND: // do nothing
-	            }
-        	}
-            rowIndex += SS.NUMBER_OF_ROWS_PER_FUNCTION;
-        }
+		int rowIndex = startRowIndex;
+		while (true) {
+			HSSFRow r = sheet.getRow(rowIndex);
+			String targetFunctionName = getTargetFunctionName(r);
+			if(targetFunctionName == null) {
+				throw new AssertionFailedError("Test spreadsheet cell empty on row (" 
+						+ (rowIndex+1) + "). Expected function name or '"
+						+ SS.FUNCTION_NAMES_END_SENTINEL + "'");
+			}
+			if(targetFunctionName.equals(SS.FUNCTION_NAMES_END_SENTINEL)) {
+				// found end of functions list
+				break;
+			}
+			if(testFocusFunctionName == null || targetFunctionName.equalsIgnoreCase(testFocusFunctionName)) {
+				
+				// expected results are on the row below
+				HSSFRow expectedValuesRow = sheet.getRow(rowIndex + 1);
+				if(expectedValuesRow == null) {
+					int missingRowNum = rowIndex + 2; //+1 for 1-based, +1 for next row
+					throw new AssertionFailedError("Missing expected values row for function '" 
+							+ targetFunctionName + " (row " + missingRowNum + ")"); 
+				}
+				switch(processFunctionRow(evaluator, targetFunctionName, r, expectedValuesRow)) {
+					case Result.ALL_EVALUATIONS_SUCCEEDED: _functionSuccessCount++; break;
+					case Result.SOME_EVALUATIONS_FAILED: _functionFailureCount++; break;
+					default:
+						throw new RuntimeException("unexpected result");
+					case Result.NO_EVALUATIONS_FOUND: // do nothing
+				}
+			}
+			rowIndex += SS.NUMBER_OF_ROWS_PER_FUNCTION;
+		}
 	}
 
 	/**
@@ -236,16 +235,16 @@ public final class TestFormulasFromSpreadsheet extends TestCase {
 	 * @return a constant from the local Result class denoting whether there were any evaluation
 	 * cases, and whether they all succeeded.
 	 */
-    private int processFunctionRow(HSSFFormulaEvaluator evaluator, String targetFunctionName, 
-    		HSSFRow formulasRow, HSSFRow expectedValuesRow) {
-    	
-        int result = Result.NO_EVALUATIONS_FOUND; // so far
-        short endcolnum = formulasRow.getLastCellNum();
-        evaluator.setCurrentRow(formulasRow);
+	private int processFunctionRow(HSSFFormulaEvaluator evaluator, String targetFunctionName, 
+			HSSFRow formulasRow, HSSFRow expectedValuesRow) {
+		
+		int result = Result.NO_EVALUATIONS_FOUND; // so far
+		short endcolnum = formulasRow.getLastCellNum();
+		evaluator.setCurrentRow(formulasRow);
 
-        // iterate across the row for all the evaluation cases
-        for (short colnum=SS.COLUMN_INDEX_FIRST_TEST_VALUE; colnum < endcolnum; colnum++) {
-            HSSFCell c = formulasRow.getCell(colnum);
+		// iterate across the row for all the evaluation cases
+		for (short colnum=SS.COLUMN_INDEX_FIRST_TEST_VALUE; colnum < endcolnum; colnum++) {
+			HSSFCell c = formulasRow.getCell(colnum);
 			if (c == null || c.getCellType() != HSSFCell.CELL_TYPE_FORMULA) {
 				continue;
 			}
@@ -265,13 +264,13 @@ public final class TestFormulasFromSpreadsheet extends TestCase {
 				printShortStackTrace(System.err, e);
 				result = Result.SOME_EVALUATIONS_FAILED;
 			}
-        }
+		}
  		return result;
 	}
 
-    /**
-     * Useful to keep output concise when expecting many failures to be reported by this test case
-     */
+	/**
+	 * Useful to keep output concise when expecting many failures to be reported by this test case
+	 */
 	private static void printShortStackTrace(PrintStream ps, AssertionFailedError e) {
 		StackTraceElement[] stes = e.getStackTrace();
 		
@@ -304,8 +303,8 @@ public final class TestFormulasFromSpreadsheet extends TestCase {
 	}
 
 	/**
-     * @return <code>null</code> if cell is missing, empty or blank
-     */
+	 * @return <code>null</code> if cell is missing, empty or blank
+	 */
 	private static String getTargetFunctionName(HSSFRow r) {
 		if(r == null) {
 			System.err.println("Warning - given null row, can't figure out function name");
