229db44d72aa3268ff2154361fa41ae8ad87ab49
Nick Burch <nick@apache.org>
2008-03-16 17:23:24 +0000

    More create helper stuff, and some sample formatting files
    git-svn-id: https://svn.apache.org/repos/asf/poi/branches/ooxml@637614 13f79535-47bb-0310-9956-ffa450edef68

--- src/examples/src/org/apache/poi/ss/usermodel/examples/FromQuickGuide.java
+++ src/examples/src/org/apache/poi/ss/usermodel/examples/FromQuickGuide.java
@@ -18,9 +18,13 @@ package org.apache.poi.ss.usermodel.examples;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Date;
 
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.CreationHelper;
+import org.apache.poi.ss.usermodel.DataFormat;
 import org.apache.poi.ss.usermodel.RichTextString;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
@@ -71,6 +75,7 @@ public class FromQuickGuide {
 		
 		for (int i = 0; i < wbs.length; i++) {
 			Workbook wb = wbs[i];
+		    CreationHelper createHelper = wb.getCreationHelper();
 		    Sheet sheet = wb.createSheet("new sheet");
 
 		    // Create a row and put some cells in it. Rows are 0 based.
@@ -82,7 +87,7 @@ public class FromQuickGuide {
 		    // Or do it on one line.
 		    row.createCell((short)1).setCellValue(1.2);
 		    row.createCell((short)2).setCellValue(
-		    		cell.createRichTextString("This is a string"));
+		    		createHelper.createRichTextString("This is a string"));
 		    row.createCell((short)3).setCellValue(true);
 
 		    // Write the output to a file
@@ -90,5 +95,34 @@ public class FromQuickGuide {
 		    wb.write(fileOut);
 		    fileOut.close();
 		}
+	}
+	
+	public void newDateCells() throws IOException {
+	    Workbook wb = new HSSFWorkbook();
+	    //Workbook wb = new XSSFWorkbook();
+	    CreationHelper createHelper = wb.getCreationHelper();
+	    Sheet sheet = wb.createSheet("new sheet");
+
+	    // Create a row and put some cells in it. Rows are 0 based.
+	    Row row = sheet.createRow((short)0);
+		
+	    // Create a cell and put a date value in it.  The first cell is not styled
+	    // as a date.
+	    Cell cell = row.createCell((short)0);
+	    cell.setCellValue(new Date());
+
+	    // we style the second cell as a date (and time).  It is important to
+	    // create a new cell style from the workbook otherwise you can end up
+	    // modifying the built in style and effecting not only this cell but other cells.
+	    CellStyle cellStyle = wb.createCellStyle();
+	    cellStyle.setDataFormat(createHelper.createDataFormat().getFormat("m/d/yy h:mm"));
+	    cell = row.createCell((short)1);
+	    cell.setCellValue(new Date());
+	    cell.setCellStyle(cellStyle);
+
+	    // Write the output to a file
+	    FileOutputStream fileOut = new FileOutputStream("workbook.xls");
+	    wb.write(fileOut);
+	    fileOut.close();
 	}
 }
