077fb6c6f91bc4f84e5a51e5f0bea426cbc8a2c1
Nick Burch <nick@apache.org>
2008-04-10 14:43:24 +0000

    Merged revisions 638786-638802,638805-638811,638813-638814,638816-639230,639233-639241,639243-639253,639255-639486,639488-639601,639603-639835,639837-639917,639919-640056,640058-640710,640712-641156,641158-641184,641186-641795,641797-641798,641800-641933,641935-641963,641965-641966,641968-641995,641997-642230,642232-642562,642564-642565,642568-642570,642572-642573,642576-642736,642739-642877,642879,642881-642890,642892-642903,642905-642945,642947-643624,643626-643653,643655-643669,643671,643673-643830,643832-643833,643835-644342,644344-644472,644474-644508,644510-645347,645349-645351,645353-645559,645561-645565,645568-645951,645953-646193,646195-646311,646313-646820 via svnmerge from https://svn.apache.org:443/repos/asf/poi/trunk
    ........
      r646405 | nick | 2008-04-09 16:36:39 +0100 (Wed, 09 Apr 2008) | 1 line
      
      Implement a proxy HSSFListener which tracks the format records, and lets you lookup the format string for a given cell. Convert the xls to csv example to use it
    ........
      r646666 | josh | 2008-04-10 08:06:55 +0100 (Thu, 10 Apr 2008) | 1 line
      
      bugzilla 44792 - fixed encode/decode problems in ExternalNameRecord and CRNRecord.
    ........
    
    
    git-svn-id: https://svn.apache.org/repos/asf/poi/branches/ooxml@646826 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/poi/hssf/record/constant/ErrorConstant.java
@@ -0,0 +1,64 @@
+/* ====================================================================
+   Licensed to the Apache Software Foundation (ASF) under one or more
+   contributor license agreements.  See the NOTICE file distributed with
+   this work for additional information regarding copyright ownership.
+   The ASF licenses this file to You under the Apache License, Version 2.0
+   (the "License"); you may not use this file except in compliance with
+   the License.  You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+==================================================================== */
+
+package org.apache.poi.hssf.record.constant;
+
+import org.apache.poi.hssf.usermodel.HSSFErrorConstants;
+/**
+ * Represents a constant error code value as encoded in a constant values array. <p/>
+ * 
+ * This class is a type-safe wrapper for a 16-bit int value performing a similar job to 
+ * <tt>ErrorEval</tt>.
+ * 
+ * @author Josh Micich
+ */
+public class ErrorConstant {
+	// convenient access to name space
+	private static final HSSFErrorConstants EC = null;
+
+	private static final ErrorConstant NULL = new ErrorConstant(EC.ERROR_NULL);
+	private static final ErrorConstant DIV_0 = new ErrorConstant(EC.ERROR_DIV_0);
+	private static final ErrorConstant VALUE = new ErrorConstant(EC.ERROR_VALUE);
+	private static final ErrorConstant REF = new ErrorConstant(EC.ERROR_REF);
+	private static final ErrorConstant NAME = new ErrorConstant(EC.ERROR_NAME);
+	private static final ErrorConstant NUM = new ErrorConstant(EC.ERROR_NUM);
+	private static final ErrorConstant NA = new ErrorConstant(EC.ERROR_NA);
+
+	private final int _errorCode;
+
+	private ErrorConstant(int errorCode) {
+		_errorCode = errorCode;
+	}
+	
+	public int getErrorCode() {
+		return _errorCode;
+	}
+
+	public static ErrorConstant valueOf(int errorCode) {
+		switch (errorCode) {
+    		case HSSFErrorConstants.ERROR_NULL:  return NULL;
+    		case HSSFErrorConstants.ERROR_DIV_0: return DIV_0;
+    		case HSSFErrorConstants.ERROR_VALUE: return VALUE;
+    		case HSSFErrorConstants.ERROR_REF:   return REF;
+    		case HSSFErrorConstants.ERROR_NAME:  return NAME;
+    		case HSSFErrorConstants.ERROR_NUM:   return NUM;
+    		case HSSFErrorConstants.ERROR_NA:    return NA;
+		}
+		System.err.println("Warning - unexpected error code (" + errorCode + ")");
+		return new ErrorConstant(errorCode);
+	}
+}
