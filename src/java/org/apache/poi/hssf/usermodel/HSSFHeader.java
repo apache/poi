074a69b42adb80534c803e2b639019a7e8fa4979
Nick Burch <nick@apache.org>
2008-08-14 20:50:06 +0000

    Refactor header/footer stuff to remove duplication
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@686036 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/poi/hssf/usermodel/HSSFHeader.java
+++ src/java/org/apache/poi/hssf/usermodel/HSSFHeader.java
@@ -32,78 +32,17 @@ import org.apache.poi.hssf.record.HeaderRecord;
  *
  * @author Shawn Laubach (slaubach at apache dot org)
  */
-public class HSSFHeader implements HeaderFooter
-{
-
-    HeaderRecord headerRecord;
-    String left;
-    String center;
-    String right;
+public class HSSFHeader extends HeaderFooter {
+	private HeaderRecord headerRecord;
 
     /**
      * Constructor.  Creates a new header interface from a header record
      *
      * @param headerRecord Header record to create the header with
      */
-    protected HSSFHeader( HeaderRecord headerRecord )
-    {
+    protected HSSFHeader( HeaderRecord headerRecord ) {
+    	super(headerRecord.getHeader());
         this.headerRecord = headerRecord;
-        String head = headerRecord.getHeader();
-        while ( head != null && head.length() > 1 )
-        {
-            int pos = head.length();
-            switch ( head.substring( 1, 2 ).charAt( 0 ) )
-            {
-                case 'L':
-                    if ( head.indexOf( "&C" ) >= 0 )
-                    {
-                        pos = Math.min( pos, head.indexOf( "&C" ) );
-                    }
-                    if ( head.indexOf( "&R" ) >= 0 )
-                    {
-                        pos = Math.min( pos, head.indexOf( "&R" ) );
-                    }
-                    left = head.substring( 2, pos );
-                    head = head.substring( pos );
-                    break;
-                case 'C':
-                    if ( head.indexOf( "&L" ) >= 0 )
-                    {
-                        pos = Math.min( pos, head.indexOf( "&L" ) );
-                    }
-                    if ( head.indexOf( "&R" ) >= 0 )
-                    {
-                        pos = Math.min( pos, head.indexOf( "&R" ) );
-                    }
-                    center = head.substring( 2, pos );
-                    head = head.substring( pos );
-                    break;
-                case 'R':
-                    if ( head.indexOf( "&C" ) >= 0 )
-                    {
-                        pos = Math.min( pos, head.indexOf( "&C" ) );
-                    }
-                    if ( head.indexOf( "&L" ) >= 0 )
-                    {
-                        pos = Math.min( pos, head.indexOf( "&L" ) );
-                    }
-                    right = head.substring( 2, pos );
-                    head = head.substring( pos );
-                    break;
-                default :
-                    head = null;
-            }
-        }
-    }
-
-    /**
-     * Get the left side of the header.
-     *
-     * @return The string representing the left side.
-     */
-    public String getLeft()
-    {
-        return left;
     }
 
     /**
@@ -118,16 +57,6 @@ public class HSSFHeader implements HeaderFooter
     }
 
     /**
-     * Get the center of the header.
-     *
-     * @return The string representing the center.
-     */
-    public String getCenter()
-    {
-        return center;
-    }
-
-    /**
      * Sets the center string.
      *
      * @param newCenter The string to set as the center.
@@ -136,16 +65,6 @@ public class HSSFHeader implements HeaderFooter
     {
         center = newCenter;
         createHeaderString();
-    }
-
-    /**
-     * Get the right side of the header.
-     *
-     * @return The string representing the right side.
-     */
-    public String getRight()
-    {
-        return right;
     }
 
     /**
@@ -171,127 +90,5 @@ public class HSSFHeader implements HeaderFooter
         headerRecord.setHeaderLength( (byte) headerRecord.getHeader().length() );
     }
 
-    /**
-     * Returns the string that represents the change in font size.
-     *
-     * @param size the new font size
-     * @return The special string to represent a new font size
-     */
-    public static String fontSize( short size )
-    {
-        return "&" + size;
-    }
-
-    /**
-     * Returns the string that represents the change in font.
-     *
-     * @param font  the new font
-     * @param style the fonts style
-     * @return The special string to represent a new font size
-     */
-    public static String font( String font, String style )
-    {
-        return "&\"" + font + "," + style + "\"";
-    }
-
-    /**
-     * Returns the string representing the current page number
-     *
-     * @return The special string for page number
-     */
-    public static String page()
-    {
-        return "&P";
-    }
-
-    /**
-     * Returns the string representing the number of pages.
-     *
-     * @return The special string for the number of pages
-     */
-    public static String numPages()
-    {
-        return "&N";
-    }
-
-    /**
-     * Returns the string representing the current date
-     *
-     * @return The special string for the date
-     */
-    public static String date()
-    {
-        return "&D";
-    }
-
-    /**
-     * Returns the string representing the current time
-     *
-     * @return The special string for the time
-     */
-    public static String time()
-    {
-        return "&T";
-    }
-
-    /**
-     * Returns the string representing the current file name
-     *
-     * @return The special string for the file name
-     */
-    public static String file()
-    {
-        return "&F";
-    }
-
-    /**
-     * Returns the string representing the current tab (sheet) name
-     *
-     * @return The special string for tab name
-     */
-    public static String tab()
-    {
-        return "&A";
-    }
-
-    /**
-     * Returns the string representing the start underline
-     *
-     * @return The special string for start underline
-     */
-    public static String startUnderline()
-    {
-        return "&U";
-    }
-
-    /**
-     * Returns the string representing the end underline
-     *
-     * @return The special string for end underline
-     */
-    public static String endUnderline()
-    {
-        return "&U";
-    }
-
-    /**
-     * Returns the string representing the start double underline
-     *
-     * @return The special string for start double underline
-     */
-    public static String startDoubleUnderline()
-    {
-        return "&E";
-    }
-
-    /**
-     * Returns the string representing the end double underline
-     *
-     * @return The special string for end double underline
-     */
-    public static String endDoubleUnderline()
-    {
-        return "&E";
-    }
 }
 
