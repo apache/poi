2dd98d377e128fffcd25157c714d324a1118cc00
Josh Micich <josh@apache.org>
2008-08-23 22:47:51 +0000

    Fix for bug 45672 - prevent MissingRecordAwareHSSFListener generating multiple LastCellOfRowDummyRecords when shared formulas are present
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@688426 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/poi/hssf/eventusermodel/MissingRecordAwareHSSFListener.java
+++ src/java/org/apache/poi/hssf/eventusermodel/MissingRecordAwareHSSFListener.java
@@ -17,22 +17,15 @@
 
 package org.apache.poi.hssf.eventusermodel;
 
-import org.apache.poi.hssf.eventusermodel.HSSFListener;
 import org.apache.poi.hssf.eventusermodel.dummyrecord.LastCellOfRowDummyRecord;
 import org.apache.poi.hssf.eventusermodel.dummyrecord.MissingCellDummyRecord;
 import org.apache.poi.hssf.eventusermodel.dummyrecord.MissingRowDummyRecord;
 import org.apache.poi.hssf.record.BOFRecord;
-import org.apache.poi.hssf.record.BlankRecord;
-import org.apache.poi.hssf.record.BoolErrRecord;
-import org.apache.poi.hssf.record.BoundSheetRecord;
-import org.apache.poi.hssf.record.FormulaRecord;
-import org.apache.poi.hssf.record.LabelRecord;
-import org.apache.poi.hssf.record.LabelSSTRecord;
+import org.apache.poi.hssf.record.CellValueRecordInterface;
 import org.apache.poi.hssf.record.NoteRecord;
-import org.apache.poi.hssf.record.NumberRecord;
-import org.apache.poi.hssf.record.RKRecord;
 import org.apache.poi.hssf.record.Record;
 import org.apache.poi.hssf.record.RowRecord;
+import org.apache.poi.hssf.record.SharedFormulaRecord;
 
 /**
  * <p>A HSSFListener which tracks rows and columns, and will
@@ -44,16 +37,16 @@ import org.apache.poi.hssf.record.RowRecord;
  *  file, or was skipped from being written as it was
  *  blank.
  */
-public class MissingRecordAwareHSSFListener implements HSSFListener {
+public final class MissingRecordAwareHSSFListener implements HSSFListener {
 	private HSSFListener childListener;
 	
 	// Need to have different counters for cell rows and
 	//  row rows, as you sometimes get a RowRecord in the
 	//  middle of some cells, and that'd break everything
-	private int lastRowRow = -1;
+	private int lastRowRow;
 	
-	private int lastCellRow = -1;
-	private int lastCellColumn = -1;
+	private int lastCellRow;
+	private int lastCellColumn;
 	
 	/**
 	 * Constructs a new MissingRecordAwareHSSFListener, which
@@ -62,128 +55,80 @@ public class MissingRecordAwareHSSFListener implements HSSFListener {
 	 * @param listener The HSSFListener to pass records on to
 	 */
 	public MissingRecordAwareHSSFListener(HSSFListener listener) {
+		resetCounts();
 		childListener = listener;
 	}
 
 	public void processRecord(Record record) {
-		int thisRow = -1;
-		int thisColumn = -1;
-		
-		switch (record.getSid())
-        {
-            // the BOFRecord can represent either the beginning of a sheet or the workbook
-            case BOFRecord.sid:
-                BOFRecord bof = (BOFRecord) record;
-                if (bof.getType() == bof.TYPE_WORKBOOK)
-                {
-                	// Reset the row and column counts - new workbook
-                	lastRowRow = -1;
-                	lastCellRow = -1;
-                	lastCellColumn = -1;
-                    //System.out.println("Encountered workbook");
-                } else if (bof.getType() == bof.TYPE_WORKSHEET)
-                {
-                	// Reset the row and column counts - new sheet
-                	lastRowRow = -1;
-                	lastCellRow = -1;
-                	lastCellColumn = -1;
-                    //System.out.println("Encountered sheet reference");
-                }
-                break;
-            case BoundSheetRecord.sid:
-                BoundSheetRecord bsr = (BoundSheetRecord) record;
-                //System.out.println("New sheet named: " + bsr.getSheetname());
-                break;
-            case RowRecord.sid:
-                RowRecord rowrec = (RowRecord) record;
-                //System.out.println("Row " + rowrec.getRowNumber() + " found, first column at "
-                //        + rowrec.getFirstCol() + " last column at " + rowrec.getLastCol());
-                
-                // If there's a jump in rows, fire off missing row records
-                if(lastRowRow+1 < rowrec.getRowNumber()) {
-                	for(int i=(lastRowRow+1); i<rowrec.getRowNumber(); i++) {
-                		MissingRowDummyRecord dr = new MissingRowDummyRecord(i);
-                		childListener.processRecord(dr);
-                	}
-                }
-                
-                // Record this as the last row we saw
-                lastRowRow = rowrec.getRowNumber();
-                break;
-                
-                
-            // These are all the "cell" records
-                
-            case BlankRecord.sid:
-            	BlankRecord brec = (BlankRecord) record;
-                thisRow = brec.getRow();
-                thisColumn = brec.getColumn();
-                break;
-            case BoolErrRecord.sid:
-            	BoolErrRecord berec = (BoolErrRecord) record;
-                thisRow = berec.getRow();
-                thisColumn = berec.getColumn();
-                break;
-            case FormulaRecord.sid:
-            	FormulaRecord frec = (FormulaRecord) record;
-            	thisRow = frec.getRow();
-            	thisColumn = frec.getColumn();
-                break;
-            case LabelRecord.sid:
-            	LabelRecord lrec = (LabelRecord) record;
-                thisRow = lrec.getRow();
-                thisColumn = lrec.getColumn();
-                //System.out.println("Cell found containing String "
-                //        + " at row " + lrec.getRow() + " and column " + lrec.getColumn());
-                break;
-            case LabelSSTRecord.sid:
-            	LabelSSTRecord lsrec = (LabelSSTRecord) record;
-                thisRow = lsrec.getRow();
-                thisColumn = lsrec.getColumn();
-                //System.out.println("Cell found containing String "
-                //        + " at row " + lsrec.getRow() + " and column " + lsrec.getColumn());
-                break;
-            case NoteRecord.sid:
-            	NoteRecord nrec = (NoteRecord) record;
-            	thisRow = nrec.getRow();
-            	thisColumn = nrec.getColumn();
-                break;
-            case NumberRecord.sid:
-                NumberRecord numrec = (NumberRecord) record;
-                thisRow = numrec.getRow();
-                thisColumn = numrec.getColumn();
-                //System.out.println("Cell found with value " + numrec.getValue()
-                //        + " at row " + numrec.getRow() + " and column " + numrec.getColumn());
-                break;
-            case RKRecord.sid:
-            	RKRecord rkrec = (RKRecord) record;
-            	thisRow = rkrec.getRow();
-            	thisColumn = rkrec.getColumn();
-                break;
-            default:
-            	//System.out.println(record.getClass());
-            	break;
-        }
-		
+		int thisRow;
+		int thisColumn;
+
+
+		if (record instanceof CellValueRecordInterface) {
+			CellValueRecordInterface valueRec = (CellValueRecordInterface) record;
+			thisRow = valueRec.getRow();
+			thisColumn = valueRec.getColumn();
+		} else {
+			thisRow = -1;
+			thisColumn = -1;
+
+			switch (record.getSid()) {
+				// the BOFRecord can represent either the beginning of a sheet or
+				// the workbook
+				case BOFRecord.sid:
+					BOFRecord bof = (BOFRecord) record;
+					if (bof.getType() == bof.TYPE_WORKBOOK || bof.getType() == bof.TYPE_WORKSHEET) {
+						// Reset the row and column counts - new workbook / worksheet
+						resetCounts();
+					}
+					break;
+				case RowRecord.sid:
+					RowRecord rowrec = (RowRecord) record;
+					//System.out.println("Row " + rowrec.getRowNumber() + " found, first column at "
+					//        + rowrec.getFirstCol() + " last column at " + rowrec.getLastCol());
+
+					// If there's a jump in rows, fire off missing row records
+					if (lastRowRow + 1 < rowrec.getRowNumber()) {
+						for (int i = (lastRowRow + 1); i < rowrec.getRowNumber(); i++) {
+							MissingRowDummyRecord dr = new MissingRowDummyRecord(i);
+							childListener.processRecord(dr);
+						}
+					}
+
+					// Record this as the last row we saw
+					lastRowRow = rowrec.getRowNumber();
+					break;
+
+				case SharedFormulaRecord.sid:
+					// SharedFormulaRecord occurs after the first FormulaRecord of the cell range.
+					// There are probably (but not always) more cell records after this
+					// - so don't fire off the LastCellOfRowDummyRecord yet
+					childListener.processRecord(record);
+					return;
+				case NoteRecord.sid:
+					NoteRecord nrec = (NoteRecord) record;
+					thisRow = nrec.getRow();
+					thisColumn = nrec.getColumn();
+					break;
+			}
+		}
 		// If we're on cells, and this cell isn't in the same
 		//  row as the last one, then fire the 
-		//  dummy end-of-row records?
+		//  dummy end-of-row records
 		if(thisRow != lastCellRow && lastCellRow > -1) {
 			for(int i=lastCellRow; i<thisRow; i++) {
 				int cols = -1;
 				if(i == lastCellRow) {
 					cols = lastCellColumn;
 				}
-				LastCellOfRowDummyRecord r = new LastCellOfRowDummyRecord(i, cols);
-				childListener.processRecord(r);
+				childListener.processRecord(new LastCellOfRowDummyRecord(i, cols));
 			}
 		}
 		
 		// If we've just finished with the cells, then fire the
-		//  final dummy end-of-row record
+		// final dummy end-of-row record
 		if(lastCellRow != -1 && lastCellColumn != -1 && thisRow == -1) {
-			LastCellOfRowDummyRecord r = new LastCellOfRowDummyRecord(lastCellRow, lastCellColumn);
-			childListener.processRecord(r);
+			childListener.processRecord(new LastCellOfRowDummyRecord(lastCellRow, lastCellColumn));
 			
 			lastCellRow = -1;
 			lastCellColumn = -1;
@@ -196,11 +141,10 @@ public class MissingRecordAwareHSSFListener implements HSSFListener {
 		}
 		
 		// If there's a gap in the cells, then fire
-		//  the dummy cell records?
-		if(lastCellColumn != (thisColumn-1)) {
+		//  the dummy cell records
+		if(lastCellColumn != thisColumn-1) {
 			for(int i=lastCellColumn+1; i<thisColumn; i++) {
-				MissingCellDummyRecord r = new MissingCellDummyRecord(thisRow, i);
-				childListener.processRecord(r);
+				childListener.processRecord(new MissingCellDummyRecord(thisRow, i));
 			}
 		}
 		
@@ -209,7 +153,13 @@ public class MissingRecordAwareHSSFListener implements HSSFListener {
 			lastCellColumn = thisColumn;
 			lastCellRow = thisRow;
 		}
-		
+
 		childListener.processRecord(record);
+	}
+
+	private void resetCounts() {
+		lastRowRow = -1;
+		lastCellRow = -1;
+		lastCellColumn = -1;
 	}
 }
