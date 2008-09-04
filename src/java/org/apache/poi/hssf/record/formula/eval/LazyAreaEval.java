176250c649ba0ca6e35ee7cd6135468cc863a259
Josh Micich <josh@apache.org>
2008-09-04 20:58:37 +0000

    Fixed 2 small bugs in RelationalOperationEval (added junits).  Refactored hierarchy.
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@692239 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/poi/hssf/record/formula/eval/LazyAreaEval.java
+++ src/java/org/apache/poi/hssf/record/formula/eval/LazyAreaEval.java
@@ -1,33 +1,35 @@
-/*
-* Licensed to the Apache Software Foundation (ASF) under one or more
-* contributor license agreements.  See the NOTICE file distributed with
-* this work for additional information regarding copyright ownership.
-* The ASF licenses this file to You under the Apache License, Version 2.0
-* (the "License"); you may not use this file except in compliance with
-* the License.  You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+/* ====================================================================
+   Licensed to the Apache Software Foundation (ASF) under one or more
+   contributor license agreements.  See the NOTICE file distributed with
+   this work for additional information regarding copyright ownership.
+   The ASF licenses this file to You under the Apache License, Version 2.0
+   (the "License"); you may not use this file except in compliance with
+   the License.  You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+==================================================================== */
 
 package org.apache.poi.hssf.record.formula.eval;
 
 import org.apache.poi.hssf.record.formula.AreaI;
 import org.apache.poi.hssf.record.formula.AreaI.OffsetArea;
+import org.apache.poi.hssf.usermodel.EvaluationCache;
 import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFFormulaEvaluator;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hssf.util.CellReference;
 
 /**
- * @author Amol S. Deshmukh &lt; amolweb at ya hoo dot com &gt;
- * 
+ *
+ * @author Josh Micich 
  */
 public final class LazyAreaEval extends AreaEvalBase {
 
@@ -61,5 +63,19 @@ public final class LazyAreaEval extends AreaEvalBase {
 				relFirstRowIx, relLastRowIx, relFirstColIx, relLastColIx);
 
 		return new LazyAreaEval(area, _sheet, _workbook);
+	}
+	public String toString() {
+		CellReference crA = new CellReference(getFirstRow(), getFirstColumn());
+		CellReference crB = new CellReference(getLastRow(), getLastColumn());
+		StringBuffer sb = new StringBuffer();
+		sb.append(getClass().getName()).append("[");
+		String sheetName = _workbook.getSheetName(_workbook.getSheetIndex(_sheet));
+		sb.append(sheetName);
+		sb.append('!');
+		sb.append(crA.formatAsString());
+		sb.append(':');
+		sb.append(crB.formatAsString());
+		sb.append("]");
+		return sb.toString();
 	}
 }
