57625c349f02bc94293f8747891efa94272731d8
Nick Burch <nick@apache.org>
2008-09-07 18:32:51 +0000

    Patch from bug #45738 - Initial HWPF support for Office Art Shapes
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@692918 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/scratchpad/src/org/apache/poi/hwpf/model/ShapesTable.java
@@ -0,0 +1,54 @@
+/* ====================================================================
+   Licensed to the Apache Software Foundation (ASF) under one or more
+   contributor license agreements.  See the NOTICE file distributed with
+   this work for additional information regarding copyright ownership.
+   The ASF licenses this file to You under the Apache License, Version 2.0
+   (the "License"); you may not use this file except in compliance with
+   the License.  You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+==================================================================== */
+
+package org.apache.poi.hwpf.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.poi.hwpf.usermodel.Shape;
+
+public class ShapesTable {
+        private List _shapes;
+        private List _shapesVisibili;  //holds visible shapes
+
+        public ShapesTable(byte [] tblStream, FileInformationBlock fib) {
+                PlexOfCps binTable = new PlexOfCps(tblStream,
+                     fib.getFcPlcspaMom(), fib.getLcbPlcspaMom(), 26);
+
+                _shapes = new ArrayList();
+                _shapesVisibili = new ArrayList();
+
+
+                for(int i = 0; i < binTable.length(); i++) {
+                        GenericPropertyNode nodo = binTable.getProperty(i);
+
+                        Shape sh = new Shape(nodo);
+                        _shapes.add(sh);
+                        if(sh.isWithinDocument())
+                                _shapesVisibili.add(sh);
+                }
+        }
+
+        public List getAllShapes() {
+                return _shapes;
+        }
+
+        public List getVisibleShapes() {
+                return _shapesVisibili;
+        }
+}
---
+++ src/scratchpad/src/org/apache/poi/hwpf/usermodel/Shape.java
@@ -0,0 +1,74 @@
+/* ====================================================================
+   Licensed to the Apache Software Foundation (ASF) under one or more
+   contributor license agreements.  See the NOTICE file distributed with
+   this work for additional information regarding copyright ownership.
+   The ASF licenses this file to You under the Apache License, Version 2.0
+   (the "License"); you may not use this file except in compliance with
+   the License.  You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+==================================================================== */
+
+package org.apache.poi.hwpf.usermodel;
+
+import org.apache.poi.hwpf.model.GenericPropertyNode;
+import org.apache.poi.util.LittleEndian;
+
+public class Shape {
+        int _id, _left, _right, _top, _bottom;
+        /**
+         * true if the Shape bounds are within document (for
+         * example, it's false if the image left corner is outside the doc, like for
+         * embedded documents)
+         */
+        boolean _inDoc; 
+
+        public Shape(GenericPropertyNode nodo) {
+                byte [] contenuto = nodo.getBytes();
+                _id = LittleEndian.getInt(contenuto);
+                _left = LittleEndian.getInt(contenuto, 4);
+                _top = LittleEndian.getInt(contenuto, 8);
+                _right = LittleEndian.getInt(contenuto, 12);
+                _bottom = LittleEndian.getInt(contenuto, 16);
+                _inDoc = (_left >= 0 && _right >= 0 && _top >= 0 && _bottom >=
+0);
+        }
+
+        public int getId() {
+                return _id;
+        }
+
+        public int getLeft() {
+                return _left;
+        }
+
+        public int getRight() {
+                return _right;
+        }
+
+        public int getTop() {
+                return _top;
+        }
+
+        public int getBottom() {
+                return _bottom;
+        }
+
+        public int getWidth() {
+                return _right - _left + 1;
+        }
+
+        public int getHeight() {
+                return _bottom - _top + 1;
+        }
+
+        public boolean isWithinDocument() {
+                return _inDoc;
+        }
+}
