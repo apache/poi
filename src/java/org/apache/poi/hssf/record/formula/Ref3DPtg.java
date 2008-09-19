26b458016ebc77a3a474e5fe67756a4c1fac6589
Josh Micich <josh@apache.org>
2008-09-19 00:02:21 +0000

    code clean-up (removed compiler warnings/unused methods)
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@696860 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/poi/hssf/record/formula/Ref3DPtg.java
+++ src/java/org/apache/poi/hssf/record/formula/Ref3DPtg.java
@@ -18,7 +18,6 @@
 package org.apache.poi.hssf.record.formula;
 
 import org.apache.poi.hssf.record.RecordInputStream;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.hssf.util.CellReference;
 import org.apache.poi.ss.formula.WorkbookDependentFormula;
 import org.apache.poi.ss.formula.FormulaRenderingWorkbook;
@@ -67,9 +66,9 @@ public final class Ref3DPtg extends RefPtgBase implements WorkbookDependentFormu
     }
 
     public void writeBytes(byte [] array, int offset) {
-    	LittleEndian.putByte(array, 0 + offset, sid + getPtgClass());
-		LittleEndian.putUShort(array, 1 + offset, getExternSheetIndex());
-		writeCoordinates(array, offset + 3);
+        LittleEndian.putByte(array, 0 + offset, sid + getPtgClass());
+        LittleEndian.putUShort(array, 1 + offset, getExternSheetIndex());
+        writeCoordinates(array, offset + 3);
     }
 
     public int getSize() {
@@ -89,9 +88,9 @@ public final class Ref3DPtg extends RefPtgBase implements WorkbookDependentFormu
      * formulas. The sheet name will get properly delimited if required.
      */
     public String toFormulaString(FormulaRenderingWorkbook book) {
-		return ExternSheetNameResolver.prependSheetName(book, field_1_index_extern_sheet, formatReferenceAsString());
+        return ExternSheetNameResolver.prependSheetName(book, field_1_index_extern_sheet, formatReferenceAsString());
     }
-	public String toFormulaString() {
-		throw new RuntimeException("3D references need a workbook to determine formula text");
-	}
+    public String toFormulaString() {
+        throw new RuntimeException("3D references need a workbook to determine formula text");
+    }
 }
