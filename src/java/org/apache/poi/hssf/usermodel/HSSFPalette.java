0c6a2742599a3ad0c068a0564b780c510b68210b
Nick Burch <nick@apache.org>
2008-09-21 18:56:32 +0000

    Test to show that bug #45492 is invalid
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@697584 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/poi/hssf/usermodel/HSSFPalette.java
+++ src/java/org/apache/poi/hssf/usermodel/HSSFPalette.java
@@ -57,6 +57,15 @@ public class HSSFPalette
     	}
         return null;
     }
+    /**
+     * Retrieves the color at a given index
+     *
+     * @param index the palette index, between 0x8 to 0x40 inclusive
+     * @return the color, or null if the index is not populated
+     */
+    public HSSFColor getColor(int index) {
+    	return getColor((short)index);
+    }
     
     /**
      * Finds the first occurance of a given color
