7a8a9bf9e13e1d611af7a01d74cfaeb38c8d320a
Nick Burch <nick@apache.org>
2008-09-21 16:56:04 +0000

    Merged revisions 697520 via svnmerge from https://svn.apache.org/repos/asf/poi/trunk
    ........
      r697520 | yegor | 2008-09-21 15:14:34 +0100 (Sun, 21 Sep 2008) | 1 line
      
      patch from bug #45844: Addtional diagnostics for SlideShowRecordDumper
    ........
    
    
    git-svn-id: https://svn.apache.org/repos/asf/poi/branches/ooxml@697548 13f79535-47bb-0310-9956-ffa450edef68

--- src/scratchpad/src/org/apache/poi/hslf/record/TextCharsAtom.java
+++ src/scratchpad/src/org/apache/poi/hslf/record/TextCharsAtom.java
@@ -19,6 +19,7 @@
 
 package org.apache.poi.hslf.record;
 
+import org.apache.poi.util.HexDump;
 import org.apache.poi.util.LittleEndian;
 import org.apache.poi.util.StringUtil;
 import java.io.IOException;
@@ -96,5 +97,16 @@ public class TextCharsAtom extends RecordAtom
 
 		// Write out our text
 		out.write(_text);
+	}
+	
+	/**
+	 * dump debug info; use getText() to return a string 
+	 * representation of the atom
+	 */
+	public String toString() {
+        StringBuffer out = new StringBuffer();
+        out.append( "TextCharsAtom:\n");
+		out.append( HexDump.dump(_text, 0, 0) );
+		return out.toString();
 	}
 }
