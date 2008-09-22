ce92f612e743de3573969584f1dbb49c9118b152
Josh Micich <josh@apache.org>
2008-09-22 23:43:50 +0000

    Some clean-up in BoundSheetRecord
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@698039 13f79535-47bb-0310-9956-ffa450edef68

--- src/testcases/org/apache/poi/hssf/record/TestBoundSheetRecord.java
+++ src/testcases/org/apache/poi/hssf/record/TestBoundSheetRecord.java
@@ -1,4 +1,3 @@
-
 /* ====================================================================
    Licensed to the Apache Software Foundation (ASF) under one or more
    contributor license agreements.  See the NOTICE file distributed with
@@ -15,10 +14,12 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 ==================================================================== */
-        
 
 package org.apache.poi.hssf.record;
 
+import java.util.ArrayList;
+
+import junit.framework.AssertionFailedError;
 import junit.framework.TestCase;
 
 /**
@@ -28,53 +29,77 @@ import junit.framework.TestCase;
  *
  * @author Glen Stampoultzis (glens at apache.org)
  */
-public class TestBoundSheetRecord
-        extends TestCase
-{
-    public TestBoundSheetRecord( String s )
-    {
-        super( s );
+public final class TestBoundSheetRecord extends TestCase {
+
+
+    public void testRecordLength() {
+        BoundSheetRecord record = new BoundSheetRecord("Sheet1");
+        assertEquals(18, record.getRecordSize());
     }
 
-    public void testRecordLength()
-            throws Exception
-    {
-        BoundSheetRecord record = new BoundSheetRecord();
-        record.setCompressedUnicodeFlag((byte)0x00);
-        record.setSheetname("Sheet1");
-        record.setSheetnameLength((byte)6);
-
-        assertEquals(" 2  +  2  +  4  +   2   +    1     +    1    + len(str)", 18, record.getRecordSize());
+    public void testWideRecordLength() {
+        BoundSheetRecord record = new BoundSheetRecord("Sheet\u20ac");
+        assertEquals(24, record.getRecordSize());
     }
 
-    public void testWideRecordLength()
-            throws Exception
-    {
-        BoundSheetRecord record = new BoundSheetRecord();        
-        record.setSheetname("Sheet\u20ac");
-        record.setSheetnameLength((byte)6);
-
-        assertEquals(" 2  +  2  +  4  +   2   +    1     +    1    + len(str) * 2", 24, record.getRecordSize());
-    }
-    
     public void testName() {
-        BoundSheetRecord record = new BoundSheetRecord();
-        record.setSheetname("1234567890223456789032345678904");
-        assertTrue("Success", true);
+        BoundSheetRecord record = new BoundSheetRecord("1234567890223456789032345678904");
+
         try {
             record.setSheetname("12345678902234567890323456789042");
-            assertTrue("Should have thrown IllegalArgumentException, but didnt", false);
+            throw new AssertionFailedError("Should have thrown IllegalArgumentException, but didnt");
         } catch (IllegalArgumentException e) {
-            assertTrue("succefully threw exception",true);
+            // expected
         }
-        
+
         try {
             record.setSheetname("s//*s");
-            assertTrue("Should have thrown IllegalArgumentException, but didnt", false);
+            throw new AssertionFailedError("Should have thrown IllegalArgumentException, but didnt");
         } catch (IllegalArgumentException e) {
-            assertTrue("succefully threw exception",true);
+            // expected
         }
-            
     }
+    
+    public void testDeserializeUnicode() {
+       
+		byte[] data = {
+//			(byte)0x85, 0x00,       // sid
+//			0x1a, 0x00, 			// length
+			0x3C, 0x09, 0x00, 0x00, // bof
+			0x00, 0x00, 			// flags
+			0x09, 					// len( str )
+			0x01, 					// unicode
+			// <str>
+			0x21, 0x04, 0x42, 0x04, 0x40, 0x04, 
+			0x30, 0x04, 0x3D, 0x04, 0x38, 0x04,
+			0x47, 0x04, 0x3A, 0x04, 0x30, 0x04
+			// </str>
+		};
+	
+		RecordInputStream in = new TestcaseRecordInputStream(BoundSheetRecord.sid, data);
+		BoundSheetRecord bsr = new BoundSheetRecord(in);
+		// sheet name is unicode Russian for 'minor page'
+		assertEquals("\u0421\u0442\u0440\u0430\u043D\u0438\u0447\u043A\u0430", bsr.getSheetname());
+		
+	}
 
+    public void testOrdering() {
+    	BoundSheetRecord bs1 = new BoundSheetRecord("SheetB");
+    	BoundSheetRecord bs2 = new BoundSheetRecord("SheetC");
+    	BoundSheetRecord bs3 = new BoundSheetRecord("SheetA");
+    	bs1.setPositionOfBof(11);
+    	bs2.setPositionOfBof(33);
+    	bs3.setPositionOfBof(22);
+
+    	ArrayList l = new ArrayList();
+    	l.add(bs1);
+    	l.add(bs2);
+    	l.add(bs3);
+
+    	BoundSheetRecord[] r = BoundSheetRecord.orderByBofPosition(l);
+    	assertEquals(3, r.length);
+    	assertEquals(bs1, r[0]);
+    	assertEquals(bs3, r[1]);
+    	assertEquals(bs2, r[2]);
+    }
 }
