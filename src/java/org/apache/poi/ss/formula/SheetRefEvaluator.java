2a9b94d931e7225a8dcb2b9686f6c86053d4ac62
Josh Micich <josh@apache.org>
2008-09-23 00:40:22 +0000

    Optimised the FormulaEvaluator to take cell dependencies into account
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@698047 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/poi/ss/formula/SheetRefEvaluator.java
+++ src/java/org/apache/poi/ss/formula/SheetRefEvaluator.java
@@ -17,26 +17,33 @@
 
 package org.apache.poi.ss.formula;
 
-import org.apache.poi.hssf.record.formula.NamePtg;
-import org.apache.poi.hssf.record.formula.NameXPtg;
-import org.apache.poi.hssf.record.formula.Ptg;
-import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.record.formula.eval.ValueEval;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 /**
- * Abstracts a workbook for the purpose of formula evaluation.<br/>
  * 
- * For POI internal use only
  * 
  * @author Josh Micich
  */
-public interface EvaluationWorkbook {
-	String getSheetName(int sheetIndex);
-	int getSheetIndex(HSSFSheet sheet);
+final class SheetRefEvaluator {
 
-	HSSFSheet getSheet(int sheetIndex);
+	private final WorkbookEvaluator _bookEvaluator;
+	private final EvaluationTracker _tracker;
+	private final HSSFSheet _sheet;
+	private final int _sheetIndex;
 
-	int convertFromExternSheetIndex(int externSheetIndex);
-	EvaluationName getName(NamePtg namePtg);
-	String resolveNameXText(NameXPtg ptg);
-	Ptg[] getFormulaTokens(HSSFCell cell);
+	public SheetRefEvaluator(WorkbookEvaluator bookEvaluator, EvaluationTracker tracker,
+			EvaluationWorkbook _workbook, int sheetIndex) {
+		_bookEvaluator = bookEvaluator;
+		_tracker = tracker;
+		_sheet = _workbook.getSheet(sheetIndex);
+		_sheetIndex = sheetIndex;
+	}
+
+	public String getSheetName() {
+		return _bookEvaluator.getSheetName(_sheetIndex);
+	}
+
+	public ValueEval getEvalForCell(int rowIndex, int columnIndex) {
+		return _bookEvaluator.evaluateReference(_sheet, _sheetIndex, rowIndex, columnIndex, _tracker);
+	}
 }
