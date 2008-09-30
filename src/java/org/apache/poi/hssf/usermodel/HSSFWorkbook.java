2c0c9b6d8bf02cd9b41cf6b1d187f53c7afcc6b5
Yegor Kozlov <yegor@apache.org>
2008-09-30 14:32:37 +0000

    reverted the change made in r693085 , see bug #45859
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@700479 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/poi/hssf/usermodel/HSSFWorkbook.java
+++ src/java/org/apache/poi/hssf/usermodel/HSSFWorkbook.java
@@ -686,8 +686,8 @@ public class HSSFWorkbook extends POIDocument
             HSSFName newName = new HSSFName(this, newNameRecord);
             names.add(newName);
 
+            workbook.cloneDrawings(clonedSheet.getSheet());
         }
-        workbook.cloneDrawings(clonedSheet.getSheet());
         // TODO - maybe same logic required for other/all built-in name records
         
         return clonedSheet;
