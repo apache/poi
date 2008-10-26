d5d23c0274369ae40c5dadf31017016eaefd7a75
Josh Micich <josh@apache.org>
2008-10-26 08:17:06 +0000

    Bugzilla 45966 - added implementation for FIND function (patch from Torstein Tauno Svendsen).
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@707953 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/poi/hssf/record/formula/functions/Find.java
+++ src/java/org/apache/poi/hssf/record/formula/functions/Find.java
@@ -1,25 +1,65 @@
-/*
-* Licensed to the Apache Software Foundation (ASF) under one or more
-* contributor license agreements.  See the NOTICE file distributed with
-* this work for additional information regarding copyright ownership.
-* The ASF licenses this file to You under the Apache License, Version 2.0
-* (the "License"); you may not use this file except in compliance with
-* the License.  You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
-/*
- * Created on May 15, 2005
- *
- */
+/* ====================================================================
+   Licensed to the Apache Software Foundation (ASF) under one or more
+   contributor license agreements.  See the NOTICE file distributed with
+   this work for additional information regarding copyright ownership.
+   The ASF licenses this file to You under the Apache License, Version 2.0
+   (the "License"); you may not use this file except in compliance with
+   the License.  You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+==================================================================== */
+
 package org.apache.poi.hssf.record.formula.functions;
 
-public class Find extends NotImplementedFunction {
+import org.apache.poi.hssf.record.formula.eval.Eval;
+import org.apache.poi.hssf.record.formula.eval.ErrorEval;
+import org.apache.poi.hssf.record.formula.eval.ValueEval;
+import org.apache.poi.hssf.record.formula.eval.NumberEval;
+import org.apache.poi.hssf.record.formula.eval.EvaluationException;
 
+/**
+ * Implementation of the FIND() function.<p/>
+ *
+ * <b>Syntax</b>:<br/>
+ * <b>FIND</b>(<b>find_text</b>, <b>within_text</b>, start_num)<p/>
+ *
+ * FIND returns the character position of the first occurrence of <tt>find_text</tt> inside 
+ * <tt>within_text</tt>.  The third parameter, <tt>start_num</tt>, is optional (default=1)
+ * and specifies where to start searching from.  Character positions are 1-based.<p/>
+ *
+ * @author Torstein Tauno Svendsen (torstei@officenet.no)
+ */
+public class Find extends TextFunction {
+
+	protected ValueEval evaluateFunc(Eval[] args, int srcCellRow, short srcCellCol)
+			throws EvaluationException {
+
+		int nArgs = args.length;
+		if (nArgs < 2 || nArgs > 3) {
+			return ErrorEval.VALUE_INVALID;
+		}
+		String needle = evaluateStringArg(args[0], srcCellRow, srcCellCol);
+		String haystack = evaluateStringArg(args[1], srcCellRow, srcCellCol);
+		int startpos;
+		if (nArgs == 3) {
+			startpos = evaluateIntArg(args[2], srcCellRow, srcCellCol);
+			if (startpos <= 0) {
+				return ErrorEval.VALUE_INVALID;
+			}
+			startpos--; // convert 1-based to zero based
+		} else {
+			startpos = 0;
+		}
+		int result = haystack.indexOf(needle, startpos);
+		if (result == -1) {
+			return ErrorEval.VALUE_INVALID;
+		}
+		return new NumberEval(result + 1);
+	}
 }
