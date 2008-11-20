e9fba3a9f6a4c3b55a36d390de537742e25aa4e2
Yegor Kozlov <yegor@apache.org>
2008-11-20 15:08:42 +0000

    improved tasks for making Maven artifacts1. maven-dist is now obsolete, maven artifacts are taken from dist2. release manager should use 'mvn deploy:deploy-file' to deploy POI artifacts, dist/mvn-deploy.sh is automatically generated by the maven-poms task3. each poi module now has its own pom template in the ./maven directory
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@719249 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven/mvn-deploy.sh
@@ -0,0 +1,46 @@
+#! /bin/sh
+#
+#   Licensed to the Apache Software Foundation (ASF) under one or more
+#   contributor license agreements.  See the NOTICE file distributed with
+#   this work for additional information regarding copyright ownership.
+#   The ASF licenses this file to You under the Apache License, Version 2.0
+#   (the "License"); you may not use this file except in compliance with
+#   the License.  You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# Shell script to deploy POI artifacts in a maven repository.
+#
+#  Note, You should configure your settings.xml and add a server with id=apache-releases:
+#
+#    <server>
+#      <id>apache-releases</id>
+#      <username>apacheId</username>
+#      <privateKey>/path/to/private/key</privateKey>
+#    </server>
+#
+#  Usage:
+#   1. ant dist
+#   2. cd dist
+#   3. ./mvn-deploy.sh 
+# @author Yegor Kozlov
+
+mvn deploy:deploy-file -DrepositoryId=apache-releases \
+  -Durl=@REPOSITORY@ \
+  -Dfile=poi-@VERSION@-@DSTAMP@.jar -DpomFile=poi-@VERSION@.pom
+mvn deploy:deploy-file -DrepositoryId=apache-releases \
+  -Durl=@REPOSITORY@ \
+  -Dfile=poi-scratchpad-@VERSION@-@DSTAMP@.jar -DpomFile=poi-scratchpad-@VERSION@.pom
+mvn deploy:deploy-file -DrepositoryId=apache-releases \
+  -Durl=@REPOSITORY@ \
+  -Dfile=poi-contrib-@VERSION@-@DSTAMP@.jar -DpomFile=poi-contrib-@VERSION@.pom
+mvn deploy:deploy-file -DrepositoryId=apache-releases \
+  -Durl=@REPOSITORY@ \
+  -Dfile=poi-ooxml-@VERSION@-@DSTAMP@.jar -DpomFile=poi-ooxml-@VERSION@.pom
+
