95afcef9e6ae7728b1629c54bc6acc96e0674454
Paolo Mottadelli <paolo@apache.org>
2009-08-18 14:08:09 +0000

    createHeader/Footer methods + tests
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@805422 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/ooxml/testcases/org/apache/poi/xwpf/usermodel/TestXWPFHeader.java
@@ -0,0 +1,77 @@
+/* ====================================================================
+   Licensed to the Apache Software Foundation (ASF) under one or more
+   contributor license agreements.  See the NOTICE file distributed with
+   this work for additional information regarding copyright ownership.
+   The ASF licenses this file to You under the Apache License, Version 2.0
+   (the "License"); you may not use this file except in compliance with
+   the License.  You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+==================================================================== */
+package org.apache.poi.xwpf.usermodel;
+
+import java.io.File;
+import java.io.IOException;
+
+import junit.framework.TestCase;
+
+import org.apache.poi.POIXMLDocument;
+import org.apache.poi.xwpf.model.XWPFHeaderFooterPolicy;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTHdrFtr;
+
+public class TestXWPFHeader extends TestCase {
+	
+	public void testSimpleHeader() throws IOException {
+		File sampleFile = new File(
+				System.getProperty("HWPF.testdata.path") +
+				File.separator + "headerFooter.docx"
+		);
+		assertTrue(sampleFile.exists());
+		XWPFDocument sampleDoc;
+		sampleDoc = new XWPFDocument(
+				POIXMLDocument.openPackage(sampleFile.toString())
+		);
+		
+		XWPFHeaderFooterPolicy policy = sampleDoc.getHeaderFooterPolicy();
+		
+		
+		XWPFHeader header = policy.getDefaultHeader();
+		XWPFFooter footer = policy.getDefaultFooter();
+		assertNotNull(header);
+		assertNotNull(footer);
+		
+		// TODO verify if the following is correct
+		assertNull(header.toString());
+		
+	}
+	
+	public void testSetHeader() throws IOException {
+		File sampleFile = new File(
+				System.getProperty("HWPF.testdata.path") +
+				File.separator + "sampleDoc.docx"
+		);
+		assertTrue(sampleFile.exists());
+		XWPFDocument sampleDoc;
+		sampleDoc = new XWPFDocument(
+				POIXMLDocument.openPackage(sampleFile.toString())
+		);
+		// no header is set (yet)
+		XWPFHeaderFooterPolicy policy = sampleDoc.getHeaderFooterPolicy();
+		assertNull(policy.getDefaultHeader());
+		// set a default header and test it is not null
+		policy.createHeader(policy.DEFAULT);
+		policy.createHeader(policy.FIRST);
+		policy.createFooter(policy.DEFAULT);
+		
+		assertNotNull(policy.getDefaultHeader());
+		assertNotNull(policy.getFirstPageHeader());
+		assertNotNull(policy.getDefaultFooter());
+	}
+
+}
