c8f135b59bd77d513d86063ee004d90eb35a2a43
Josh Micich <josh@apache.org>
2009-10-16 19:07:11 +0000

    fixed compiler warnings and TODOs in org.apache.poi.hssf.record.formula.eval
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@826041 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/poi/hssf/record/formula/eval/MissingArgEval.java
+++ src/java/org/apache/poi/hssf/record/formula/eval/MissingArgEval.java
@@ -20,7 +20,7 @@ package org.apache.poi.hssf.record.formula.eval;
 /**
  * Represents the (intermediate) evaluated result of a missing function argument.  In most cases
  * this can be translated into {@link BlankEval} but there are some notable exceptions.  Functions
- * COUNT and COUNTA <em>do</em> count their missing args.  Note - the differences between 
+ * COUNT and COUNTA <em>do</em> count their missing args.  Note - the differences between
  * {@link MissingArgEval} and {@link BlankEval} have not been investigated fully, so the POI
  * evaluator may need to be updated to account for these as they are found.
  *
@@ -28,8 +28,9 @@ package org.apache.poi.hssf.record.formula.eval;
  */
 public final class MissingArgEval implements ValueEval {
 
-    public static MissingArgEval instance = new MissingArgEval();
+	public static MissingArgEval instance = new MissingArgEval();
 
-    private MissingArgEval() {
-    }
+	private MissingArgEval() {
+		// enforce singleton
+	}
 }
--- src/java/org/apache/poi/hssf/record/formula/functions/IsError.java
+++ src/java/org/apache/poi/hssf/record/formula/functions/IsError.java
@@ -17,76 +17,26 @@
 
 package org.apache.poi.hssf.record.formula.functions;
 
-import org.apache.poi.hssf.record.formula.eval.AreaEval;
 import org.apache.poi.hssf.record.formula.eval.BoolEval;
 import org.apache.poi.hssf.record.formula.eval.ErrorEval;
-import org.apache.poi.hssf.record.formula.eval.RefEval;
+import org.apache.poi.hssf.record.formula.eval.EvaluationException;
+import org.apache.poi.hssf.record.formula.eval.OperandResolver;
 import org.apache.poi.hssf.record.formula.eval.ValueEval;
 
 /**
- * @author Amol S. Deshmukh &lt; amolweb at ya hoo dot com &gt;
- *
+ * @author Josh Micich
  */
 public final class IsError implements Function {
 
-    public ValueEval evaluate(ValueEval[] operands, int srcCellRow, short srcCellCol) {
-        ValueEval retval = null;
-        boolean b = false;
-
-        switch (operands.length) {
-        default:
-            retval = ErrorEval.VALUE_INVALID;
-            break;
-        case 1:
-            if (operands[0] instanceof ErrorEval) {
-                b = true;
-            }
-            else if (operands[0] instanceof AreaEval) {
-                AreaEval ae = (AreaEval) operands[0];
-                if (ae.contains(srcCellRow, srcCellCol)) { // circular ref!
-                    retval = ErrorEval.CIRCULAR_REF_ERROR;
-                }
-                else if (ae.isRow()) {
-                    if (ae.containsColumn(srcCellCol)) {
-                        ValueEval ve = ae.getValueAt(ae.getFirstRow(), srcCellCol);
-                        if (ve instanceof RefEval)
-                            b = ((RefEval) ve).getInnerValueEval() instanceof ErrorEval;
-                        else
-                            b = (ve instanceof ErrorEval);
-                    }
-                    else {
-                        b = true;
-                    }
-                }
-                else if (ae.isColumn()) {
-                    if (ae.containsRow(srcCellRow)) {
-                        ValueEval ve = ae.getValueAt(srcCellRow, ae.getFirstColumn());
-                        if (ve instanceof RefEval)
-                            b = ((RefEval) ve).getInnerValueEval() instanceof ErrorEval;
-                        else
-                            b = (ve instanceof ErrorEval);
-                    }
-                    else {
-                        b = true;
-                    }
-                }
-                else {
-                    b = true;
-                }
-            }
-            else if (operands[0] instanceof RefEval) {
-                b = ((RefEval) operands[0]).getInnerValueEval() instanceof ErrorEval;
-            }
-            else {
-                b = false;
-            }
-        }
-
-        if (retval == null) {
-            retval = b
-                    ? BoolEval.TRUE
-                    : BoolEval.FALSE;
-        }
-        return retval;
-    }
+	public ValueEval evaluate(ValueEval[] operands, int srcCellRow, short srcCellCol) {
+		if (operands.length != 1) {
+			return ErrorEval.VALUE_INVALID;
+		}
+		try {
+			OperandResolver.getSingleValue(operands[0], srcCellRow, srcCellCol);
+		} catch (EvaluationException e) {
+			return BoolEval.TRUE;
+		}
+		return BoolEval.FALSE;
+	}
 }
