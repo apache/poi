f69404de8d0b8d974b9eefe8411f1ce7620bb15c
Nick Burch <nick@apache.org>
2010-06-29 13:38:24 +0000

    Fix bug #49446 - Don't consider 17.16.23 field codes as properly part of the paragraph's text
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@958965 13f79535-47bb-0310-9956-ffa450edef68

--- src/ooxml/testcases/org/apache/poi/xwpf/extractor/TestXWPFWordExtractor.java
+++ src/ooxml/testcases/org/apache/poi/xwpf/extractor/TestXWPFWordExtractor.java
@@ -237,4 +237,17 @@ public class TestXWPFWordExtractor extends TestCase {
        // Now check the first paragraph in total
        assertTrue(extractor.getText().contains("a\tb\n"));
     }
+    
+    /**
+     * The output should not contain field codes, e.g. those specified in the
+     * w:instrText tag (spec sec. 17.16.23)
+     */
+    public void testNoFieldCodes() {
+        XWPFDocument doc = XWPFTestDataSamples.openSampleDocument("FieldCodes.docx");
+        XWPFWordExtractor extractor = new XWPFWordExtractor(doc);
+        String text = extractor.getText();
+        assertTrue(text.length() > 0);
+        assertFalse(text.contains("AUTHOR"));
+        assertFalse(text.contains("CREATEDATE"));
+    }
 }
