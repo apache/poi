017bde52e1a5a65eb5bc0823bf89b5d1a630f0d7
Maxim Valyanskiy <maxcom@apache.org>
2010-09-09 13:18:48 +0000

    Add code for reading Ole10Native data
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@995415 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/poi/poifs/filesystem/Ole10NativeException.java
@@ -0,0 +1,7 @@
+package org.apache.poi.poifs.filesystem;
+
+public class Ole10NativeException extends Exception {
+    public Ole10NativeException(String message) {
+        super(message);
+    }
+}
