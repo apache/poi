97c27ca476bb76486cef7bd98e867c3289d92633
Nick Burch <nick@apache.org>
2010-09-20 15:43:07 +0000

    Fix bug #49765 - Fix XWPFDocument.addPicture so that it correctly sets up relationships
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@998960 13f79535-47bb-0310-9956-ffa450edef68

--- src/ooxml/testcases/org/apache/poi/xwpf/usermodel/TestXWPFPictureData.java
+++ src/ooxml/testcases/org/apache/poi/xwpf/usermodel/TestXWPFPictureData.java
@@ -24,6 +24,7 @@ import junit.framework.TestCase;
 
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.openxml4j.opc.PackageRelationship;
+import org.apache.poi.xssf.usermodel.XSSFRelation;
 import org.apache.poi.xwpf.XWPFTestDataSamples;
 
 public class TestXWPFPictureData extends TestCase {
@@ -57,30 +58,58 @@ public class TestXWPFPictureData extends TestCase {
 			}
 	    }
 
-	    public void testNew(){
+	    public void testNew() throws Exception {
 	        XWPFDocument doc = XWPFTestDataSamples.openSampleDocument("EmptyDocumentWithHeaderFooter.docx");
 	        byte[] jpegData = "test jpeg data".getBytes();
 	        byte[] wmfData =  "test wmf data".getBytes();
 	        byte[] pngData =  "test png data".getBytes();
+	        
 	        List<XWPFPictureData> pictures = doc.getAllPictures();
 	        assertEquals(0, pictures.size());
+	        
+	        // Document shouldn't have any image relationships
+	        assertEquals(13, doc.getPackagePart().getRelationships().size());
+	        for(PackageRelationship rel : doc.getPackagePart().getRelationships()) {
+	           if(rel.getRelationshipType().equals(XSSFRelation.IMAGE_JPEG.getRelation())) {
+	              fail("Shouldn't have JPEG yet");
+	           }
+	        }
+	        
+	        // Add the image
 	        int jpegIdx;
-			try {
-				jpegIdx = doc.addPicture(jpegData, XWPFDocument.PICTURE_TYPE_JPEG);
-				assertEquals(1, pictures.size());
-				assertEquals("jpeg", pictures.get(jpegIdx).suggestFileExtension());
-				assertTrue(Arrays.equals(jpegData, pictures.get(jpegIdx).getData()));
-				
-				PackageRelationship addPictureReference = doc.addPictureReference(jpegData, Document.PICTURE_TYPE_JPEG );
-				XWPFPictureData pictureDataByID = doc.getPictureDataByID(addPictureReference.getId());
-				byte [] newJPEGData = pictureDataByID.getData();
-				assertEquals(newJPEGData.length, jpegData.length);
-				for(int i = 0; i < newJPEGData.length; i++){
-					assertEquals(newJPEGData[i], jpegData[i]);	
-				}
-			} catch (InvalidFormatException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+	        
+	        jpegIdx = doc.addPicture(jpegData, XWPFDocument.PICTURE_TYPE_JPEG);
+	        assertEquals(1, pictures.size());
+	        assertEquals("jpeg", pictures.get(jpegIdx).suggestFileExtension());
+	        assertTrue(Arrays.equals(jpegData, pictures.get(jpegIdx).getData()));
+	        
+	        // Ensure it now has one
+           assertEquals(14, doc.getPackagePart().getRelationships().size());
+           PackageRelationship jpegRel = null;
+           for(PackageRelationship rel : doc.getPackagePart().getRelationships()) {
+              if(rel.getRelationshipType().equals(XWPFRelation.IMAGE_JPEG.getRelation())) {
+                 if(jpegRel != null)
+                    fail("Found 2 jpegs!");
+                 jpegRel = rel;
+              }
+           }
+           assertNotNull("JPEG Relationship not found", jpegRel);
+           
+           // Check the details
+           assertEquals(XWPFRelation.IMAGE_JPEG.getRelation(), jpegRel.getRelationshipType());
+           assertEquals("/word/document.xml", jpegRel.getSource().getPartName().toString());
+           assertEquals("/word/media/image1.jpeg", jpegRel.getTargetURI().getPath());
+
+	        XWPFPictureData pictureDataByID = doc.getPictureDataByID(jpegRel.getId());
+	        byte [] newJPEGData = pictureDataByID.getData();
+	        assertEquals(newJPEGData.length, jpegData.length);
+	        for(int i = 0; i < newJPEGData.length; i++){
+	           assertEquals(newJPEGData[i], jpegData[i]);	
+	        }
+	        
+	        // Save an re-load, check it appears
+	        doc = XWPFTestDataSamples.writeOutAndReadBack(doc);
+	        assertEquals(1, doc.getAllPictures().size());
+           assertEquals(1, doc.getAllPackagePictures().size());
 	    }
 }
