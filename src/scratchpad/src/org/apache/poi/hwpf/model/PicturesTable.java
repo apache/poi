1c64221e5a697da24174bc0d9c191b4d71fd27ef
Maxim Valyanskiy <maxcom@apache.org>
2010-11-22 11:10:01 +0000

    hwpf: Ignore null argument in PicturesTable.hasPicture
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1037670 13f79535-47bb-0310-9956-ffa450edef68

--- src/scratchpad/src/org/apache/poi/hwpf/model/PicturesTable.java
+++ src/scratchpad/src/org/apache/poi/hwpf/model/PicturesTable.java
@@ -87,6 +87,10 @@ public final class PicturesTable
    * @param run
    */
   public boolean hasPicture(CharacterRun run) {
+    if (run==null) {
+        return false;
+    }
+
     if (run.isSpecialCharacter() && !run.isObj() && !run.isOle2() && !run.isData()) {
        // Image should be in it's own run, or in a run with the end-of-special marker
        if("\u0001".equals(run.text()) || "\u0001\u0015".equals(run.text())) {
