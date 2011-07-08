7e92ade6575e334bf28395454249d1f11d8cd229
Sergey Vladimirov <sergey@apache.org>
2011-07-08 09:49:47 +0000

    refactor FieldDescriptor to be based on autogenerated code
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1144224 13f79535-47bb-0310-9956-ffa450edef68

--- src/scratchpad/src/org/apache/poi/hwpf/model/FieldDescriptor.java
+++ src/scratchpad/src/org/apache/poi/hwpf/model/FieldDescriptor.java
@@ -17,146 +17,90 @@
 
 package org.apache.poi.hwpf.model;
 
-import java.text.MessageFormat;
-
-import org.apache.poi.util.HexDump;
+import org.apache.poi.hwpf.model.types.FLDAbstractType;
 
 /**
  * Class for the FLD structure.
  * 
  * @author Cedric Bosdonnat <cbosdonnat@novell.com>
- *
  */
-public final class FieldDescriptor
+public final class FieldDescriptor extends FLDAbstractType
 {
-  public static final int FIELD_BEGIN_MARK = 0x13;
-  public static final int FIELD_SEPARATOR_MARK = 0x14;
-  public static final int FIELD_END_MARK = 0x15;
+    public static final int FIELD_BEGIN_MARK = 0x13;
+    public static final int FIELD_SEPARATOR_MARK = 0x14;
+    public static final int FIELD_END_MARK = 0x15;
 
-  private static final short BOUNDARY_MASK = 0x1F;
-  private static final short TYPE_MASK = 0xFF;
-  private static final short ZOMBIE_EMBED_MASK = 0x02;
-  private static final short RESULT_DIRTY_MASK = 0x04;
-  private static final short RESULT_EDITED_MASK = 0x08;
-  private static final short LOCKED_MASK = 0x10;
-  private static final short PRIVATE_RESULT_MASK = 0x20;
-  private static final short NESTED_MASK = 0x40;
-  private static final short HAS_SEP_MASK = 0x80;
-
-  byte _fieldBoundaryType;
-  byte _info;
-
-  private int fieldType;
-  private boolean zombieEmbed;
-  private boolean resultDirty;
-  private boolean resultEdited;
-  private boolean locked;
-  private boolean privateResult;
-  private boolean nested;
-  private boolean hasSep;
-
-  public FieldDescriptor(byte[] data)
-  {
-    _fieldBoundaryType = (byte) (data[0] & BOUNDARY_MASK);
-    _info = data[1];
-
-    if (_fieldBoundaryType == FIELD_BEGIN_MARK)
+    public FieldDescriptor( byte[] data )
     {
-      fieldType = _info & TYPE_MASK;
-    } else if (_fieldBoundaryType == FIELD_END_MARK)
-    {
-      zombieEmbed = ((_info & ZOMBIE_EMBED_MASK) == 1);
-      resultDirty = ((_info & RESULT_DIRTY_MASK) == 1);
-      resultEdited = ((_info & RESULT_EDITED_MASK) == 1);
-      locked = ((_info & LOCKED_MASK) == 1);
-      privateResult = ((_info & PRIVATE_RESULT_MASK) == 1);
-      nested = ((_info & NESTED_MASK) == 1);
-      hasSep = ((_info & HAS_SEP_MASK) == 1);
-    }
-  }
-
-  public int getBoundaryType()
-  {
-    return _fieldBoundaryType;
-  }
-
-  public int getFieldType()
-  {
-    if (_fieldBoundaryType != FIELD_BEGIN_MARK)
-      throw new UnsupportedOperationException(
-          "This field is only defined for begin marks.");
-    return fieldType;
-  }
-
-  public boolean isZombieEmbed()
-  {
-    if (_fieldBoundaryType != FIELD_END_MARK)
-      throw new UnsupportedOperationException(
-          "This field is only defined for end marks.");
-    return zombieEmbed;
-  }
-
-  public boolean isResultDirty()
-  {
-    if (_fieldBoundaryType != FIELD_END_MARK)
-      throw new UnsupportedOperationException(
-          "This field is only defined for end marks.");
-    return resultDirty;
-  }
-
-  public boolean isResultEdited()
-  {
-    if (_fieldBoundaryType != FIELD_END_MARK)
-      throw new UnsupportedOperationException(
-          "This field is only defined for end marks.");
-    return resultEdited;
-  }
-
-  public boolean isLocked()
-  {
-    if (_fieldBoundaryType != FIELD_END_MARK)
-      throw new UnsupportedOperationException(
-          "This field is only defined for end marks.");
-    return locked;
-  }
-
-  public boolean isPrivateResult()
-  {
-    if (_fieldBoundaryType != FIELD_END_MARK)
-      throw new UnsupportedOperationException(
-          "This field is only defined for end marks.");
-    return privateResult;
-  }
-
-  public boolean isNested()
-  {
-    if (_fieldBoundaryType != FIELD_END_MARK)
-      throw new UnsupportedOperationException(
-          "This field is only defined for end marks.");
-    return nested;
-  }
-
-  public boolean isHasSep()
-  {
-    if (_fieldBoundaryType != FIELD_END_MARK)
-      throw new UnsupportedOperationException(
-          "This field is only defined for end marks.");
-    return hasSep;
-  }
-
-  public String toString()
-  {
-    String details = new String();
-    if (_fieldBoundaryType == FIELD_BEGIN_MARK)
-    {
-      details = " type: " + fieldType;
-    }
-    else if (_fieldBoundaryType == FIELD_END_MARK)
-    {
-      details = " flags: 0x" + HexDump.toHex(_info);
+        fillFields( data, 0 );
     }
 
-    return MessageFormat.format("FLD - 0x{0}{1}", HexDump
-        .toHex((byte) _fieldBoundaryType), details);
-  }
+    public int getBoundaryType()
+    {
+        return getCh();
+    }
+
+    public int getFieldType()
+    {
+        if ( getCh() != FIELD_BEGIN_MARK )
+            throw new UnsupportedOperationException(
+                    "This field is only defined for begin marks." );
+        return getFlt();
+    }
+
+    public boolean isZombieEmbed()
+    {
+        if ( getCh() != FIELD_END_MARK )
+            throw new UnsupportedOperationException(
+                    "This field is only defined for end marks." );
+        return isFZombieEmbed();
+    }
+
+    public boolean isResultDirty()
+    {
+        if ( getCh() != FIELD_END_MARK )
+            throw new UnsupportedOperationException(
+                    "This field is only defined for end marks." );
+        return isFResultDirty();
+    }
+
+    public boolean isResultEdited()
+    {
+        if ( getCh() != FIELD_END_MARK )
+            throw new UnsupportedOperationException(
+                    "This field is only defined for end marks." );
+        return isFResultEdited();
+    }
+
+    public boolean isLocked()
+    {
+        if ( getCh() != FIELD_END_MARK )
+            throw new UnsupportedOperationException(
+                    "This field is only defined for end marks." );
+        return isFLocked();
+    }
+
+    public boolean isPrivateResult()
+    {
+        if ( getCh() != FIELD_END_MARK )
+            throw new UnsupportedOperationException(
+                    "This field is only defined for end marks." );
+        return isFPrivateResult();
+    }
+
+    public boolean isNested()
+    {
+        if ( getCh() != FIELD_END_MARK )
+            throw new UnsupportedOperationException(
+                    "This field is only defined for end marks." );
+        return isFNested();
+    }
+
+    public boolean isHasSep()
+    {
+        if ( getCh() != FIELD_END_MARK )
+            throw new UnsupportedOperationException(
+                    "This field is only defined for end marks." );
+        return isFHasSep();
+    }
 }
