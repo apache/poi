7e4be8f2ba3b79655c8a88f5b28d3ba655644184
Sergey Vladimirov <sergey@apache.org>
2011-07-10 18:05:11 +0000

    fox compiler warnings
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1144924 13f79535-47bb-0310-9956-ffa450edef68

--- src/scratchpad/src/org/apache/poi/hwpf/model/io/HWPFOutputStream.java
+++ src/scratchpad/src/org/apache/poi/hwpf/model/io/HWPFOutputStream.java
@@ -34,19 +34,19 @@ public final class HWPFOutputStream extends ByteArrayOutputStream
     return _offset;
   }
 
-  public void reset()
+  public synchronized void reset()
   {
     super.reset();
     _offset = 0;
   }
 
-  public void write(byte[] buf, int off, int len)
+  public synchronized void write(byte[] buf, int off, int len)
   {
     super.write(buf, off, len);
     _offset += len;
   }
 
-  public void write(int b)
+  public synchronized void write(int b)
   {
     super.write(b);
     _offset++;
