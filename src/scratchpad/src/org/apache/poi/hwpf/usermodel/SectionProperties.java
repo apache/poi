9b7c834b5af235916b050d498eab73e41b04aaab
Sergey Vladimirov <sergey@apache.org>
2011-07-16 09:10:38 +0000

    format and sort members
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1147384 13f79535-47bb-0310-9956-ffa450edef68

--- src/scratchpad/src/org/apache/poi/hwpf/usermodel/SectionProperties.java
+++ src/scratchpad/src/org/apache/poi/hwpf/usermodel/SectionProperties.java
@@ -17,61 +17,61 @@
 
 package org.apache.poi.hwpf.usermodel;
 
+import java.lang.reflect.AccessibleObject;
+import java.lang.reflect.Field;
+
 import org.apache.poi.hwpf.model.types.SEPAbstractType;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.AccessibleObject;
-
-public final class SectionProperties
-  extends SEPAbstractType
+public final class SectionProperties extends SEPAbstractType
 {
-  public SectionProperties()
-  {
-    field_20_brcTop = new BorderCode();
-    field_21_brcLeft = new BorderCode();
-    field_22_brcBottom = new BorderCode();
-    field_23_brcRight = new BorderCode();
-    field_26_dttmPropRMark = new DateAndTime();
-  }
-
-  public Object clone()
-    throws CloneNotSupportedException
-  {
-    SectionProperties copy = (SectionProperties)super.clone();
-    copy.field_20_brcTop = (BorderCode)field_20_brcTop.clone();
-    copy.field_21_brcLeft = (BorderCode)field_21_brcLeft.clone();
-    copy.field_22_brcBottom = (BorderCode)field_22_brcBottom.clone();
-    copy.field_23_brcRight = (BorderCode)field_23_brcRight.clone();
-    copy.field_26_dttmPropRMark = (DateAndTime)field_26_dttmPropRMark.clone();
-
-    return copy;
-  }
-
-  public boolean equals(Object obj)
-  {
-    Field[] fields = SectionProperties.class.getSuperclass().getDeclaredFields();
-    AccessibleObject.setAccessible(fields, true);
-    try
+    public SectionProperties()
     {
-      for (int x = 0; x < fields.length; x++)
-      {
-        Object obj1 = fields[x].get(this);
-        Object obj2 = fields[x].get(obj);
-        if (obj1 == null && obj2 == null)
-        {
-          continue;
-        }
-        if (!obj1.equals(obj2))
-        {
-          return false;
-        }
-      }
-      return true;
+        field_20_brcTop = new BorderCode();
+        field_21_brcLeft = new BorderCode();
+        field_22_brcBottom = new BorderCode();
+        field_23_brcRight = new BorderCode();
+        field_26_dttmPropRMark = new DateAndTime();
     }
-    catch (Exception e)
+
+    public Object clone() throws CloneNotSupportedException
     {
-      return false;
+        SectionProperties copy = (SectionProperties) super.clone();
+        copy.field_20_brcTop = (BorderCode) field_20_brcTop.clone();
+        copy.field_21_brcLeft = (BorderCode) field_21_brcLeft.clone();
+        copy.field_22_brcBottom = (BorderCode) field_22_brcBottom.clone();
+        copy.field_23_brcRight = (BorderCode) field_23_brcRight.clone();
+        copy.field_26_dttmPropRMark = (DateAndTime) field_26_dttmPropRMark
+                .clone();
+
+        return copy;
     }
-  }
+
+    public boolean equals( Object obj )
+    {
+        Field[] fields = SectionProperties.class.getSuperclass()
+                .getDeclaredFields();
+        AccessibleObject.setAccessible( fields, true );
+        try
+        {
+            for ( int x = 0; x < fields.length; x++ )
+            {
+                Object obj1 = fields[x].get( this );
+                Object obj2 = fields[x].get( obj );
+                if ( obj1 == null && obj2 == null )
+                {
+                    continue;
+                }
+                if ( !obj1.equals( obj2 ) )
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        catch ( Exception e )
+        {
+            return false;
+        }
+    }
 
 }
