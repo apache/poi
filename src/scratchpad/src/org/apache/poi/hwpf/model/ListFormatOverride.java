909ce058fafd8ea972a16662f619e4753e824de5
Sergey Vladimirov <sergey@apache.org>
2011-07-24 19:23:20 +0000

    use autogenerated LFO type definition
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1150475 13f79535-47bb-0310-9956-ffa450edef68

--- src/scratchpad/src/org/apache/poi/hwpf/model/ListFormatOverride.java
+++ src/scratchpad/src/org/apache/poi/hwpf/model/ListFormatOverride.java
@@ -17,105 +17,59 @@
 
 package org.apache.poi.hwpf.model;
 
-import org.apache.poi.util.LittleEndian;
+import org.apache.poi.hwpf.model.types.LFOAbstractType;
 
-import java.util.Arrays;
-
-public final class ListFormatOverride
+public final class ListFormatOverride extends LFOAbstractType
 {
-  int _lsid;
-  int _reserved1;
-  int _reserved2;
-  byte _clfolvl;
-  byte[] _reserved3 = new byte[3];
-  ListFormatOverrideLevel[] _levelOverrides;
+    private ListFormatOverrideLevel[] _levelOverrides;
 
-  public ListFormatOverride(int lsid)
-  {
-    _lsid = lsid;
-    _levelOverrides = new ListFormatOverrideLevel[0];
-  }
-
-  public ListFormatOverride(byte[] buf, int offset)
-  {
-    _lsid = LittleEndian.getInt(buf, offset);
-    offset += LittleEndian.INT_SIZE;
-    _reserved1 = LittleEndian.getInt(buf, offset);
-    offset += LittleEndian.INT_SIZE;
-    _reserved2 = LittleEndian.getInt(buf, offset);
-    offset += LittleEndian.INT_SIZE;
-    _clfolvl = buf[offset++];
-    System.arraycopy(buf, offset, _reserved3, 0, _reserved3.length);
-    _levelOverrides = new ListFormatOverrideLevel[_clfolvl];
-  }
-
-  public int numOverrides()
-  {
-    return _clfolvl;
-  }
-
-  public int getLsid()
-  {
-    return _lsid;
-  }
-
-  void setLsid(int lsid)
-  {
-    _lsid = lsid;
-  }
-
-  public ListFormatOverrideLevel[] getLevelOverrides()
-  {
-    return _levelOverrides;
-  }
-
-  public void setOverride(int index, ListFormatOverrideLevel lfolvl)
-  {
-    _levelOverrides[index] = lfolvl;
-  }
-
-  public ListFormatOverrideLevel getOverrideLevel(int level)
-  {
-
-    ListFormatOverrideLevel retLevel = null;
-
-    for(int x = 0; x < _levelOverrides.length; x++)
+    public ListFormatOverride( byte[] buf, int offset )
     {
-      if (_levelOverrides[x].getLevelNum() == level)
-      {
-        retLevel = _levelOverrides[x];
-      }
-    }
-    return retLevel;
-  }
+        fillFields( buf, offset );
 
-  public boolean equals(Object obj)
-  {
-    if (obj == null)
-    {
-      return false;
+        _levelOverrides = new ListFormatOverrideLevel[getClfolvl()];
     }
 
-    ListFormatOverride lfo = (ListFormatOverride)obj;
-    return lfo._clfolvl == _clfolvl && lfo._lsid == _lsid &&
-      lfo._reserved1 == _reserved1 && lfo._reserved2 == _reserved2 &&
-      Arrays.equals(lfo._reserved3, _reserved3) &&
-      Arrays.equals(lfo._levelOverrides, _levelOverrides);
-  }
+    public ListFormatOverride( int lsid )
+    {
+        setLsid( lsid );
+        _levelOverrides = new ListFormatOverrideLevel[0];
+    }
 
-  public byte[] toByteArray()
-  {
-    byte[] buf = new byte[16];
-    int offset = 0;
-    LittleEndian.putInt(buf, offset, _lsid);
-    offset += LittleEndian.INT_SIZE;
-    LittleEndian.putInt(buf, offset, _reserved1);
-    offset += LittleEndian.INT_SIZE;
-    LittleEndian.putInt(buf, offset, _reserved2);
-    offset += LittleEndian.INT_SIZE;
-    buf[offset++] = _clfolvl;
-    System.arraycopy(_reserved3, 0, buf, offset, 3);
+    public ListFormatOverrideLevel[] getLevelOverrides()
+    {
+        return _levelOverrides;
+    }
 
-    return buf;
-  }
+    public ListFormatOverrideLevel getOverrideLevel( int level )
+    {
+
+        ListFormatOverrideLevel retLevel = null;
+
+        for ( int x = 0; x < _levelOverrides.length; x++ )
+        {
+            if ( _levelOverrides[x].getLevelNum() == level )
+            {
+                retLevel = _levelOverrides[x];
+            }
+        }
+        return retLevel;
+    }
+
+    public int numOverrides()
+    {
+        return getClfolvl();
+    }
+
+    public void setOverride( int index, ListFormatOverrideLevel lfolvl )
+    {
+        _levelOverrides[index] = lfolvl;
+    }
+
+    public byte[] toByteArray()
+    {
+        byte[] bs = new byte[getSize()];
+        serialize( bs, 0 );
+        return bs;
+    }
 }
