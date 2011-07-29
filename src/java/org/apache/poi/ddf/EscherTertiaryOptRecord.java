e4efe9c5b5321bef19f96723d3458272340d88bd
Sergey Vladimirov <sergey@apache.org>
2011-07-29 07:01:39 +0000

    exctract common properties record code into AbstractEscherOptRecord
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1152126 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/poi/ddf/EscherTertiaryOptRecord.java
+++ src/java/org/apache/poi/ddf/EscherTertiaryOptRecord.java
@@ -16,96 +16,18 @@
 ==================================================================== */
 package org.apache.poi.ddf;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.apache.poi.util.HexDump;
-import org.apache.poi.util.LittleEndian;
-
 /**
  * "The OfficeArtTertiaryFOPT record specifies a table of OfficeArtRGFOPTE properties, as defined in section 2.3.1."
  * -- [MS-ODRAW] — v20110608; Office Drawing Binary File Format
  * 
  * @author Sergey Vladimirov (vlsergey {at} gmail {dot} com)
  */
-public class EscherTertiaryOptRecord extends EscherRecord
+public class EscherTertiaryOptRecord extends AbstractEscherOptRecord
 {
     public static final short RECORD_ID = (short) 0xF122;
-
-    private List<EscherProperty> properties = new ArrayList<EscherProperty>();
-
-    public int fillFields( byte[] data, int offset,
-            EscherRecordFactory recordFactory )
-    {
-        int bytesRemaining = readHeader( data, offset );
-        int pos = offset + 8;
-
-        EscherPropertyFactory f = new EscherPropertyFactory();
-        properties = f.createProperties( data, pos, getInstance() );
-        return bytesRemaining + 8;
-    }
-
-    private int getPropertiesSize()
-    {
-        int totalSize = 0;
-        for ( EscherProperty property : properties )
-        {
-            totalSize += property.getPropertySize();
-        }
-
-        return totalSize;
-    }
 
     public String getRecordName()
     {
         return "TertiaryOpt";
     }
-
-    @Override
-    public int getRecordSize()
-    {
-        return 8 + getPropertiesSize();
-    }
-
-    public int serialize( int offset, byte[] data,
-            EscherSerializationListener listener )
-    {
-        listener.beforeRecordSerialize( offset, getRecordId(), this );
-
-        LittleEndian.putShort( data, offset, getOptions() );
-        LittleEndian.putShort( data, offset + 2, getRecordId() );
-        LittleEndian.putInt( data, offset + 4, getPropertiesSize() );
-        int pos = offset + 8;
-        for ( EscherProperty property : properties )
-        {
-            pos += property.serializeSimplePart( data, pos );
-        }
-        for ( EscherProperty property : properties )
-        {
-            pos += property.serializeComplexPart( data, pos );
-        }
-        listener.afterRecordSerialize( pos, getRecordId(), pos - offset, this );
-        return pos - offset;
-    }
-
-    /**
-     * Retrieve the string representation of this record.
-     */
-    public String toString()
-    {
-        String nl = System.getProperty( "line.separator" );
-        StringBuffer propertiesBuf = new StringBuffer();
-        for ( EscherProperty property : properties )
-        {
-            propertiesBuf.append( "    " + property.toString() + nl );
-        }
-
-        return "org.apache.poi.ddf.EscherTertiaryOptRecord:" + nl
-                + "  isContainer: " + isContainerRecord() + nl
-                + "  options: 0x" + HexDump.toHex( getOptions() ) + nl
-                + "  recordId: 0x" + HexDump.toHex( getRecordId() ) + nl
-                + "  numchildren: " + getChildRecords().size() + nl
-                + "  properties:" + nl + propertiesBuf.toString();
-    }
-
 }
