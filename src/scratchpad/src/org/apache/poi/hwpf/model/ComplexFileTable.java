79d02d4e677d30ce34afbb96463eda0e411aac90
Sergey Vladimirov <sergey@apache.org>
2011-08-11 18:49:35 +0000

    reuse existing POIFS of hwpf document to preserve all OLE streams
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1156727 13f79535-47bb-0310-9956-ffa450edef68

--- src/scratchpad/src/org/apache/poi/hwpf/model/ComplexFileTable.java
+++ src/scratchpad/src/org/apache/poi/hwpf/model/ComplexFileTable.java
@@ -80,20 +80,26 @@ public final class ComplexFileTable
         return _grpprls;
     }
 
-  public void writeTo(HWPFFileSystem sys)
-    throws IOException
-  {
-    HWPFOutputStream docStream = sys.getStream("WordDocument");
-    HWPFOutputStream tableStream = sys.getStream("1Table");
+    @Deprecated
+    public void writeTo( HWPFFileSystem sys ) throws IOException
+    {
+        HWPFOutputStream docStream = sys.getStream( "WordDocument" );
+        HWPFOutputStream tableStream = sys.getStream( "1Table" );
 
-    tableStream.write(TEXT_PIECE_TABLE_TYPE);
+        writeTo( docStream, tableStream );
+    }
 
-    byte[] table = _tpt.writeTo(docStream);
+    public void writeTo( HWPFOutputStream wordDocumentStream,
+            HWPFOutputStream tableStream ) throws IOException
+    {
+        tableStream.write( TEXT_PIECE_TABLE_TYPE );
 
-    byte[] numHolder = new byte[LittleEndian.INT_SIZE];
-    LittleEndian.putInt(numHolder, table.length);
-    tableStream.write(numHolder);
-    tableStream.write(table);
-  }
+        byte[] table = _tpt.writeTo( wordDocumentStream );
+
+        byte[] numHolder = new byte[LittleEndian.INT_SIZE];
+        LittleEndian.putInt( numHolder, table.length );
+        tableStream.write( numHolder );
+        tableStream.write( table );
+    }
 
 }
