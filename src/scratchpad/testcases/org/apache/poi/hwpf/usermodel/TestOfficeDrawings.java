357f78f1da6a22e4934b85050b8880af7cf278fc
Sergey Vladimirov <sergey@apache.org>
2011-08-23 15:44:03 +0000

    add copyrights and comments
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1160749 13f79535-47bb-0310-9956-ffa450edef68

--- src/scratchpad/testcases/org/apache/poi/hwpf/usermodel/TestOfficeDrawings.java
+++ src/scratchpad/testcases/org/apache/poi/hwpf/usermodel/TestOfficeDrawings.java
@@ -1,6 +1,20 @@
-package org.apache.poi.hwpf.usermodel;
+/* ====================================================================
+   Licensed to the Apache Software Foundation (ASF) under one or more
+   contributor license agreements.  See the NOTICE file distributed with
+   this work for additional information regarding copyright ownership.
+   The ASF licenses this file to You under the Apache License, Version 2.0
+   (the "License"); you may not use this file except in compliance with
+   the License.  You may obtain a copy of the License at
 
-import java.io.UnsupportedEncodingException;
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+==================================================================== */
+package org.apache.poi.hwpf.usermodel;
 
 import junit.framework.TestCase;
 
@@ -10,9 +24,17 @@ import org.apache.poi.ddf.EscherOptRecord;
 import org.apache.poi.hwpf.HWPFDocument;
 import org.apache.poi.hwpf.HWPFTestDataSamples;
 
+/**
+ * Test cases for {@link OfficeDrawing} and {@link OfficeDrawingsImpl} classes.
+ * 
+ * @author Sergey Vladimirov (vlsergey {at} gmail {dot} com)
+ */
 public class TestOfficeDrawings extends TestCase
 {
-    public void testWatermark() throws UnsupportedEncodingException
+    /**
+     * Tests watermark text extraction
+     */
+    public void testWatermark() throws Exception
     {
         HWPFDocument hwpfDocument = HWPFTestDataSamples
                 .openSampleFile( "watermark.doc" );
