0a2def0955953962295800a70bd5360b6fea63a5
Sergey Vladimirov <sergey@apache.org>
2011-10-22 02:01:14 +0000

    replace exception with warning
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1187638 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/poi/hpsf/VariantBool.java
+++ src/java/org/apache/poi/hpsf/VariantBool.java
@@ -1,12 +1,15 @@
 package org.apache.poi.hpsf;
 
-import org.apache.poi.util.LittleEndian;
-
 import org.apache.poi.util.Internal;
+import org.apache.poi.util.LittleEndian;
+import org.apache.poi.util.POILogFactory;
+import org.apache.poi.util.POILogger;
 
 @Internal
 class VariantBool
 {
+    private final static POILogger logger = POILogFactory.getLogger( VariantBool.class );
+    
     static final int SIZE = 2;
 
     private boolean _value;
@@ -26,7 +29,18 @@ class VariantBool
             return;
         }
 
-        throw new IllegalPropertySetDataException( "VARIANT_BOOL value '"
-                + value + "' is incorrect" );
+        logger.log( POILogger.WARN, "VARIANT_BOOL value '",
+                Short.valueOf( value ), "' is incorrect" );
+        _value = value != 0;
+    }
+
+    boolean getValue()
+    {
+        return _value;
+    }
+
+    void setValue( boolean value )
+    {
+        this._value = value;
     }
 }
