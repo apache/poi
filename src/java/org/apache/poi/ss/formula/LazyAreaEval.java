107c23505b8a7e1eed77e62732bcfcdc0661b052
Evgeniy Berlog <berlog@apache.org>
2012-08-28 08:24:07 +0000

    fixed bug 53434
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1378016 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/poi/ss/formula/LazyAreaEval.java
+++ src/java/org/apache/poi/ss/formula/LazyAreaEval.java
@@ -93,6 +93,6 @@ final class LazyAreaEval extends AreaEvalBase {
     */
     public boolean isSubTotal(int rowIndex, int columnIndex){
         // delegate the query to the sheet evaluator which has access to internal ptgs
-        return _evaluator.isSubTotal(rowIndex, columnIndex);
+        return _evaluator.isSubTotal(getFirstRow() + rowIndex, getFirstColumn() + columnIndex);
     }
 }
