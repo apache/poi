82e268acf37542f43c3d430f71560bb5c5b6000b
Yegor Kozlov <yegor@apache.org>
2012-10-10 15:41:35 +0000

    Bug 53965: Fixed XmlValueOutOfRangeExceptio calling getDataValidations for custom validations with XSSFSheet
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1396650 13f79535-47bb-0310-9956-ffa450edef68

--- src/ooxml/testcases/org/apache/poi/xssf/usermodel/TestXSSFDataValidation.java
+++ src/ooxml/testcases/org/apache/poi/xssf/usermodel/TestXSSFDataValidation.java
@@ -16,6 +16,7 @@
 ==================================================================== */
 package org.apache.poi.xssf.usermodel;
 
+import java.io.FileOutputStream;
 import java.math.BigDecimal;
 import java.util.List;
 
@@ -239,4 +240,25 @@ public class TestXSSFDataValidation extends BaseTestDataValidation {
 		validation.createPromptBox("Prompt", "Enter some value");
 		validation.setSuppressDropDownArrow(yesNo);
 	}
+
+    public void test53965() throws Exception {
+
+        XSSFWorkbook wb = new XSSFWorkbook();
+        XSSFSheet sheet = wb.createSheet();
+        List<XSSFDataValidation> lst = sheet.getDataValidations();    //<-- works
+        assertEquals(0, lst.size());
+
+        //create the cell that will have the validation applied
+        sheet.createRow(0).createCell(0);
+
+        DataValidationHelper dataValidationHelper = sheet.getDataValidationHelper();
+        DataValidationConstraint constraint = dataValidationHelper.createCustomConstraint("SUM($A$1:$A$1) <= 3500");
+        CellRangeAddressList addressList = new CellRangeAddressList(0, 0, 0, 0);
+        DataValidation validation = dataValidationHelper.createValidation(constraint, addressList);
+        sheet.addValidationData(validation);
+
+        // this line caused XmlValueOutOfRangeException , see Bugzilla 3965
+        lst = sheet.getDataValidations();
+        assertEquals(1, lst.size());
+    }
 }
