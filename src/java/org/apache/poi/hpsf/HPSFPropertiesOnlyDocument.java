85cca5bc2d27c219a69e143f687b2a22620c6ea8
Nick Burch <nick@apache.org>
2013-06-26 00:18:41 +0000

    Add a write method to HPSFPropertiesOnlyDocument, and use this to finish the unit tests for bug #54233
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1496683 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/poi/hpsf/HPSFPropertiesOnlyDocument.java
+++ src/java/org/apache/poi/hpsf/HPSFPropertiesOnlyDocument.java
@@ -1,8 +1,12 @@
 package org.apache.poi.hpsf;
 
+import java.io.IOException;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.poi.POIDocument;
+import org.apache.poi.poifs.filesystem.EntryUtils;
 import org.apache.poi.poifs.filesystem.NPOIFSFileSystem;
 import org.apache.poi.poifs.filesystem.POIFSFileSystem;
 
@@ -20,7 +24,22 @@ public class HPSFPropertiesOnlyDocument extends POIDocument {
         super(fs);
     }
 
-    public void write(OutputStream out) {
-        throw new IllegalStateException("Unable to write, only for properties!");
+    /**
+     * Write out, with any properties changes, but nothing else
+     */
+    public void write(OutputStream out) throws IOException {
+        POIFSFileSystem fs = new POIFSFileSystem();
+
+        // For tracking what we've written out, so far
+        List<String> excepts = new ArrayList<String>(1);
+
+        // Write out our HPFS properties, with any changes
+        writeProperties(fs, excepts);
+        
+        // Copy over everything else unchanged
+        EntryUtils.copyNodes(directory, fs.getRoot(), excepts);
+        
+        // Save the resultant POIFSFileSystem to the output stream
+        fs.writeFilesystem(out);
     }
 }
