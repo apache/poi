f49f20daec40fbfe08ae693df58deaa90efa0ee1
Cédric Walter <cedricwalter@apache.org>
2013-10-07 09:42:38 +0000

    Bug 55037: patch for missing function DELTA  Add missing TestDeltaFunctionsFromSpreadsheet and register Delta in AnalysisToolPak
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1529809 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/testcases/org/apache/poi/ss/formula/functions/TestDeltaFunctionsFromSpreadsheet.java
@@ -0,0 +1,13 @@
+package org.apache.poi.ss.formula.functions;
+
+/**
+* Tests DELTA() as loaded from a test data spreadsheet.<p/>
+*
+* @author cedric dot walter @ gmail dot com
+*/
+public class TestDeltaFunctionsFromSpreadsheet extends BaseTestFunctionsFromSpreadsheet {
+
+    protected String getFilename() {
+        return "DeltaFunctionTestCaseData.xls";
+    }
+}
