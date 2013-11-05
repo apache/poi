fac5f0ea5068736dfdcef8fc8d70c6ca3ea4231c
Cédric Walter <cedricwalter@apache.org>
2013-11-05 20:46:20 +0000

    fixed red build, cause by invalid java 5 method isEmpty and unmappable character for encoding ASCII
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1539127 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/poi/ss/formula/functions/Mirr.java
+++ src/java/org/apache/poi/ss/formula/functions/Mirr.java
@@ -37,7 +37,7 @@ import org.apache.poi.ss.formula.eval.EvaluationException;
  * Reinvest_rate     is the interest rate you receive on the cash flows as you reinvest them.
  *
  * @author Carlos Delgado (carlos dot del dot est at gmail dot com)
- * @author Cédric Walter (cedric dot walter at gmail dot com)
+ * @author Cedric Walter (cedric dot walter at gmail dot com)
  *
  * @see <a href="http://en.wikipedia.org/wiki/MIRR">Wikipedia on MIRR</a>
  * @see <a href="http://office.microsoft.com/en-001/excel-help/mirr-HP005209180.aspx">Excel MIRR</a>
