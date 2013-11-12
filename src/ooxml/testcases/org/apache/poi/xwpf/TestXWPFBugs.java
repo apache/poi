c842462a7771b19b09db0e81dcf7347c2d7134cc
Nick Burch <nick@apache.org>
2013-11-12 11:32:10 +0000

    Fix whitespace
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1541005 13f79535-47bb-0310-9956-ffa450edef68

--- src/ooxml/testcases/org/apache/poi/xwpf/TestXWPFBugs.java
+++ src/ooxml/testcases/org/apache/poi/xwpf/TestXWPFBugs.java
@@ -16,37 +16,37 @@ import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 
 public class TestXWPFBugs extends TestCase {
-	/**
-	 * A word document that's encrypted with non-standard
-	 *  Encryption options, and no cspname section. See bug 53475
-	 */
-	public void test53475() throws Exception {
-		try {
-    		Biff8EncryptionKey.setCurrentUserPassword("solrcell");
-    		File file = POIDataSamples.getDocumentInstance().getFile("bug53475-password-is-solrcell.docx");
-    		NPOIFSFileSystem filesystem = new NPOIFSFileSystem(file, true);
-    		
-    		// Check the encryption details
-    		EncryptionInfo info = new EncryptionInfo(filesystem);
-    		assertEquals(128, info.getHeader().getKeySize());
-    		assertEquals(EncryptionHeader.ALGORITHM_AES_128, info.getHeader().getAlgorithm());
-    		assertEquals(EncryptionHeader.HASH_SHA1, info.getHeader().getHashAlgorithm());
-    		
-    		// Check it can be decoded
-    		Decryptor d = Decryptor.getInstance(info);		
-    		assertTrue("Unable to process: document is encrypted", d.verifyPassword("solrcell"));
-    
-    		// Check we can read the word document in that
-    	    InputStream dataStream = d.getDataStream(filesystem);
-    	    OPCPackage opc = OPCPackage.open(dataStream);
-    	    XWPFDocument doc = new XWPFDocument(opc);
-    	    XWPFWordExtractor ex = new XWPFWordExtractor(doc);
-    	    String text = ex.getText();
-    	    assertNotNull(text);
-    	    assertEquals("This is password protected Word document.", text.trim());
-    	    ex.close();
-		} finally {
-			Biff8EncryptionKey.setCurrentUserPassword(null);
-		}
-	}
+    /**
+     * A word document that's encrypted with non-standard
+     *  Encryption options, and no cspname section. See bug 53475
+     */
+    public void test53475() throws Exception {
+        try {
+            Biff8EncryptionKey.setCurrentUserPassword("solrcell");
+            File file = POIDataSamples.getDocumentInstance().getFile("bug53475-password-is-solrcell.docx");
+            NPOIFSFileSystem filesystem = new NPOIFSFileSystem(file, true);
+
+            // Check the encryption details
+            EncryptionInfo info = new EncryptionInfo(filesystem);
+            assertEquals(128, info.getHeader().getKeySize());
+            assertEquals(EncryptionHeader.ALGORITHM_AES_128, info.getHeader().getAlgorithm());
+            assertEquals(EncryptionHeader.HASH_SHA1, info.getHeader().getHashAlgorithm());
+
+            // Check it can be decoded
+            Decryptor d = Decryptor.getInstance(info);		
+            assertTrue("Unable to process: document is encrypted", d.verifyPassword("solrcell"));
+
+            // Check we can read the word document in that
+            InputStream dataStream = d.getDataStream(filesystem);
+            OPCPackage opc = OPCPackage.open(dataStream);
+            XWPFDocument doc = new XWPFDocument(opc);
+            XWPFWordExtractor ex = new XWPFWordExtractor(doc);
+            String text = ex.getText();
+            assertNotNull(text);
+            assertEquals("This is password protected Word document.", text.trim());
+            ex.close();
+        } finally {
+            Biff8EncryptionKey.setCurrentUserPassword(null);
+        }
+    }
 }
