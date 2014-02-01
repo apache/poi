5d0f69097c1137a16181c5c724c59f7f8a092651
Andreas Beeker <kiwiwings@apache.org>
2014-02-01 21:45:48 +0000

    Bug 51891 - Fix StringIndexOutOfBoundsException : Ole10Native.<init> (parsing word file)
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1563483 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/poi/hpsf/ClassID.java
+++ src/java/org/apache/poi/hpsf/ClassID.java
@@ -40,6 +40,7 @@ public class ClassID
     public static final ClassID WORD95       = new ClassID("{00020900-0000-0000-C000-000000000046}");
     public static final ClassID POWERPOINT97 = new ClassID("{64818D10-4F9B-11CF-86EA-00AA00B929E8}");
     public static final ClassID POWERPOINT95 = new ClassID("{EA7BAE70-FB3B-11CD-A903-00AA00510EA3}");
+    public static final ClassID EQUATION30   = new ClassID("{0002CE02-0000-0000-C000-000000000046}");
 	
 	
     /**
