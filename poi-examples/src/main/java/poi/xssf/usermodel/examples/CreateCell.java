5a1255db3543afbd301cf191be5246bf169f54b3
Cédric Walter <cedricwalter@apache.org>
2014-04-02 11:23:16 +0000

    moved poi example code to module poi-examples
    git-svn-id: https://svn.apache.org/repos/asf/poi/branches/maven@1583970 13f79535-47bb-0310-9956-ffa450edef68

--- poi-examples/src/main/java/poi/xssf/usermodel/examples/CreateCell.java
+++ poi-examples/src/main/java/poi/xssf/usermodel/examples/CreateCell.java
@@ -15,7 +15,7 @@
    limitations under the License.
 ==================================================================== */
 
-package org.apache.poi.xssf.usermodel.examples;
+package poi.xssf.usermodel.examples;
 
 import java.io.FileOutputStream;
 import java.util.Date;
