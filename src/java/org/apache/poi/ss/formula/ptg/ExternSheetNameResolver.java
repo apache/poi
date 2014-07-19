76c8077a40c74b78ee788b73b21b00736037df9f
Nick Burch <nick@apache.org>
2014-07-19 19:11:54 +0000

    Fix inconsistent indents
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1611947 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/poi/ss/formula/ptg/ExternSheetNameResolver.java
+++ src/java/org/apache/poi/ss/formula/ptg/ExternSheetNameResolver.java
@@ -25,36 +25,35 @@ import org.apache.poi.ss.formula.SheetNameFormatter;
  * @author Josh Micich
  */
 final class ExternSheetNameResolver {
+    private ExternSheetNameResolver() {
+        // no instances of this class
+    }
 
-	private ExternSheetNameResolver() {
-		// no instances of this class
-	}
-
-	public static String prependSheetName(FormulaRenderingWorkbook book, int field_1_index_extern_sheet, String cellRefText) {
-		ExternalSheet externalSheet = book.getExternalSheet(field_1_index_extern_sheet);
-		StringBuffer sb;
-		if (externalSheet != null) {
-			String wbName = externalSheet.getWorkbookName();
-			String sheetName = externalSheet.getSheetName();
-			if (wbName != null) {
-    			sb = new StringBuffer(wbName.length() + sheetName.length() + cellRefText.length() + 4);
-    			SheetNameFormatter.appendFormat(sb, wbName, sheetName);
-			} else {
+    public static String prependSheetName(FormulaRenderingWorkbook book, int field_1_index_extern_sheet, String cellRefText) {
+        ExternalSheet externalSheet = book.getExternalSheet(field_1_index_extern_sheet);
+        StringBuffer sb;
+        if (externalSheet != null) {
+            String wbName = externalSheet.getWorkbookName();
+            String sheetName = externalSheet.getSheetName();
+            if (wbName != null) {
+                sb = new StringBuffer(wbName.length() + sheetName.length() + cellRefText.length() + 4);
+                SheetNameFormatter.appendFormat(sb, wbName, sheetName);
+            } else {
                 sb = new StringBuffer(sheetName.length() + cellRefText.length() + 4);
                 SheetNameFormatter.appendFormat(sb, sheetName);
-			}
-		} else {
-			String sheetName = book.getSheetNameByExternSheet(field_1_index_extern_sheet);
-			sb = new StringBuffer(sheetName.length() + cellRefText.length() + 4);
-			if (sheetName.length() < 1) {
-				// What excel does if sheet has been deleted
-				sb.append("#REF"); // note - '!' added just once below
-			} else {
-				SheetNameFormatter.appendFormat(sb, sheetName);
-			}
-		}
-   		sb.append('!');
-		sb.append(cellRefText);
-		return sb.toString();
-	}
+            }
+        } else {
+            String sheetName = book.getSheetNameByExternSheet(field_1_index_extern_sheet);
+            sb = new StringBuffer(sheetName.length() + cellRefText.length() + 4);
+            if (sheetName.length() < 1) {
+                // What excel does if sheet has been deleted
+                sb.append("#REF"); // note - '!' added just once below
+            } else {
+                SheetNameFormatter.appendFormat(sb, sheetName);
+            }
+        }
+        sb.append('!');
+        sb.append(cellRefText);
+        return sb.toString();
+    }
 }
