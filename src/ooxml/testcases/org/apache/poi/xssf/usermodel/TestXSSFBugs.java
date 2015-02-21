04ae3247eb9cbf426bb550d89f129dc71958725d
Dominik Stadler <centic@apache.org>
2015-02-21 11:58:45 +0000

    Revert organize imports
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1661327 13f79535-47bb-0310-9956-ffa450edef68

--- src/ooxml/testcases/org/apache/poi/xssf/usermodel/TestXSSFBugs.java
+++ src/ooxml/testcases/org/apache/poi/xssf/usermodel/TestXSSFBugs.java
@@ -17,7 +17,13 @@
 
 package org.apache.poi.xssf.usermodel;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
