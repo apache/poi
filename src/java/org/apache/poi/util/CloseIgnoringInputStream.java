a080a9c56f1e22a523b5edcf900c0e8896cf961e
Andreas Beeker <kiwiwings@apache.org>
2015-06-20 15:08:42 +0000

    fixed javadoc links after imports have been organized
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1686636 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/poi/util/CloseIgnoringInputStream.java
+++ src/java/org/apache/poi/util/CloseIgnoringInputStream.java
@@ -24,7 +24,7 @@ import java.io.InputStream;
  * A wrapper around an {@link InputStream}, which 
  *  ignores close requests made to it.
  *
- * Useful with {@link POIFSFileSystem}, where you want
+ * Useful with {@link org.apache.poi.poifs.filesystem.POIFSFileSystem}, where you want
  *  to control the close yourself.
  */
 public class CloseIgnoringInputStream extends FilterInputStream {
