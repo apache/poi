8c1b7df4bc119a1b6e93ebb1db51e21585107f31
Dominik Stadler <centic@apache.org>
2015-07-19 12:11:19 +0000

    Bug 57484: Allow processing of non-OOXML core namespace packages
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1691821 13f79535-47bb-0310-9956-ffa450edef68

--- src/integrationtest/org/apache/poi/stress/POIXMLDocumentHandler.java
+++ src/integrationtest/org/apache/poi/stress/POIXMLDocumentHandler.java
@@ -20,17 +20,10 @@ import static org.junit.Assert.assertNotNull;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.List;
 
 import org.apache.poi.POIXMLDocument;
-import org.apache.poi.openxml4j.exceptions.OpenXML4JException;
-import org.apache.poi.openxml4j.opc.OPCPackage;
-import org.apache.poi.openxml4j.opc.PackageAccess;
-import org.apache.poi.openxml4j.opc.PackagePart;
 import org.apache.poi.poifs.crypt.Decryptor;
 import org.apache.poi.poifs.filesystem.POIFSFileSystem;
-import org.junit.Ignore;
-import org.junit.Test;
 
 public final class POIXMLDocumentHandler {
 	protected void handlePOIXMLDocument(POIXMLDocument doc) throws Exception {
@@ -44,33 +37,15 @@ public final class POIXMLDocumentHandler {
     protected static boolean isEncrypted(InputStream stream) throws IOException {
         if (POIFSFileSystem.hasPOIFSHeader(stream)) {
             POIFSFileSystem poifs = new POIFSFileSystem(stream);
-            if (poifs.getRoot().hasEntry(Decryptor.DEFAULT_POIFS_ENTRY)) {
-                return true;
+            try {
+                if (poifs.getRoot().hasEntry(Decryptor.DEFAULT_POIFS_ENTRY)) {
+                    return true;
+                }
+            } finally {
+                poifs.close();
             }
             throw new IOException("wrong file format or file extension for OO XML file");
         }
         return false;
     }
-	
-	// a test-case to test this locally without executing the full TestAllFiles
-    @Ignore("POIXMLDocument cannot handle this Visio file currently...")
-	@Test
-	public void test() throws Exception {
-		OPCPackage pkg = OPCPackage.open("test-data/diagram/test.vsdx", PackageAccess.READ);
-		try {
-			handlePOIXMLDocument(new TestPOIXMLDocument(pkg));
-		} finally {
-			pkg.close();
-		}
-	}
-	
-	private final static class TestPOIXMLDocument extends POIXMLDocument {
-		public TestPOIXMLDocument(OPCPackage pkg) {
-			super(pkg);
-		}
-
-		public List<PackagePart> getAllEmbedds() throws OpenXML4JException {
-			return null;
-		}
-	}
 }
