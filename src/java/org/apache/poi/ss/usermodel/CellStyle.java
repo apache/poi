f7939c4c95c351b44ca6a78124d8c8b84e4f81a6
Javen O'Neal <onealj@apache.org>
2016-04-05 21:34:24 +0000

    bug 59264: add link to BorderStyle on deprecated BORDER_* constants
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1737884 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/poi/ss/usermodel/CellStyle.java
+++ src/java/org/apache/poi/ss/usermodel/CellStyle.java
@@ -87,98 +87,98 @@ public interface CellStyle {
 
     /**
      * No border
-     * @deprecated Use {@link BorderStyle}
+     * @deprecated 3.15 beta 1. Use {@link BorderStyle} instead.
      */
 
     short BORDER_NONE = 0x0;
 
     /**
      * Thin border
-     * @deprecated Use {@link BorderStyle}
+     * @deprecated 3.15 beta 1. Use {@link BorderStyle} instead.
      */
 
     short BORDER_THIN = 0x1;
 
     /**
      * Medium border
-     * @deprecated Use {@link BorderStyle}
+     * @deprecated 3.15 beta 1. Use {@link BorderStyle} instead.
      */
 
     short BORDER_MEDIUM = 0x2;
 
     /**
      * dash border
-     * @deprecated Use {@link BorderStyle}
+     * @deprecated 3.15 beta 1. Use {@link BorderStyle} instead.
      */
 
     short BORDER_DASHED = 0x3;
 
     /**
      * dot border
-     * @deprecated Use {@link BorderStyle}
+     * @deprecated 3.15 beta 1. Use {@link BorderStyle} instead.
      */
 
     short BORDER_DOTTED = 0x4;
 
     /**
      * Thick border
-     * @deprecated Use {@link BorderStyle}
+     * @deprecated 3.15 beta 1. Use {@link BorderStyle} instead.
      */
 
     short BORDER_THICK = 0x5;
 
     /**
      * double-line border
-     * @deprecated Use {@link BorderStyle}
+     * @deprecated 3.15 beta 1. Use {@link BorderStyle} instead.
      */
 
     short BORDER_DOUBLE = 0x6;
 
     /**
      * hair-line border
-     * @deprecated Use {@link BorderStyle}
+     * @deprecated 3.15 beta 1. Use {@link BorderStyle} instead.
      */
 
     short BORDER_HAIR = 0x7;
 
     /**
      * Medium dashed border
-     * @deprecated Use {@link BorderStyle}
+     * @deprecated 3.15 beta 1. Use {@link BorderStyle} instead.
      */
 
     short BORDER_MEDIUM_DASHED = 0x8;
 
     /**
      * dash-dot border
-     * @deprecated Use {@link BorderStyle}
+     * @deprecated 3.15 beta 1. Use {@link BorderStyle} instead.
      */
 
     short BORDER_DASH_DOT = 0x9;
 
     /**
      * medium dash-dot border
-     * @deprecated Use {@link BorderStyle}
+     * @deprecated 3.15 beta 1. Use {@link BorderStyle} instead.
      */
 
     short BORDER_MEDIUM_DASH_DOT = 0xA;
 
     /**
      * dash-dot-dot border
-     * @deprecated Use {@link BorderStyle}
+     * @deprecated 3.15 beta 1. Use {@link BorderStyle} instead.
      */
 
     short BORDER_DASH_DOT_DOT = 0xB;
 
     /**
      * medium dash-dot-dot border
-     * @deprecated Use {@link BorderStyle}
+     * @deprecated 3.15 beta 1. Use {@link BorderStyle} instead.
      */
 
     short BORDER_MEDIUM_DASH_DOT_DOT = 0xC;
 
     /**
      * slanted dash-dot border
-     * @deprecated Use {@link BorderStyle}
+     * @deprecated 3.15 beta 1. Use {@link BorderStyle} instead.
      */
 
     short BORDER_SLANTED_DASH_DOT = 0xD;
