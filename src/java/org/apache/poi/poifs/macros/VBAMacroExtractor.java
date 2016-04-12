b302d10d8c976304efcb9995e10f179030711185
Javen O'Neal <onealj@apache.org>
2016-04-12 01:30:20 +0000

    fix javadocs and findbugs warnings
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1738674 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/poi/poifs/macros/VBAMacroExtractor.java
+++ src/java/org/apache/poi/poifs/macros/VBAMacroExtractor.java
@@ -23,12 +23,15 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import org.apache.poi.util.StringUtil;
 
 /**
  * This tool extracts out the source of all VBA Modules of an office file,
  *  both OOXML (eg XLSM) and OLE2/POIFS (eg DOC), to STDOUT or a directory.
+ * 
+ * @since 3.15-beta2
  */
 public class VBAMacroExtractor {
     public static void main(String args[]) throws IOException {
@@ -47,15 +50,30 @@ public class VBAMacroExtractor {
             output = new File(args[1]);
         }
         
-        VBAMacroExtractor extract = new VBAMacroExtractor();
-        extract.extract(input, output);
+        VBAMacroExtractor extractor = new VBAMacroExtractor();
+        extractor.extract(input, output);
     }
     
-    public void extract(File input, File outputDir) throws IOException {
+    /**
+      * Extracts the VBA modules from a macro-enabled office file and writes them
+      * to files in <tt>outputDir</tt>.
+      *
+      * Creates the <tt>outputDir</tt>, directory, including any necessary but
+      * nonexistent parent directories, if <tt>outputDir</tt> does not exist.
+      * If <tt>outputDir</tt> is null, writes the contents to standard out instead.
+      *
+      * @param input  the macro-enabled office file.
+      * @param outputDir  the directory to write the extracted VBA modules to.
+      * @param extension  file extension of the extracted VBA modules
+      * @since 3.15-beta2
+      */
+    public void extract(File input, File outputDir, String extension) throws IOException {
         if (! input.exists()) throw new FileNotFoundException(input.toString());
         System.err.print("Extracting VBA Macros from " + input + " to ");
         if (outputDir != null) {
-            if (! outputDir.exists()) outputDir.mkdir();
+            if (!outputDir.exists() && !outputDir.mkdirs()) {
+                throw new IOException("Output directory " + outputDir + " could not be created");
+            }
             System.err.println(outputDir);
         } else {
             System.err.println("STDOUT");
@@ -66,17 +84,19 @@ public class VBAMacroExtractor {
         reader.close();
         
         final String divider = "---------------------------------------";
-        for (String macro : macros.keySet()) {
+        for (Entry<String, String> entry : macros.entrySet()) {
+            String moduleName = entry.getKey();
+            String moduleCode = entry.getValue();
             if (outputDir == null) {
                 System.out.println(divider);
-                System.out.println(macro);
+                System.out.println(moduleName);
                 System.out.println("");
-                System.out.println(macros.get(macro));
+                System.out.println(moduleCode);
             } else {
-                File out = new File(outputDir, macro + ".vba");
+                File out = new File(outputDir, moduleName + extension);
                 FileOutputStream fout = new FileOutputStream(out);
                 OutputStreamWriter fwriter = new OutputStreamWriter(fout, StringUtil.UTF8);
-                fwriter.write(macros.get(macro));
+                fwriter.write(moduleCode);
                 fwriter.close();
                 fout.close();
                 System.out.println("Extracted " + out);
@@ -85,5 +105,21 @@ public class VBAMacroExtractor {
         if (outputDir == null) {
             System.out.println(divider);
         }
+    }
+
+    /**
+      * Extracts the VBA modules from a macro-enabled office file and writes them
+      * to <tt>.vba</tt> files in <tt>outputDir</tt>. 
+      * 
+      * Creates the <tt>outputDir</tt>, directory, including any necessary but
+      * nonexistent parent directories, if <tt>outputDir</tt> does not exist.
+      * If <tt>outputDir</tt> is null, writes the contents to standard out instead.
+      * 
+      * @param input  the macro-enabled office file.
+      * @param outputDir  the directory to write the extracted VBA modules to.
+      * @since 3.15-beta2
+      */
+    public void extract(File input, File outputDir) throws IOException {
+        extract(input, outputDir, ".vba");
     }
 }
