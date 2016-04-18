20b11d54b18ece53ca7b99546ebc0babdbade486
Javen O'Neal <onealj@apache.org>
2016-04-18 04:47:06 +0000

    remove unused import
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1739689 13f79535-47bb-0310-9956-ffa450edef68

--- src/ooxml/testcases/org/apache/poi/xssf/streaming/TestSXSSFSheet.java
+++ src/ooxml/testcases/org/apache/poi/xssf/streaming/TestSXSSFSheet.java
@@ -20,7 +20,6 @@
 package org.apache.poi.xssf.streaming;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertSame;
 import static org.junit.Assert.fail;
 
 import java.io.IOException;
