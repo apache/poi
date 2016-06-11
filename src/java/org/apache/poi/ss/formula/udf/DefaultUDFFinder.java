3ccf7eabf8e2865332622b51bc2cb49aa5531b3f
Javen O'Neal <onealj@apache.org>
2016-06-11 00:34:22 +0000

    whitespace (tabs to spaces)
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1747822 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/poi/ss/formula/udf/DefaultUDFFinder.java
+++ src/java/org/apache/poi/ss/formula/udf/DefaultUDFFinder.java
@@ -29,22 +29,22 @@ import org.apache.poi.ss.formula.functions.FreeRefFunction;
  * @author PUdalau
  */
 public final class DefaultUDFFinder implements UDFFinder {
-	private final Map<String, FreeRefFunction> _functionsByName;
+    private final Map<String, FreeRefFunction> _functionsByName;
 
-	public DefaultUDFFinder(String[] functionNames, FreeRefFunction[] functionImpls) {
-		int nFuncs = functionNames.length;
-		if (functionImpls.length != nFuncs) {
-			throw new IllegalArgumentException(
-					"Mismatch in number of function names and implementations");
-		}
-		HashMap<String, FreeRefFunction> m = new HashMap<String, FreeRefFunction>(nFuncs * 3 / 2);
-		for (int i = 0; i < functionImpls.length; i++) {
-			m.put(functionNames[i].toUpperCase(Locale.ROOT), functionImpls[i]);
-		}
-		_functionsByName = m;
-	}
+    public DefaultUDFFinder(String[] functionNames, FreeRefFunction[] functionImpls) {
+        int nFuncs = functionNames.length;
+        if (functionImpls.length != nFuncs) {
+            throw new IllegalArgumentException(
+                    "Mismatch in number of function names and implementations");
+        }
+        HashMap<String, FreeRefFunction> m = new HashMap<String, FreeRefFunction>(nFuncs * 3 / 2);
+        for (int i = 0; i < functionImpls.length; i++) {
+            m.put(functionNames[i].toUpperCase(Locale.ROOT), functionImpls[i]);
+        }
+        _functionsByName = m;
+    }
 
-	public FreeRefFunction findFunction(String name) {
-		return _functionsByName.get(name.toUpperCase(Locale.ROOT));
-	}
+    public FreeRefFunction findFunction(String name) {
+        return _functionsByName.get(name.toUpperCase(Locale.ROOT));
+    }
 }
