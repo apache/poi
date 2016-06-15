449e1eed209b555969e91e4d91f6a4958355d839
Javen O'Neal <onealj@apache.org>
2016-06-15 03:55:45 +0000

    whitespace (2 spaces or tabs to 4 spaces)
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1748489 13f79535-47bb-0310-9956-ffa450edef68

--- src/scratchpad/src/org/apache/poi/hwpf/model/FontTable.java
+++ src/scratchpad/src/org/apache/poi/hwpf/model/FontTable.java
@@ -1,18 +1,18 @@
 /* ====================================================================
-   Licensed to the Apache Software Foundation (ASF) under one or more
-   contributor license agreements.  See the NOTICE file distributed with
-   this work for additional information regarding copyright ownership.
-   The ASF licenses this file to You under the Apache License, Version 2.0
-   (the "License"); you may not use this file except in compliance with
-   the License.  You may obtain a copy of the License at
+     Licensed to the Apache Software Foundation (ASF) under one or more
+     contributor license agreements.    See the NOTICE file distributed with
+     this work for additional information regarding copyright ownership.
+     The ASF licenses this file to You under the Apache License, Version 2.0
+     (the "License"); you may not use this file except in compliance with
+     the License.    You may obtain a copy of the License at
 
-       http://www.apache.org/licenses/LICENSE-2.0
+             http://www.apache.org/licenses/LICENSE-2.0
 
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
+     Unless required by applicable law or agreed to in writing, software
+     distributed under the License is distributed on an "AS IS" BASIS,
+     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+     See the License for the specific language governing permissions and
+     limitations under the License.
 ==================================================================== */
 
 package org.apache.poi.hwpf.model;
@@ -37,83 +37,83 @@ import org.apache.poi.util.POILogger;
 @Internal
 public final class FontTable
 {
-  private final static POILogger _logger = POILogFactory.getLogger(FontTable.class);
-  private short _stringCount;// how many strings are included in the string table
-  private short _extraDataSz;// size in bytes of the extra data
+    private final static POILogger _logger = POILogFactory.getLogger(FontTable.class);
+    private short _stringCount;// how many strings are included in the string table
+    private short _extraDataSz;// size in bytes of the extra data
 
-  // added extra facilitator members
-  private int lcbSttbfffn;// count of bytes in sttbfffn
-  private int fcSttbfffn;// table stream offset for sttbfffn
+    // added extra facilitator members
+    private int lcbSttbfffn;// count of bytes in sttbfffn
+    private int fcSttbfffn;// table stream offset for sttbfffn
 
-  // FFN structure containing strings of font names
-  private Ffn[] _fontNames = null;
+    // FFN structure containing strings of font names
+    private Ffn[] _fontNames = null;
 
 
-  public FontTable(byte[] buf, int offset, int lcbSttbfffn)
-  {
-    this.lcbSttbfffn = lcbSttbfffn;
-    this.fcSttbfffn = offset;
-
-    _stringCount = LittleEndian.getShort(buf, offset);
-    offset += LittleEndian.SHORT_SIZE;
-    _extraDataSz = LittleEndian.getShort(buf, offset);
-    offset += LittleEndian.SHORT_SIZE;
-
-    _fontNames = new Ffn[_stringCount]; //Ffn corresponds to a Pascal style String in STTBF.
-
-    for(int i = 0;i<_stringCount; i++)
+    public FontTable(byte[] buf, int offset, int lcbSttbfffn)
     {
-      _fontNames[i] = new Ffn(buf,offset);
-      offset += _fontNames[i].getSize();
-    }
-  }
+        this.lcbSttbfffn = lcbSttbfffn;
+        this.fcSttbfffn = offset;
 
-  public short getStringCount()
-  {
-    return  _stringCount;
-  }
+        _stringCount = LittleEndian.getShort(buf, offset);
+        offset += LittleEndian.SHORT_SIZE;
+        _extraDataSz = LittleEndian.getShort(buf, offset);
+        offset += LittleEndian.SHORT_SIZE;
 
-  public short getExtraDataSz()
-  {
-  	return _extraDataSz;
-  }
+        _fontNames = new Ffn[_stringCount]; //Ffn corresponds to a Pascal style String in STTBF.
 
-  public Ffn[] getFontNames()
-  {
-  	return _fontNames;
-  }
-
-  public int getSize()
-  {
-    return lcbSttbfffn;
-  }
-
-  public String getMainFont(int chpFtc )
-  {
-    if(chpFtc >= _stringCount)
-    {
-      _logger.log(POILogger.INFO, "Mismatch in chpFtc with stringCount");
-      return null;
+        for(int i = 0;i<_stringCount; i++)
+        {
+            _fontNames[i] = new Ffn(buf,offset);
+            offset += _fontNames[i].getSize();
+        }
     }
 
-    return _fontNames[chpFtc].getMainFontName();
-  }
-
-  public String getAltFont(int chpFtc )
-  {
-    if(chpFtc >= _stringCount)
+    public short getStringCount()
     {
-      _logger.log(POILogger.INFO, "Mismatch in chpFtc with stringCount");
-      return null;
+        return _stringCount;
     }
 
-    return _fontNames[chpFtc].getAltFontName();
-  }
+    public short getExtraDataSz()
+    {
+        return _extraDataSz;
+    }
 
-  public void setStringCount(short stringCount)
-  {
-    this._stringCount = stringCount;
-  }
+    public Ffn[] getFontNames()
+    {
+        return _fontNames;
+    }
+
+    public int getSize()
+    {
+        return lcbSttbfffn;
+    }
+
+    public String getMainFont(int chpFtc )
+    {
+        if(chpFtc >= _stringCount)
+        {
+            _logger.log(POILogger.INFO, "Mismatch in chpFtc with stringCount");
+            return null;
+        }
+
+        return _fontNames[chpFtc].getMainFontName();
+    }
+
+    public String getAltFont(int chpFtc )
+    {
+        if(chpFtc >= _stringCount)
+        {
+            _logger.log(POILogger.INFO, "Mismatch in chpFtc with stringCount");
+            return null;
+        }
+
+        return _fontNames[chpFtc].getAltFontName();
+    }
+
+    public void setStringCount(short stringCount)
+    {
+        this._stringCount = stringCount;
+    }
 
     @Deprecated
     public void writeTo( HWPFFileSystem sys ) throws IOException
@@ -124,40 +124,40 @@ public final class FontTable
 
     public void writeTo( HWPFOutputStream tableStream ) throws IOException
     {
-	  byte[] buf = new byte[LittleEndian.SHORT_SIZE];
-	  LittleEndian.putShort(buf, 0, _stringCount);
-	  tableStream.write(buf);
-	  LittleEndian.putShort(buf, 0, _extraDataSz);
-	  tableStream.write(buf);
+        byte[] buf = new byte[LittleEndian.SHORT_SIZE];
+        LittleEndian.putShort(buf, 0, _stringCount);
+        tableStream.write(buf);
+        LittleEndian.putShort(buf, 0, _extraDataSz);
+        tableStream.write(buf);
 
-	  for(int i = 0; i < _fontNames.length; i++)
-	  {
-		  tableStream.write(_fontNames[i].toByteArray());
-	  }
+        for(int i = 0; i < _fontNames.length; i++)
+        {
+            tableStream.write(_fontNames[i].toByteArray());
+        }
 
-  }
+    }
 
-  @Override
-  public boolean equals(Object other) {
-      if (!(other instanceof FontTable)) return false;
-      FontTable o = (FontTable)other;
+    @Override
+    public boolean equals(Object other) {
+        if (!(other instanceof FontTable)) return false;
+        FontTable o = (FontTable)other;
 
-      if (o._stringCount != this._stringCount
-          || o._extraDataSz != this._extraDataSz
-          || o._fontNames.length != this._fontNames.length
-      ) return false;
-      
-      for (int i=0; i<o._fontNames.length; i++) {
-          if (!o._fontNames[i].equals(this._fontNames[i])) return false;
-      }
-      
-      return true;
-  }
+        if (o._stringCount != this._stringCount
+                || o._extraDataSz != this._extraDataSz
+                || o._fontNames.length != this._fontNames.length
+        ) return false;
+        
+        for (int i=0; i<o._fontNames.length; i++) {
+            if (!o._fontNames[i].equals(this._fontNames[i])) return false;
+        }
+        
+        return true;
+    }
 
-  @Override
-  public int hashCode() {
-      assert false : "hashCode not designed";
-      return 42; // any arbitrary constant will do
-  }
+    @Override
+    public int hashCode() {
+        assert false : "hashCode not designed";
+        return 42; // any arbitrary constant will do
+    }
 
 }
--- src/scratchpad/src/org/apache/poi/hwpf/model/PAPX.java
+++ src/scratchpad/src/org/apache/poi/hwpf/model/PAPX.java
@@ -1,18 +1,18 @@
 /* ====================================================================
-   Licensed to the Apache Software Foundation (ASF) under one or more
-   contributor license agreements.  See the NOTICE file distributed with
-   this work for additional information regarding copyright ownership.
-   The ASF licenses this file to You under the Apache License, Version 2.0
-   (the "License"); you may not use this file except in compliance with
-   the License.  You may obtain a copy of the License at
+     Licensed to the Apache Software Foundation (ASF) under one or more
+     contributor license agreements.    See the NOTICE file distributed with
+     this work for additional information regarding copyright ownership.
+     The ASF licenses this file to You under the Apache License, Version 2.0
+     (the "License"); you may not use this file except in compliance with
+     the License.    You may obtain a copy of the License at
 
-       http://www.apache.org/licenses/LICENSE-2.0
+             http://www.apache.org/licenses/LICENSE-2.0
 
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
+     Unless required by applicable law or agreed to in writing, software
+     distributed under the License is distributed on an "AS IS" BASIS,
+     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+     See the License for the specific language governing permissions and
+     limitations under the License.
 ==================================================================== */
 
 package org.apache.poi.hwpf.model;
@@ -29,8 +29,8 @@ import org.apache.poi.util.LittleEndian;
  * DANGER - works in bytes!
  *
  * Make sure you call getStart() / getEnd() when you want characters
- *  (normal use), but getStartByte() / getEndByte() when you're
- *  reading in / writing out!
+ *    (normal use), but getStartByte() / getEndByte() when you're
+ *    reading in / writing out!
  *
  * @author Ryan Ackley
  */
@@ -38,16 +38,16 @@ import org.apache.poi.util.LittleEndian;
 @SuppressWarnings( "deprecation" )
 public final class PAPX extends BytePropertyNode<PAPX> {
 
-  private ParagraphHeight _phe;
+    private ParagraphHeight _phe;
 
-  public PAPX(int fcStart, int fcEnd, CharIndexTranslator translator, byte[] papx, ParagraphHeight phe, byte[] dataStream)
-  {
-    super(fcStart, fcEnd, translator, new SprmBuffer(papx, 2));
-    _phe = phe;
-    SprmBuffer buf = findHuge(new SprmBuffer(papx, 2), dataStream);
-    if(buf != null)
-      _buf = buf;
-  }
+    public PAPX(int fcStart, int fcEnd, CharIndexTranslator translator, byte[] papx, ParagraphHeight phe, byte[] dataStream)
+    {
+        super(fcStart, fcEnd, translator, new SprmBuffer(papx, 2));
+        _phe = phe;
+        SprmBuffer buf = findHuge(new SprmBuffer(papx, 2), dataStream);
+        if(buf != null)
+            _buf = buf;
+    }
 
     public PAPX( int charStart, int charEnd, byte[] papx, ParagraphHeight phe,
             byte[] dataStream )
@@ -60,14 +60,14 @@ public final class PAPX extends BytePropertyNode<PAPX> {
     }
 
     @Deprecated
-  public PAPX(int fcStart, int fcEnd, CharIndexTranslator translator, SprmBuffer buf, byte[] dataStream)
-  {
-    super(fcStart, fcEnd, translator, buf);
-    _phe = new ParagraphHeight();
-    buf = findHuge(buf, dataStream);
-    if(buf != null)
-      _buf = buf;
-  }
+    public PAPX(int fcStart, int fcEnd, CharIndexTranslator translator, SprmBuffer buf, byte[] dataStream)
+    {
+        super(fcStart, fcEnd, translator, buf);
+        _phe = new ParagraphHeight();
+        buf = findHuge(buf, dataStream);
+        if(buf != null)
+            _buf = buf;
+    }
 
     public PAPX( int charStart, int charEnd, SprmBuffer buf )
     {
@@ -75,105 +75,105 @@ public final class PAPX extends BytePropertyNode<PAPX> {
         _phe = new ParagraphHeight();
     }
 
-  private SprmBuffer findHuge(SprmBuffer buf, byte[] datastream)
-  {
-    byte[] grpprl = buf.toByteArray();
-    if(grpprl.length==8 && datastream!=null) // then check for sprmPHugePapx
+    private SprmBuffer findHuge(SprmBuffer buf, byte[] datastream)
     {
-      SprmOperation sprm = new SprmOperation(grpprl, 2);
-      if ((sprm.getOperation()==0x45 || sprm.getOperation()==0x46)
-          && sprm.getSizeCode() == 3)
-      {
-        int hugeGrpprlOffset = sprm.getOperand();
-        if(hugeGrpprlOffset+1 < datastream.length)
+        byte[] grpprl = buf.toByteArray();
+        if(grpprl.length==8 && datastream!=null) // then check for sprmPHugePapx
         {
-          int grpprlSize = LittleEndian.getShort(datastream, hugeGrpprlOffset);
-          if( hugeGrpprlOffset+grpprlSize < datastream.length)
-          {
-            byte[] hugeGrpprl = new byte[grpprlSize + 2];
-            // copy original istd into huge Grpprl
-            hugeGrpprl[0] = grpprl[0]; hugeGrpprl[1] = grpprl[1];
-            // copy Grpprl from dataStream
-            System.arraycopy(datastream, hugeGrpprlOffset + 2, hugeGrpprl, 2,
-                             grpprlSize);
-            return new SprmBuffer(hugeGrpprl, 2);
-          }
+            SprmOperation sprm = new SprmOperation(grpprl, 2);
+            if ((sprm.getOperation()==0x45 || sprm.getOperation()==0x46)
+                    && sprm.getSizeCode() == 3)
+            {
+                int hugeGrpprlOffset = sprm.getOperand();
+                if(hugeGrpprlOffset+1 < datastream.length)
+                {
+                    int grpprlSize = LittleEndian.getShort(datastream, hugeGrpprlOffset);
+                    if( hugeGrpprlOffset+grpprlSize < datastream.length)
+                    {
+                        byte[] hugeGrpprl = new byte[grpprlSize + 2];
+                        // copy original istd into huge Grpprl
+                        hugeGrpprl[0] = grpprl[0]; hugeGrpprl[1] = grpprl[1];
+                        // copy Grpprl from dataStream
+                        System.arraycopy(datastream, hugeGrpprlOffset + 2, hugeGrpprl, 2,
+                                                         grpprlSize);
+                        return new SprmBuffer(hugeGrpprl, 2);
+                    }
+                }
+            }
         }
-      }
+        return null;
     }
-    return null;
-  }
 
 
-  public ParagraphHeight getParagraphHeight()
-  {
-    return _phe;
-  }
+    public ParagraphHeight getParagraphHeight()
+    {
+        return _phe;
+    }
 
-  public byte[] getGrpprl()
-  {
-      if (_buf == null)
-          return new byte[0];
+    public byte[] getGrpprl()
+    {
+        if (_buf == null)
+            return new byte[0];
 
-    return ((SprmBuffer)_buf).toByteArray();
-  }
+        return ((SprmBuffer)_buf).toByteArray();
+    }
 
     public short getIstd()
     {
         if ( _buf == null )
             return 0;
+    
+        byte[] buf = getGrpprl();
+        if (buf.length == 0)
+        {
+            return 0;
+        }
+        if (buf.length == 1)
+        {
+            return LittleEndian.getUByte(buf, 0);
+        }
+        return LittleEndian.getShort(buf);
+    }
 
-    byte[] buf = getGrpprl();
-    if (buf.length == 0)
+    public SprmBuffer getSprmBuf()
     {
-      return 0;
+        return (SprmBuffer)_buf;
     }
-    if (buf.length == 1)
+
+    @Deprecated
+    @Internal
+    public ParagraphProperties getParagraphProperties(StyleSheet ss)
     {
-      return LittleEndian.getUByte(buf, 0);
+        if(ss == null) {
+            // TODO Fix up for Word 6/95
+            return new ParagraphProperties();
+        }
+            
+        short istd = getIstd();
+        ParagraphProperties baseStyle = ss.getParagraphStyle(istd);
+        ParagraphProperties props = ParagraphSprmUncompressor.uncompressPAP(baseStyle, getGrpprl(), 2);
+        return props;
     }
-    return LittleEndian.getShort(buf);
-  }
 
-  public SprmBuffer getSprmBuf()
-  {
-    return (SprmBuffer)_buf;
-  }
-
-  @Deprecated
-  @Internal
-  public ParagraphProperties getParagraphProperties(StyleSheet ss)
-  {
-    if(ss == null) {
-        // TODO Fix up for Word 6/95
-        return new ParagraphProperties();
-    }
-      
-    short istd = getIstd();
-    ParagraphProperties baseStyle = ss.getParagraphStyle(istd);
-    ParagraphProperties props = ParagraphSprmUncompressor.uncompressPAP(baseStyle, getGrpprl(), 2);
-    return props;
-  }
-
-  @Override
-  public boolean equals(Object o)
-  {
-    if (super.equals(o))
+    @Override
+    public boolean equals(Object o)
     {
-      return _phe.equals(((PAPX)o)._phe);
+        if (super.equals(o))
+        {
+            return _phe.equals(((PAPX)o)._phe);
+        }
+        return false;
     }
-    return false;
-  }
 
-  @Override
-  public int hashCode() {
-      assert false : "hashCode not designed";
-      return 42; // any arbitrary constant will do
-  }
+    @Override
+    public int hashCode() {
+        assert false : "hashCode not designed";
+        return 42; // any arbitrary constant will do
+    }
 
     public String toString()
     {
         return "PAPX from " + getStart() + " to " + getEnd() + " (in bytes "
-                + getStartBytes() + " to " + getEndBytes() + ")";
+                            + getStartBytes() + " to " + getEndBytes() + ")";
     }
 }
