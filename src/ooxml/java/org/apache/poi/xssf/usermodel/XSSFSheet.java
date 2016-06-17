26d3072b1e2fa631652f67ed870283affd3eb4ad
Javen O'Neal <onealj@apache.org>
2016-06-17 10:22:09 +0000

    bug 56958: validate array formulas wasn't correctly checking for overlap with a merged region
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1748829 13f79535-47bb-0310-9956-ffa450edef68

--- src/ooxml/java/org/apache/poi/xssf/usermodel/XSSFSheet.java
+++ src/ooxml/java/org/apache/poi/xssf/usermodel/XSSFSheet.java
@@ -395,10 +395,7 @@ public class XSSFSheet extends POIXMLDocumentPart implements Sheet {
 
                 if(cell.isPartOfArrayFormulaGroup()){
                     CellRangeAddress arrayRange = cell.getArrayFormulaRange();
-                    if (arrayRange.getNumberOfCells() > 1 &&
-                            // region.intersects(arrayRange) is more concise and probably correct. Is it equivalent?
-                            ( arrayRange.isInRange(region.getFirstRow(), region.getFirstColumn()) ||
-                              arrayRange.isInRange(region.getFirstRow(), region.getFirstColumn()))  ){
+                    if (arrayRange.getNumberOfCells() > 1 && region.intersects(arrayRange)) {
                         String msg = "The range " + region.formatAsString() + " intersects with a multi-cell array formula. " +
                                 "You cannot merge cells of an array.";
                         throw new IllegalStateException(msg);
