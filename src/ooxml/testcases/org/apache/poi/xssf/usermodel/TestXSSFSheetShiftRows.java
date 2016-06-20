0aed737c34ad1a7155111a42ea241dccd7c0f29f
Javen O'Neal <onealj@apache.org>
2016-06-20 04:21:26 +0000

    whitespace (tabs to 4 spaces)
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1749264 13f79535-47bb-0310-9956-ffa450edef68

--- src/ooxml/testcases/org/apache/poi/xssf/usermodel/TestXSSFSheetShiftRows.java
+++ src/ooxml/testcases/org/apache/poi/xssf/usermodel/TestXSSFSheetShiftRows.java
@@ -44,115 +44,115 @@ public final class TestXSSFSheetShiftRows extends BaseTestSheetShiftRows {
 
     @Override
     @Test
-	public void testShiftRowBreaks() {
+    public void testShiftRowBreaks() {
         // disabled test from superclass
         // TODO - support shifting of page breaks
     }
 
     @Test
-	public void testBug54524() throws IOException {
+    public void testBug54524() throws IOException {
         XSSFWorkbook workbook = XSSFTestDataSamples.openSampleWorkbook("54524.xlsx");
         XSSFSheet sheet = workbook.getSheetAt(0);
-		sheet.shiftRows(3, 5, -1);
+        sheet.shiftRows(3, 5, -1);
 
         Cell cell = CellUtil.getCell(sheet.getRow(1), 0);
-		assertEquals(1.0, cell.getNumericCellValue(), 0);
-		cell = CellUtil.getCell(sheet.getRow(2), 0);
-		assertEquals("SUM(A2:A2)", cell.getCellFormula());
-		cell = CellUtil.getCell(sheet.getRow(3), 0);
-		assertEquals("X", cell.getStringCellValue());
-		workbook.close();
-	}
+        assertEquals(1.0, cell.getNumericCellValue(), 0);
+        cell = CellUtil.getCell(sheet.getRow(2), 0);
+        assertEquals("SUM(A2:A2)", cell.getCellFormula());
+        cell = CellUtil.getCell(sheet.getRow(3), 0);
+        assertEquals("X", cell.getStringCellValue());
+        workbook.close();
+    }
 
     @Test
-	public void testBug53798() throws IOException {
-		// NOTE that for HSSF (.xls) negative shifts combined with positive ones do work as expected  
-		Workbook wb = XSSFTestDataSamples.openSampleWorkbook("53798.xlsx");
+    public void testBug53798() throws IOException {
+        // NOTE that for HSSF (.xls) negative shifts combined with positive ones do work as expected  
+        Workbook wb = XSSFTestDataSamples.openSampleWorkbook("53798.xlsx");
 
-		Sheet testSheet	= wb.getSheetAt(0);
-		// 1) corrupted xlsx (unreadable data in the first row of a shifted group) already comes about  
-		// when shifted by less than -1 negative amount (try -2)
-		testSheet.shiftRows(3, 3, -2);
-		
-		Row newRow = null; Cell newCell = null;
-		// 2) attempt to create a new row IN PLACE of a removed row by a negative shift causes corrupted 
-		// xlsx file with  unreadable data in the negative shifted row. 
-		// NOTE it's ok to create any other row.
-		newRow = testSheet.createRow(3);
-		newCell = newRow.createCell(0);
-		newCell.setCellValue("new Cell in row "+newRow.getRowNum());
-		
-		// 3) once a negative shift has been made any attempt to shift another group of rows 
-		// (note: outside of previously negative shifted rows) by a POSITIVE amount causes POI exception: 
-		// org.apache.xmlbeans.impl.values.XmlValueDisconnectedException.
-		// NOTE: another negative shift on another group of rows is successful, provided no new rows in  
-		// place of previously shifted rows were attempted to be created as explained above.
+        Sheet testSheet    = wb.getSheetAt(0);
+        // 1) corrupted xlsx (unreadable data in the first row of a shifted group) already comes about  
+        // when shifted by less than -1 negative amount (try -2)
+        testSheet.shiftRows(3, 3, -2);
+        
+        Row newRow = null; Cell newCell = null;
+        // 2) attempt to create a new row IN PLACE of a removed row by a negative shift causes corrupted 
+        // xlsx file with  unreadable data in the negative shifted row. 
+        // NOTE it's ok to create any other row.
+        newRow = testSheet.createRow(3);
+        newCell = newRow.createCell(0);
+        newCell.setCellValue("new Cell in row "+newRow.getRowNum());
+        
+        // 3) once a negative shift has been made any attempt to shift another group of rows 
+        // (note: outside of previously negative shifted rows) by a POSITIVE amount causes POI exception: 
+        // org.apache.xmlbeans.impl.values.XmlValueDisconnectedException.
+        // NOTE: another negative shift on another group of rows is successful, provided no new rows in  
+        // place of previously shifted rows were attempted to be created as explained above.
 
-		// -- CHANGE the shift to positive once the behaviour of the above has been tested
-		testSheet.shiftRows(6, 7, 1); 
-		
-		Workbook read = XSSFTestDataSamples.writeOutAndReadBack(wb);
-		wb.close();
-		assertNotNull(read);
-		
-		Sheet readSheet = read.getSheetAt(0);
-		verifyCellContent(readSheet, 0, "0.0");
-		verifyCellContent(readSheet, 1, "3.0");
-		verifyCellContent(readSheet, 2, "2.0");
-		verifyCellContent(readSheet, 3, "new Cell in row 3");
-		verifyCellContent(readSheet, 4, "4.0");
-		verifyCellContent(readSheet, 5, "5.0");
-		verifyCellContent(readSheet, 6, null);
-		verifyCellContent(readSheet, 7, "6.0");
-		verifyCellContent(readSheet, 8, "7.0");
-		read.close();
-	}
+        // -- CHANGE the shift to positive once the behaviour of the above has been tested
+        testSheet.shiftRows(6, 7, 1); 
+        
+        Workbook read = XSSFTestDataSamples.writeOutAndReadBack(wb);
+        wb.close();
+        assertNotNull(read);
+        
+        Sheet readSheet = read.getSheetAt(0);
+        verifyCellContent(readSheet, 0, "0.0");
+        verifyCellContent(readSheet, 1, "3.0");
+        verifyCellContent(readSheet, 2, "2.0");
+        verifyCellContent(readSheet, 3, "new Cell in row 3");
+        verifyCellContent(readSheet, 4, "4.0");
+        verifyCellContent(readSheet, 5, "5.0");
+        verifyCellContent(readSheet, 6, null);
+        verifyCellContent(readSheet, 7, "6.0");
+        verifyCellContent(readSheet, 8, "7.0");
+        read.close();
+    }
 
-	private void verifyCellContent(Sheet readSheet, int row, String expect) {
-		Row readRow = readSheet.getRow(row);
-		if(expect == null) {
-			assertNull(readRow);
-			return;
-		}
-		Cell readCell = readRow.getCell(0);
-		if(readCell.getCellType() == Cell.CELL_TYPE_NUMERIC) {
-			assertEquals(expect, Double.toString(readCell.getNumericCellValue()));
-		} else {
-			assertEquals(expect, readCell.getStringCellValue());
-		}
-	}
-	
-	@Test
-	public void testBug53798a() throws IOException {
-		Workbook wb = XSSFTestDataSamples.openSampleWorkbook("53798.xlsx");
-
-		Sheet testSheet	= wb.getSheetAt(0);
-		testSheet.shiftRows(3, 3, -1);
-        for (Row r : testSheet) {
-        	r.getRowNum();
+    private void verifyCellContent(Sheet readSheet, int row, String expect) {
+        Row readRow = readSheet.getRow(row);
+        if(expect == null) {
+            assertNull(readRow);
+            return;
         }
-		testSheet.shiftRows(6, 6, 1);
-		
-		Workbook read = XSSFTestDataSamples.writeOutAndReadBack(wb);
-		wb.close();
-		assertNotNull(read);
-		
-		Sheet readSheet = read.getSheetAt(0);
-		verifyCellContent(readSheet, 0, "0.0");
-		verifyCellContent(readSheet, 1, "1.0");
-		verifyCellContent(readSheet, 2, "3.0");
-		verifyCellContent(readSheet, 3, null);
-		verifyCellContent(readSheet, 4, "4.0");
-		verifyCellContent(readSheet, 5, "5.0");
-		verifyCellContent(readSheet, 6, null);
-		verifyCellContent(readSheet, 7, "6.0");
-		verifyCellContent(readSheet, 8, "8.0");
-		read.close();
-	}
-	
-	@Test
-	public void testBug56017() throws IOException {
-	    Workbook wb = XSSFTestDataSamples.openSampleWorkbook("56017.xlsx");
+        Cell readCell = readRow.getCell(0);
+        if(readCell.getCellType() == Cell.CELL_TYPE_NUMERIC) {
+            assertEquals(expect, Double.toString(readCell.getNumericCellValue()));
+        } else {
+            assertEquals(expect, readCell.getStringCellValue());
+        }
+    }
+    
+    @Test
+    public void testBug53798a() throws IOException {
+        Workbook wb = XSSFTestDataSamples.openSampleWorkbook("53798.xlsx");
+
+        Sheet testSheet    = wb.getSheetAt(0);
+        testSheet.shiftRows(3, 3, -1);
+        for (Row r : testSheet) {
+            r.getRowNum();
+        }
+        testSheet.shiftRows(6, 6, 1);
+        
+        Workbook read = XSSFTestDataSamples.writeOutAndReadBack(wb);
+        wb.close();
+        assertNotNull(read);
+        
+        Sheet readSheet = read.getSheetAt(0);
+        verifyCellContent(readSheet, 0, "0.0");
+        verifyCellContent(readSheet, 1, "1.0");
+        verifyCellContent(readSheet, 2, "3.0");
+        verifyCellContent(readSheet, 3, null);
+        verifyCellContent(readSheet, 4, "4.0");
+        verifyCellContent(readSheet, 5, "5.0");
+        verifyCellContent(readSheet, 6, null);
+        verifyCellContent(readSheet, 7, "6.0");
+        verifyCellContent(readSheet, 8, "8.0");
+        read.close();
+    }
+    
+    @Test
+    public void testBug56017() throws IOException {
+        Workbook wb = XSSFTestDataSamples.openSampleWorkbook("56017.xlsx");
 
         Sheet sheet = wb.getSheetAt(0);
 
@@ -189,11 +189,11 @@ public final class TestXSSFSheetShiftRows extends BaseTestSheetShiftRows {
         assertEquals("Amdocs", comment.getAuthor());
         assertEquals("Amdocs:\ntest\n", comment.getString().getString());
         wbBack.close();
-	}
+    }
 
-	@Test
+    @Test
     public void test57171() throws IOException {
-	    Workbook wb = XSSFTestDataSamples.openSampleWorkbook("57171_57163_57165.xlsx");
+        Workbook wb = XSSFTestDataSamples.openSampleWorkbook("57171_57163_57165.xlsx");
         assertEquals(5, wb.getActiveSheetIndex());
         removeAllSheetsBut(5, wb); // 5 is the active / selected sheet
         assertEquals(0, wb.getActiveSheetIndex());
@@ -208,7 +208,7 @@ public final class TestXSSFSheetShiftRows extends BaseTestSheetShiftRows {
         wbRead.close();
     }
 
-	@Test
+    @Test
     public void test57163() throws IOException {
         Workbook wb = XSSFTestDataSamples.openSampleWorkbook("57171_57163_57165.xlsx");
         assertEquals(5, wb.getActiveSheetIndex());
@@ -218,7 +218,7 @@ public final class TestXSSFSheetShiftRows extends BaseTestSheetShiftRows {
         wb.close();
     }
 
-	@Test
+    @Test
     public void testSetSheetOrderAndAdjustActiveSheet() throws IOException {
         Workbook wb = XSSFTestDataSamples.openSampleWorkbook("57171_57163_57165.xlsx");
         
@@ -268,7 +268,7 @@ public final class TestXSSFSheetShiftRows extends BaseTestSheetShiftRows {
         wb.close();
     }   
 
-	@Test
+    @Test
     public void testRemoveSheetAndAdjustActiveSheet() throws IOException {
         Workbook wb = XSSFTestDataSamples.openSampleWorkbook("57171_57163_57165.xlsx");
         
@@ -312,7 +312,7 @@ public final class TestXSSFSheetShiftRows extends BaseTestSheetShiftRows {
         wb.close();
     }
 
-	@Test
+    @Test
     public void test57165() throws IOException {
         Workbook wb = XSSFTestDataSamples.openSampleWorkbook("57171_57163_57165.xlsx");
         assertEquals(5, wb.getActiveSheetIndex());
