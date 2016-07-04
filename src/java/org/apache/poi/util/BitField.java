f5b531db589ad94e9cd84796ae5f3eece68dce4f
Javen O'Neal <onealj@apache.org>
2016-07-04 19:00:35 +0000

    typo
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1751372 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/poi/util/BitField.java
+++ src/java/org/apache/poi/util/BitField.java
@@ -25,7 +25,7 @@ package org.apache.poi.util;
  * @author Marc Johnson (mjohnson at apache dot org)
  * @author Andrew C. Oliver (acoliver at apache dot org)
  */
-@Interal
+@Internal
 public class BitField
 {
     private final int _mask;
