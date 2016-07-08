06e170fffc23c04b991dfb0b4b370b4f844cc633
Javen O'Neal <onealj@apache.org>
2016-07-08 08:38:28 +0000

    whitespace and +svn:eol-style=native
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1751867 13f79535-47bb-0310-9956-ffa450edef68

--- src/ooxml/testcases/org/apache/poi/openxml4j/opc/internal/TestContentTypeManager.java
+++ src/ooxml/testcases/org/apache/poi/openxml4j/opc/internal/TestContentTypeManager.java
@@ -30,81 +30,81 @@ import org.apache.poi.openxml4j.opc.PackagingURIHelper;
 
 public final class TestContentTypeManager extends TestCase {
 
-	/**
-	 * Test the properties part content parsing.
-	 */
-	public void disabled_testContentType() throws Exception {
-		String filepath =  OpenXML4JTestDataSamples.getSampleFileName("sample.docx");
+    /**
+     * Test the properties part content parsing.
+     */
+    public void disabled_testContentType() throws Exception {
+        String filepath =  OpenXML4JTestDataSamples.getSampleFileName("sample.docx");
 
-		 // Retrieves core properties part
-		 OPCPackage p = OPCPackage.open(filepath, PackageAccess.READ);
-		 PackageRelationship corePropertiesRelationship = p
-		 .getRelationshipsByType(
-		 PackageRelationshipTypes.CORE_PROPERTIES)
-		 .getRelationship(0);
-		 PackagePart coreDocument = p.getPart(corePropertiesRelationship);
+         // Retrieves core properties part
+         OPCPackage p = OPCPackage.open(filepath, PackageAccess.READ);
+         PackageRelationship corePropertiesRelationship = p
+         .getRelationshipsByType(
+         PackageRelationshipTypes.CORE_PROPERTIES)
+         .getRelationship(0);
+         PackagePart coreDocument = p.getPart(corePropertiesRelationship);
 
-		 ContentTypeManager ctm = new ZipContentTypeManager(coreDocument.getInputStream(), p);
+         ContentTypeManager ctm = new ZipContentTypeManager(coreDocument.getInputStream(), p);
 
-		 // TODO - finish writing this test
-		fail();
-	}
+         // TODO - finish writing this test
+        fail();
+    }
 
-	/**
-	 * Test the addition of several default and override content types.
-	 */
-	public void testContentTypeAddition() throws Exception {
-		ContentTypeManager ctm = new ZipContentTypeManager(null, null);
+    /**
+     * Test the addition of several default and override content types.
+     */
+    public void testContentTypeAddition() throws Exception {
+        ContentTypeManager ctm = new ZipContentTypeManager(null, null);
 
-		PackagePartName name1 = PackagingURIHelper.createPartName("/foo/foo.XML");
-		PackagePartName name2 = PackagingURIHelper.createPartName("/foo/foo2.xml");
-		PackagePartName name3 = PackagingURIHelper.createPartName("/foo/doc.rels");
-		PackagePartName name4 = PackagingURIHelper.createPartName("/foo/doc.RELS");
+        PackagePartName name1 = PackagingURIHelper.createPartName("/foo/foo.XML");
+        PackagePartName name2 = PackagingURIHelper.createPartName("/foo/foo2.xml");
+        PackagePartName name3 = PackagingURIHelper.createPartName("/foo/doc.rels");
+        PackagePartName name4 = PackagingURIHelper.createPartName("/foo/doc.RELS");
 
-		// Add content types
-		ctm.addContentType(name1, "foo-type1");
-		ctm.addContentType(name2, "foo-type2");
-		ctm.addContentType(name3, "text/xml+rel");
-		ctm.addContentType(name4, "text/xml+rel");
+        // Add content types
+        ctm.addContentType(name1, "foo-type1");
+        ctm.addContentType(name2, "foo-type2");
+        ctm.addContentType(name3, "text/xml+rel");
+        ctm.addContentType(name4, "text/xml+rel");
 
-		assertEquals(ctm.getContentType(name1), "foo-type1");
-		assertEquals(ctm.getContentType(name2), "foo-type2");
-		assertEquals(ctm.getContentType(name3), "text/xml+rel");
-		assertEquals(ctm.getContentType(name3), "text/xml+rel");
-	}
+        assertEquals(ctm.getContentType(name1), "foo-type1");
+        assertEquals(ctm.getContentType(name2), "foo-type2");
+        assertEquals(ctm.getContentType(name3), "text/xml+rel");
+        assertEquals(ctm.getContentType(name3), "text/xml+rel");
+    }
 
-	/**
-	 * Test the addition then removal of content types.
-	 */
-	public void testContentTypeRemoval() throws Exception {
-		ContentTypeManager ctm = new ZipContentTypeManager(null, null);
+    /**
+     * Test the addition then removal of content types.
+     */
+    public void testContentTypeRemoval() throws Exception {
+        ContentTypeManager ctm = new ZipContentTypeManager(null, null);
 
-		PackagePartName name1 = PackagingURIHelper.createPartName("/foo/foo.xml");
-		PackagePartName name2 = PackagingURIHelper.createPartName("/foo/foo2.xml");
-		PackagePartName name3 = PackagingURIHelper.createPartName("/foo/doc.rels");
-		PackagePartName name4 = PackagingURIHelper.createPartName("/foo/doc.RELS");
+        PackagePartName name1 = PackagingURIHelper.createPartName("/foo/foo.xml");
+        PackagePartName name2 = PackagingURIHelper.createPartName("/foo/foo2.xml");
+        PackagePartName name3 = PackagingURIHelper.createPartName("/foo/doc.rels");
+        PackagePartName name4 = PackagingURIHelper.createPartName("/foo/doc.RELS");
 
-		// Add content types
-		ctm.addContentType(name1, "foo-type1");
-		ctm.addContentType(name2, "foo-type2");
-		ctm.addContentType(name3, "text/xml+rel");
-		ctm.addContentType(name4, "text/xml+rel");
-		ctm.removeContentType(name2);
-		ctm.removeContentType(name3);
+        // Add content types
+        ctm.addContentType(name1, "foo-type1");
+        ctm.addContentType(name2, "foo-type2");
+        ctm.addContentType(name3, "text/xml+rel");
+        ctm.addContentType(name4, "text/xml+rel");
+        ctm.removeContentType(name2);
+        ctm.removeContentType(name3);
 
-		assertEquals(ctm.getContentType(name1), "foo-type1");
-		assertEquals(ctm.getContentType(name2), "foo-type1");
-		assertEquals(ctm.getContentType(name3), null);
+        assertEquals(ctm.getContentType(name1), "foo-type1");
+        assertEquals(ctm.getContentType(name2), "foo-type1");
+        assertEquals(ctm.getContentType(name3), null);
 
-		ctm.removeContentType(name1);
-		assertEquals(ctm.getContentType(name1), null);
-		assertEquals(ctm.getContentType(name2), null);
-	}
+        ctm.removeContentType(name1);
+        assertEquals(ctm.getContentType(name1), null);
+        assertEquals(ctm.getContentType(name2), null);
+    }
 
-	/**
-	 * Test the addition then removal of content types in a package.
-	 */
-	public void testContentTypeRemovalPackage() {
-		// TODO
-	}
+    /**
+     * Test the addition then removal of content types in a package.
+     */
+    public void testContentTypeRemovalPackage() {
+        // TODO
+    }
 }
