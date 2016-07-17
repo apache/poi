efbfef84a6adc7ba9a7d1650a1df8798be1f02a3
Javen O'Neal <onealj@apache.org>
2016-07-17 07:29:40 +0000

    make private maps final
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1753018 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/poi/ss/usermodel/FormulaError.java
+++ src/java/org/apache/poi/ss/usermodel/FormulaError.java
@@ -147,9 +147,9 @@ public enum FormulaError {
         return repr;
     }
 
-    private static Map<String, FormulaError> smap = new HashMap<String, FormulaError>();
-    private static Map<Byte, FormulaError> bmap = new HashMap<Byte, FormulaError>();
-    private static Map<Integer, FormulaError> imap = new HashMap<Integer, FormulaError>();
+    private static final Map<String, FormulaError> smap = new HashMap<String, FormulaError>();
+    private static final Map<Byte, FormulaError> bmap = new HashMap<Byte, FormulaError>();
+    private static final Map<Integer, FormulaError> imap = new HashMap<Integer, FormulaError>();
     static{
         for (FormulaError error : values()) {
             bmap.put(error.getCode(), error);
