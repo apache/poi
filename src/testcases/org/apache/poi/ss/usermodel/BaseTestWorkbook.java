d7ffe9305d234bc00f62a075bfd3e2ae9b2c0e36
Javen O'Neal <onealj@apache.org>
2016-10-09 10:26:49 +0000

    bug 55075: add missing import
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1763955 13f79535-47bb-0310-9956-ffa450edef68

--- src/testcases/org/apache/poi/ss/usermodel/BaseTestWorkbook.java
+++ src/testcases/org/apache/poi/ss/usermodel/BaseTestWorkbook.java
@@ -26,6 +26,7 @@ import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
