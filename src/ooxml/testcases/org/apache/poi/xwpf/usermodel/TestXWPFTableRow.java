5335dd775fcebfba6cee380f95bf5d1c6871e78d
Mark Murphy <jmarkmurphy@apache.org>
2016-11-05 06:12:24 +0000

    60337: XWPFTableRow.isRepeatHeader throws NullPointerException, setRepeatHeader does not overwrite old value
    Task-Url: https://bz.apache.org/bugzilla/show_bug.cgi?id=60337
    
    
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1768153 13f79535-47bb-0310-9956-ffa450edef68

--- src/ooxml/testcases/org/apache/poi/xwpf/usermodel/TestXWPFTableRow.java
+++ src/ooxml/testcases/org/apache/poi/xwpf/usermodel/TestXWPFTableRow.java
@@ -17,51 +17,124 @@
 
 package org.apache.poi.xwpf.usermodel;
 
-import junit.framework.TestCase;
-import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTRow;
-import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTTbl;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
-public class TestXWPFTableRow extends TestCase {
-    @Override
-    protected void setUp() throws Exception {
-        super.setUp();
+import java.io.IOException;
+
+import org.apache.poi.xwpf.XWPFTestDataSamples;
+import org.junit.Test;
+
+public class TestXWPFTableRow {
+
+    @Test
+    public void testCreateRow() throws IOException {
+        XWPFDocument doc = new XWPFDocument();
+        XWPFTable table = doc.createTable(1, 1);
+        XWPFTableRow tr = table.createRow();
+        assertNotNull(tr);
+        doc.close();
     }
 
-    public void testCreateRow() throws Exception {
-        CTRow ctRow = CTRow.Factory.newInstance();
-        assertNotNull(ctRow);
-    }
-
-    @Override
-    protected void tearDown() throws Exception {
-        super.tearDown();
-    }
-
-    public void testSetGetCantSplitRow() {
+    @Test
+    public void testSetGetCantSplitRow() throws IOException {
         // create a table
         XWPFDocument doc = new XWPFDocument();
-        CTTbl ctTable = CTTbl.Factory.newInstance();
-        XWPFTable table = new XWPFTable(ctTable, doc);
+        XWPFTable table = doc.createTable(1, 1);
         // table has a single row by default; grab it
         XWPFTableRow tr = table.getRow(0);
         assertNotNull(tr);
 
+        // Assert the repeat header is false by default
+        boolean isCantSplit = tr.isCantSplitRow();
+        assertFalse(isCantSplit);
+        
+        // Repeat the header
         tr.setCantSplitRow(true);
-        boolean isCant = tr.isCantSplitRow();
-        assert (isCant);
+        isCantSplit = tr.isCantSplitRow();
+        assertTrue(isCantSplit);
+
+        // Make the header no longer repeating
+        tr.setCantSplitRow(false);
+        isCantSplit = tr.isCantSplitRow();
+        assertFalse(isCantSplit);
+        
+        doc.close();
     }
 
-    public void testSetGetRepeatHeader() {
+    @Test
+    public void testSetGetRepeatHeader() throws IOException {
         // create a table
         XWPFDocument doc = new XWPFDocument();
-        CTTbl ctTable = CTTbl.Factory.newInstance();
-        XWPFTable table = new XWPFTable(ctTable, doc);
+        XWPFTable table = doc.createTable(3, 1);
         // table has a single row by default; grab it
         XWPFTableRow tr = table.getRow(0);
         assertNotNull(tr);
-
-        tr.setRepeatHeader(true);
+        
+        // Assert the repeat header is false by default
         boolean isRpt = tr.isRepeatHeader();
-        assert (isRpt);
+        assertFalse(isRpt);
+        
+        // Repeat the header
+        tr.setRepeatHeader(true);
+        isRpt = tr.isRepeatHeader();
+        assertTrue(isRpt);
+
+        // Make the header no longer repeating
+        tr.setRepeatHeader(false);
+        isRpt = tr.isRepeatHeader();
+        assertFalse(isRpt);
+        
+        // If the third row is set to repeat, but not the second,
+        // isRepeatHeader should report false because Word will
+        // ignore it.
+        tr = table.getRow(2);
+        tr.setRepeatHeader(true);
+        isRpt = tr.isRepeatHeader();
+        assertFalse(isRpt);
+        
+        doc.close();
+    }
+    
+    // Test that validates the table header value can be parsed from a document
+    // generated in Word
+    @Test
+    public void testIsRepeatHeader() throws Exception {
+        XWPFDocument doc = XWPFTestDataSamples
+                .openSampleDocument("Bug60337.docx");
+        XWPFTable table = doc.getTables().get(0);
+        XWPFTableRow tr = table.getRow(0);
+        boolean isRpt = tr.isRepeatHeader();
+        assertTrue(isRpt);
+        
+        tr = table.getRow(1);
+        isRpt = tr.isRepeatHeader();
+        assertFalse(isRpt);
+        
+        tr = table.getRow(2);
+        isRpt = tr.isRepeatHeader();
+        assertFalse(isRpt);
+    }
+    
+    
+    // Test that validates the table header value can be parsed from a document
+    // generated in Word
+    @Test
+    public void testIsCantSplit() throws Exception {
+        XWPFDocument doc = XWPFTestDataSamples
+                .openSampleDocument("Bug60337.docx");
+        XWPFTable table = doc.getTables().get(0);
+        XWPFTableRow tr = table.getRow(0);
+        boolean isCantSplit = tr.isCantSplitRow();
+        assertFalse(isCantSplit);
+        
+        tr = table.getRow(1);
+        isCantSplit = tr.isCantSplitRow();
+        assertFalse(isCantSplit);
+
+        tr = table.getRow(2);
+        isCantSplit = tr.isCantSplitRow();
+        assertTrue(isCantSplit);
     }
 }
