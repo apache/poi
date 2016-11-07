76a0c6378e14eca4cb8eb025ef6bb57849006283
Dominik Stadler <centic@apache.org>
2016-11-07 21:32:04 +0000

    Add setFormattingRanges() to interface ConditionalFormatting, closes #42
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1768588 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/poi/hssf/usermodel/HSSFConditionalFormatting.java
+++ src/java/org/apache/poi/hssf/usermodel/HSSFConditionalFormatting.java
@@ -94,8 +94,15 @@ public final class HSSFConditionalFormatting  implements ConditionalFormatting {
     /**
      * @return array of <tt>CellRangeAddress</tt>s. never <code>null</code> 
      */
+    @Override
     public CellRangeAddress[] getFormattingRanges() {
         return cfAggregate.getHeader().getCellRanges();
+    }
+
+    @Override
+    public void setFormattingRanges(
+            final CellRangeAddress[] ranges) {
+        cfAggregate.getHeader().setCellRanges(ranges);
     }
 
     /**
@@ -111,6 +118,7 @@ public final class HSSFConditionalFormatting  implements ConditionalFormatting {
         cfAggregate.setRule(idx, cfRule.getCfRuleRecord());
     }
 
+    @Override
     public void setRule(int idx, ConditionalFormattingRule cfRule){
         setRule(idx, (HSSFConditionalFormattingRule)cfRule);
     }
@@ -124,6 +132,7 @@ public final class HSSFConditionalFormatting  implements ConditionalFormatting {
         cfAggregate.addRule(cfRule.getCfRuleRecord());
     }
 
+    @Override
     public void addRule(ConditionalFormattingRule cfRule){
         addRule((HSSFConditionalFormattingRule)cfRule);
     }
@@ -131,6 +140,7 @@ public final class HSSFConditionalFormatting  implements ConditionalFormatting {
     /**
      * @return the Conditional Formatting rule at position idx.
      */
+    @Override
     public HSSFConditionalFormattingRule getRule(int idx) {
         CFRuleBase ruleRecord = cfAggregate.getRule(idx);
         return new HSSFConditionalFormattingRule(sheet, ruleRecord);
@@ -139,10 +149,12 @@ public final class HSSFConditionalFormatting  implements ConditionalFormatting {
     /**
      * @return number of Conditional Formatting rules.
      */
+    @Override
     public int getNumberOfRules() {
         return cfAggregate.getNumberOfRules();
     }
 
+    @Override
     public String toString() {
         return cfAggregate.toString();
     }
