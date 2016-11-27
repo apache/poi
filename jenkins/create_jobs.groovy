b7fbd0d5fed4ea6c31ffacf7e200cef76a6634f5
Dominik Stadler <centic@apache.org>
2016-11-27 20:28:21 +0000

    Add all items from the current job 'POI' to the new Jenkins DSL script.
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1771643 13f79535-47bb-0310-9956-ffa450edef68

--- jenkins/create_jobs.groovy
+++ jenkins/create_jobs.groovy
@@ -7,12 +7,28 @@ job('POI-DSL-Test') {
 'Apache POI - the Java API for Microsoft Documents\n' +
 '</p>\n' +
 '<p>\n' +
+'  <b>This is an automatically generated Job Config, do not edit it here!\n' +
+'    Instead change the Jenkins Job DSL at <a href="http://svn.apache.org/repos/asf/poi/trunk/jenkins">http://svn.apache.org/repos/asf/poi/trunk/jenkins</a>,\n' +
+'    see <a href="https://github.com/jenkinsci/job-dsl-plugin/wiki">https://github.com/jenkinsci/job-dsl-plugin/wiki</a>\n' +
+'    for more details about the DSL.</b>\n' +
+'</p>\n' +
+'<p>\n' +
 '    <b><a href="lastSuccessfulBuild/findbugsResult/" target="_blank">Findbugs report of latest build</a></b> -\n' +
 '    <b><a href="https://analysis.apache.org/dashboard/index/221489" target="_blank">Sonar reports</a></b> -\n' +
 '    <b><a href="lastSuccessfulBuild/artifact/build/coverage/index.html" target="_blank">Coverage of latest build</a></b>\n' +
 '</p>\n')
 	logRotator(numToKeep = 5, artifactNumToKeep = 1)
 	label('ubuntu&&!cloud-slave')
+    environmentVariables {
+        env('LANG', 'en_US.UTF-8')
+    }
+    wrappers {
+        timeout {
+            absolute(180)
+            abortBuild()
+            writeDescription('Build was aborted due to timeout')
+        }
+    }
 	jdk('JDK 1.6 (latest')
     scm {
         svn(svnBase) { svnNode ->
@@ -46,11 +62,23 @@ job('POI-DSL-Test') {
             thresholdLimit('low')
             defaultEncoding('UTF-8')
         }
-        archiveArtifacts('build/test-output/**/*.html')
+        archiveArtifacts('build/dist/*.tar.gz,build/findbugs.html,build/coverage/**,build/integration-test-results/**,ooxml-lib/**')
         archiveJunit('**/target/surefire-reports/*.xml')
-        warnings(['Java Compiler (javac)'], ['Java Compiler (javac)': '**/*.log']) {
-            excludePattern('**/test**')
+        warnings(['Java Compiler (javac)', 'JavaDoc Tool'], null) {
             resolveRelativePaths()
         }
+        archiveJunit('build/ooxml-test-results/*.xml,build/scratchpad-test-results/*.xml,build/test-results/*.xml,build/excelant-test-results/*.xml,build/integration-test-results/*.xml') {
+            allowEmptyResults()
+            testDataPublishers {
+                publishTestStabilityData()
+            }
+        }
+        jacocoCodeCoverage {
+            classPattern('build/classes,build/examples-classes,build/excelant-classes,build/ooxml-classes,build/scratchpad-classes')
+            execPattern('build/*.exec')
+            sourcePattern('src/java,src/excelant/java,src/ooxml/java,src/scratchpad/src')
+            exclusionPattern('com/microsoft/**,org/openxmlformats/**,org/etsi/**,org/w3/**,schemaorg*/**,schemasMicrosoft*/**,org/apache/poi/hdf/model/hdftypes/definitions/*.class,org/apache/poi/hwpf/model/types/*.class,org/apache/poi/hssf/usermodel/DummyGraphics2d.class,org/apache/poi/sl/draw/binding/*.class')
+        }
+        mailer('dev@poi.apache.org', false, false)
     }
 }
