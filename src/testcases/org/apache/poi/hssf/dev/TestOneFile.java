8003ad78f49247ba1f0eb5781a2094639f2855bb
Dominik Stadler <centic@apache.org>
2016-12-11 21:00:00 +0000

    Adjust local test class run with Gradle-Build as well
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1773661 13f79535-47bb-0310-9956-ffa450edef68

--- src/testcases/org/apache/poi/hssf/dev/TestOneFile.java
+++ src/testcases/org/apache/poi/hssf/dev/TestOneFile.java
@@ -16,8 +16,7 @@
 ==================================================================== */
 package org.apache.poi.hssf.dev;
 
-import java.io.File;
-
+import org.apache.poi.POIDataSamples;
 import org.junit.Test;
 
 public class TestOneFile {
@@ -25,7 +24,7 @@ public class TestOneFile {
     public void test() throws Exception {
         //Biff8EncryptionKey.setCurrentUserPassword("blabla");
         TestEFBiffViewer test = new TestEFBiffViewer();
-        test.file = new File("test-data/spreadsheet/50833.xls"); 
+        test.file = POIDataSamples.getSpreadSheetInstance().getFile("50833.xls");
         test.testMain();
     }
 }
