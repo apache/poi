2c0d4a3bb0f662b63f545b4c7540cdbe96e6fe62
Dominik Stadler <centic@apache.org>
2016-12-11 20:57:12 +0000

    Check if we did find jars in view.sh
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1773621 13f79535-47bb-0310-9956-ffa450edef68

--- view.sh
+++ view.sh
@@ -4,6 +4,13 @@
 for i in `ls build/dist/poi-*.jar | grep -v -- "-sources" | grep -v -- "-examples" | grep -v -- "-excelant" | grep -v -- "-ooxml" | grep -v -- "-scratchpad"`;do
 	CP=$i
 done
+
+if [ "$CP" = "" ]
+then
+	echo Did not find jar files in directory build/dist, please run target "jar"
+	exit 1
+fi
+
 echo Using jar $CP
 
 java -cp $CP org.apache.poi.hssf.dev.BiffViewer --escher "$@"
