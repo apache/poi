166f2d30009a871a5a0cd02b6cf7b7e04d4608bb
Dominik Stadler <centic@apache.org>
2016-12-11 20:59:38 +0000

    Use Java 6 from Oracle in run.sh
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1773656 13f79535-47bb-0310-9956-ffa450edef68

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export JAVA_HOME=/usr/lib/jvm/java-1.6.0-openjdk-amd64
+export JAVA_HOME=/usr/lib/jvm/java-6-oracle
 
 # use this JVM for creating javadocs:
 #export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
