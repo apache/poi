5e476cd95eb10c488ba7ef8746f14e3f02242e26
Dominik Stadler <centic@apache.org>
2016-12-11 20:58:42 +0000

    Enhance view.sh for HWPF/doc files
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1773642 13f79535-47bb-0310-9956-ffa450edef68

--- view.sh
+++ view.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 # find jar-file with highest version number
-for i in `ls build/dist/poi-*.jar | grep -v -- "-sources" | grep -v -- "-examples" | grep -v -- "-excelant" | grep -v -- "-ooxml" | grep -v -- "-scratchpad" | grep -v -- "-javadoc"`;do
+for i in `ls build/dist/poi-*.jar | grep -v -- "-sources" | grep -v -- "-examples" | grep -v -- "-excelant" | grep -v -- "-ooxml" | grep -v -- "-javadoc"`;do
 	CP=$i:$CP
 done
 
@@ -13,4 +13,11 @@ fi
 
 echo Using jar $CP
 
-java -cp $CP org.apache.poi.hssf.dev.BiffViewer --escher "$@"
+file=$1
+echo Found extension ${file: -4}
+if [ ${file: -4} == ".doc" ]
+then
+	java -cp $CP org.apache.poi.hwpf.dev.HWPFLister "$@" --pictures --escher
+else
+	java -cp $CP org.apache.poi.hssf.dev.BiffViewer --escher "$@"
+fi
