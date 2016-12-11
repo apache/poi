f3fc14ebdae3f0e6f6fbe8874282a6ec9134d492
Dominik Stadler <centic@apache.org>
2016-12-11 21:17:22 +0000

    Revert "Add script to run BiffViewer"
    This reverts commit a7f97798f1ffa2e90daa570f244065e01ca931fc.
    
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1773715 13f79535-47bb-0310-9956-ffa450edef68

--- view.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# find jar-file with highest version number
-for i in `ls build/dist/poi-*.jar | grep -v -- "-sources" | grep -v -- "-examples" | grep -v -- "-excelant" | grep -v -- "-ooxml" | grep -v -- "-scratchpad"`;do
-	CP=$i
-done
-echo Using jar $CP
-
-java -cp $CP org.apache.poi.hssf.dev.BiffViewer --escher "$@"
