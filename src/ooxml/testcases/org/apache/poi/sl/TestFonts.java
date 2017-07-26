515cd684e5022229d537b9d681e20e332f3f1a20
Greg Woolsey <gwoolsey@apache.org>
2017-07-26 22:00:23 +0000

    Another font size variation.  I suspect it is my Windows 10 scaling factor, set to 100% instead of the Windows 10 default 125%.
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1803117 13f79535-47bb-0310-9956-ffa450edef68

--- src/ooxml/testcases/org/apache/poi/sl/TestFonts.java
+++ src/ooxml/testcases/org/apache/poi/sl/TestFonts.java
@@ -73,6 +73,7 @@ public class TestFonts {
 
     // currently linux and mac return quite different values
     private static final int[] expected_sizes = {
+            304, // windows 10, 1080p, MS Office 2016, system text scaling 100% instead of default 125%
             306, //Windows 10, 15.6" 3840x2160
             311, 312, 313, 318,
             348, //Windows 10, 15.6" 3840x2160
