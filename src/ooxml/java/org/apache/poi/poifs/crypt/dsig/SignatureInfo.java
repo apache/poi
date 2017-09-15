f097cb22562616662a98ae0897b97ba1997f6c64
Dominik Stadler <centic@apache.org>
2017-09-15 18:39:29 +0000

    Adjust some more places for the new versions of some third party libraries
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1808473 13f79535-47bb-0310-9956-ffa450edef68

--- src/ooxml/java/org/apache/poi/poifs/crypt/dsig/SignatureInfo.java
+++ src/ooxml/java/org/apache/poi/poifs/crypt/dsig/SignatureInfo.java
@@ -163,9 +163,9 @@ import org.xml.sax.SAXException;
  * <p>To use SignatureInfo and its sibling classes, you'll need to have the following libs
  * in the classpath:</p>
  * <ul>
- * <li>BouncyCastle bcpkix and bcprov (tested against 1.54)</li>
- * <li>Apache Santuario "xmlsec" (tested against 2.0.5)</li>
- * <li>and slf4j-api (tested against 1.7.12)</li>
+ * <li>BouncyCastle bcpkix and bcprov (tested against 1.58)</li>
+ * <li>Apache Santuario "xmlsec" (tested against 2.1.0)</li>
+ * <li>and slf4j-api (tested against 1.7.25)</li>
  * </ul>
  */
 public class SignatureInfo implements SignatureConfigurable {
