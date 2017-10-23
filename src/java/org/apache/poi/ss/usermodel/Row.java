7dce661861340d31d50b4f18dd7fe85cc63d41e5
PJ Fanning <fanningpj@apache.org>
2017-10-23 21:17:49 +0000

    Remove more deprecated code
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1813102 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/poi/ss/usermodel/Row.java
+++ src/java/org/apache/poi/ss/usermodel/Row.java
@@ -50,28 +50,6 @@ public interface Row extends Iterable<Cell> {
      * @return Cell a high level representation of the created cell.
      * @throws IllegalArgumentException if columnIndex &lt; 0 or greater than a maximum number of supported columns
      * (255 for *.xls, 1048576 for *.xlsx)
-     * @see CellType#BLANK
-     * @see CellType#BOOLEAN
-     * @see CellType#ERROR
-     * @see CellType#FORMULA
-     * @see CellType#NUMERIC
-     * @see CellType#STRING
-     * @deprecated POI 3.15 beta 3. Use {@link #createCell(int, CellType)} instead.
-     */
-    Cell createCell(int column, int type);
-    /**
-     * Use this to create new cells within the row and return it.
-     * <p>
-     * The cell that is returned will be of the requested type.
-     * The type can be changed either through calling setCellValue 
-     *  or setCellType, but there is a small overhead to doing this,
-     *  so it is best to create of the required type up front.
-     *
-     * @param column - the column number this cell represents
-     * @param type - the cell's data type
-     * @return Cell a high level representation of the created cell.
-     * @throws IllegalArgumentException if columnIndex &lt; 0 or greater than a maximum number of supported columns
-     * (255 for *.xls, 1048576 for *.xlsx)
      */
     Cell createCell(int column, CellType type);
 
