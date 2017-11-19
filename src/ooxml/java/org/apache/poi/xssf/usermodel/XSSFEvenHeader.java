c37a85d97479e4fed8bc0de06668fdae6296b44a
Mark Murphy <jmarkmurphy@apache.org>
2017-11-19 01:35:07 +0000

    javadocs and Quick page improvements for XSSF Header/Footer
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1815706 13f79535-47bb-0310-9956-ffa450edef68

--- src/ooxml/java/org/apache/poi/xssf/usermodel/XSSFEvenHeader.java
+++ src/ooxml/java/org/apache/poi/xssf/usermodel/XSSFEvenHeader.java
@@ -27,13 +27,21 @@ import org.openxmlformats.schemas.spreadsheetml.x2006.main.CTHeaderFooter;
  * the sheet may not be printed, for example, if the print area is specified to
  * be a range such that it falls outside an even page's scope. If no even header
  * is specified, then odd header value is assumed for even page headers.
+ * </p><p>
+ * The even header is activated by the "Different Even/Odd" Header/Footer property for the sheet.
+ * If this property is not set, the even header is ignored, and the odd footer is used instead.
+ * </p><p>
+ * Creating an even header or footer sets this property by default, so all you need to do to
+ * get an even header or footer to display is to create it. Likewise, if both the even header
+ * and footer are usnset, then this property is unset, and the odd header and footer are used
+ * for even pages.
  * </p>
- *
  */
 public class XSSFEvenHeader extends XSSFHeaderFooter implements Header {
 
     /**
-     * Create an instance of XSSFEvenHeader from the supplied XML bean
+     * Create an instance of XSSFEvenHeader from the supplied XML bean. If an even
+     * header is created, The property "DifferentOddEven" is set for this sheet as well.
      * 
      * @see XSSFSheet#getEvenHeader()
      * @param headerFooter
@@ -48,18 +56,22 @@ public class XSSFEvenHeader extends XSSFHeaderFooter implements Header {
      * 
      * @return text
      */
+    @Override
     public String getText() {
         return getHeaderFooter().getEvenHeader();
     }
 
     /**
-     * Set a text for the header. If null unset the value
+     * Set a text for the header. If null, unset the value. If unsetting and there is no
+     * Even Footer for this sheet, the "DifferentEvenOdd" property for this sheet is
+     * unset.
      * 
      * @see XSSFHeaderFooter to see how to create a string with Header/Footer
      *      Formatting Syntax
      * @param text
      *            - a string representing the header.
      */
+    @Override
     public void setText(String text) {
         if (text == null) {
             getHeaderFooter().unsetEvenHeader();
