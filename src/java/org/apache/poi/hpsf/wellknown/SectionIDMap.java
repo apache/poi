2cda98a4aac6e78546c3e7a360d7e336f2c86575
Andreas Beeker <kiwiwings@apache.org>
2017-11-24 01:02:20 +0000

    #61809 - Infinite loop in SectionIDMap.get() and .put()
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1816205 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/poi/hpsf/wellknown/SectionIDMap.java
+++ src/java/org/apache/poi/hpsf/wellknown/SectionIDMap.java
@@ -17,136 +17,44 @@
 
 package org.apache.poi.hpsf.wellknown;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.apache.poi.hpsf.ClassID;
+import org.apache.poi.hpsf.DocumentSummaryInformation;
+import org.apache.poi.hpsf.SummaryInformation;
 import org.apache.poi.util.Internal;
+import org.apache.poi.util.Removal;
 
 /**
- * <p>Maps section format IDs to {@link PropertyIDMap}s. It is
- * initialized with two well-known section format IDs: those of the
- * <tt>\005SummaryInformation</tt> stream and the
- * <tt>\005DocumentSummaryInformation</tt> stream.</p>
- *
- * <p>If you have a section format ID you can use it as a key to query
- * this map.  If you get a {@link PropertyIDMap} returned your section
- * is well-known and you can query the {@link PropertyIDMap} for PID
- * strings. If you get back <code>null</code> you are on your own.</p>
- *
- * <p>This {@link java.util.Map} expects the byte arrays of section format IDs
- * as keys. A key maps to a {@link PropertyIDMap} describing the
- * property IDs in sections with the specified section format ID.</p>
+ * This classed used to map section format IDs to {@link PropertyIDMap PropertyIDMaps},
+ * but there's no way to use custom PropertyIDMaps.<p>
+ * 
+ * It is only kept for its ClassIDs until removal.
+ * 
+ * @deprecated in 4.0.0, there's no way to create custom PropertyIDMaps, therefore
+ *   this class is obsolete
  */
 @Internal
+@Deprecated
+@Removal(version="4.2.0")
 public class SectionIDMap {
 
     /**
-     * The default section ID map. It maps section format IDs to {@link PropertyIDMap PropertyIDMaps}
+     * The SummaryInformation's section's format ID.
+     * @deprecated use {@link SummaryInformation#FORMAT_ID}
      */
-    private static ThreadLocal<Map<ClassID,PropertyIDMap>> defaultMap =
-            new ThreadLocal<>();
-    
-    /**
-     * <p>The SummaryInformation's section's format ID.</p>
-     */
-    public static final ClassID SUMMARY_INFORMATION_ID =
-        new ClassID("{F29F85E0-4FF9-1068-AB91-08002B27B3D9}");
+    @Deprecated
+    public static final ClassID SUMMARY_INFORMATION_ID = SummaryInformation.FORMAT_ID;
 
     /**
      * The DocumentSummaryInformation's first and second sections' format ID.
+     * @deprecated use {@link DocumentSummaryInformation#FORMAT_ID}
      */
-    private static final ClassID DOC_SUMMARY_INFORMATION =
-        new ClassID("{D5CDD502-2E9C-101B-9397-08002B2CF9AE}");    
-    private static final ClassID USER_DEFINED_PROPERTIES =
-        new ClassID("{D5CDD505-2E9C-101B-9397-08002B2CF9AE}");
-    
-    public static final ClassID[] DOCUMENT_SUMMARY_INFORMATION_ID = {
-        DOC_SUMMARY_INFORMATION, USER_DEFINED_PROPERTIES
-    };
+    @Deprecated
+    public static final ClassID[] DOCUMENT_SUMMARY_INFORMATION_ID = DocumentSummaryInformation.FORMAT_ID;
 
     /**
      * A property without a known name is described by this string.
+     * @deprecated use {@link PropertyIDMap#UNDEFINED}
      */
-    public static final String UNDEFINED = "[undefined]";
-
-    /**
-     * <p>Returns the singleton instance of the default {@link
-     * SectionIDMap}.</p>
-     *
-     * @return The instance value
-     */
-    public static SectionIDMap getInstance() {
-        Map<ClassID,PropertyIDMap> m = defaultMap.get();
-        if (m == null) {
-            m = new HashMap<>();
-            m.put(SUMMARY_INFORMATION_ID, PropertyIDMap.getSummaryInformationProperties());
-            m.put(DOCUMENT_SUMMARY_INFORMATION_ID[0], PropertyIDMap.getDocumentSummaryInformationProperties());
-            defaultMap.set(m);
-        }
-        return new SectionIDMap();
-    }
-
-
-
-    /**
-     * <p>Returns the property ID string that is associated with a
-     * given property ID in a section format ID's namespace.</p>
-     *
-     * @param sectionFormatID Each section format ID has its own name
-     * space of property ID strings and thus must be specified.
-     * @param  pid The property ID
-     * @return The well-known property ID string associated with the
-     * property ID <var>pid</var> in the name space spanned by <var>
-     * sectionFormatID</var> . If the <var>pid</var>
-     * /<var>sectionFormatID </var> combination is not well-known, the
-     * string "[undefined]" is returned.
-     */
-    public static String getPIDString(ClassID sectionFormatID, long pid) {
-        final PropertyIDMap m = getInstance().get(sectionFormatID);
-        if (m == null) {
-            return UNDEFINED;
-        }
-        final String s = m.get(pid);
-        if (s == null) {
-            return UNDEFINED;
-        }
-        return s;
-    }
-
-
-
-    /**
-     * <p>Returns the {@link PropertyIDMap} for a given section format
-     * ID.</p>
-     *
-     * @param sectionFormatID the section format ID
-     * @return the property ID map
-     */
-    public PropertyIDMap get(final ClassID sectionFormatID) {
-        return getInstance().get(sectionFormatID);
-    }
-
-    /**
-     * Associates a section format ID with a {@link PropertyIDMap}.
-     *
-     * @param sectionFormatID the section format ID
-     * @param propertyIDMap the property ID map
-     * @return as defined by {@link java.util.Map#put}
-     */
-    public PropertyIDMap put(ClassID sectionFormatID, PropertyIDMap propertyIDMap) {
-        return getInstance().put(sectionFormatID, propertyIDMap);
-    }
-
-    /**
-     * Associates the string representation of a section format ID with a {@link PropertyIDMap}
-     * 
-     * @param key the key of the PropertyIDMap
-     * @param value the PropertyIDMap itself
-     * 
-     * @return the previous PropertyIDMap stored under this key, or {@code null} if there wasn't one
-     */
-    protected PropertyIDMap put(String key, PropertyIDMap value) {
-        return put(new ClassID(key), value);
-    }
+    @Deprecated
+    public static final String UNDEFINED = PropertyIDMap.UNDEFINED;
 }
