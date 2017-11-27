e8234fd03282217716e1b20d368bbc2ad003480f
Nick Burch <nick@apache.org>
2017-11-27 14:05:17 +0000

    Javadoc fixes
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1816453 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/poi/ss/usermodel/charts/LayoutMode.java
+++ src/java/org/apache/poi/ss/usermodel/charts/LayoutMode.java
@@ -20,8 +20,8 @@ package org.apache.poi.ss.usermodel.charts;
 import org.apache.poi.util.Removal;
 
 /**
- *  Specifies the possible ways to store a chart element's position.
- * @use XDDF LayoutMode instead
+ * Specifies the possible ways to store a chart element's position.
+ * @see org.apache.poi.xddf.usermodel.chart.LayoutMode XDDF LayoutMode instead
  */
 @Deprecated
 @Removal(version="4.2")
