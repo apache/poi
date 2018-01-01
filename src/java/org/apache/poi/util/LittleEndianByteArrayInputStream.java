7e04bd33e5eda1c7fb2bd4478a4a1fd745351480
Dominik Stadler <centic@apache.org>
2018-01-01 14:39:26 +0000

    Bug 61550: Add more information to exception text and verify that it is thrown
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1819772 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/poi/util/LittleEndianByteArrayInputStream.java
+++ src/java/org/apache/poi/util/LittleEndianByteArrayInputStream.java
@@ -37,7 +37,8 @@ public class LittleEndianByteArrayInputStream extends ByteArrayInputStream imple
 
 	protected void checkPosition(int i) {
 		if (i > count - pos) {
-			throw new RuntimeException("Buffer overrun");
+			throw new RuntimeException("Buffer overrun, having " + count + " bytes in the stream and position is at " + pos +
+					", but trying to increment position by " + i);
 		}
 	}
 
