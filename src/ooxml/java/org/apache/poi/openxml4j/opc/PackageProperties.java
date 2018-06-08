9bd179c39aa492acb9806c47e76f9ca76b08a010
PJ Fanning <fanningpj@yahoo.com>
2018-06-09 00:26:41 +0100

    fix indents

--- src/ooxml/java/org/apache/poi/openxml4j/opc/PackageProperties.java
+++ src/ooxml/java/org/apache/poi/openxml4j/opc/PackageProperties.java
@@ -28,46 +28,46 @@ import java.util.Optional;
  * @see org.apache.poi.openxml4j.opc.OPCPackage
  */
 public interface PackageProperties {
-	
-	/**
-	 * Dublin Core Terms URI.
-	 */
-	String NAMESPACE_DCTERMS = "http://purl.org/dc/terms/";
-	
-	/**
-	 * Dublin Core namespace URI.
-	 */
-	String NAMESPACE_DC = "http://purl.org/dc/elements/1.1/";
 
-	/* Getters and setters */
+    /**
+     * Dublin Core Terms URI.
+     */
+    String NAMESPACE_DCTERMS = "http://purl.org/dc/terms/";
 
-	/**
-	 * Set the category of the content of this package.
-	 * @return property value
-	 */
-	Optional<String> getCategoryProperty();
+    /**
+     * Dublin Core namespace URI.
+     */
+    String NAMESPACE_DC = "http://purl.org/dc/elements/1.1/";
 
-	/**
-	 * Set the category of the content of this package.
-	 */
-	void setCategoryProperty(String category);
+    /* Getters and setters */
 
     /**
      * Set the category of the content of this package.
-	 * @since 4.0.0
+     * @return property value
+     */
+    Optional<String> getCategoryProperty();
+
+    /**
+     * Set the category of the content of this package.
+     */
+    void setCategoryProperty(String category);
+
+    /**
+     * Set the category of the content of this package.
+     * @since 4.0.0
      */
     void setCategoryProperty(Optional<String> category);
 
     /**
-	 * Set the status of the content.
+     * Set the status of the content.
      * @return property value
-	 */
-	Optional<String> getContentStatusProperty();
+     */
+    Optional<String> getContentStatusProperty();
 
-	/**
-	 * Get the status of the content.
-	 */
-	void setContentStatusProperty(String contentStatus);
+    /**
+     * Get the status of the content.
+     */
+    void setContentStatusProperty(String contentStatus);
 
     /**
      * Get the status of the content.
@@ -76,17 +76,17 @@ public interface PackageProperties {
     void setContentStatusProperty(Optional<String> contentStatus);
 
     /**
-	 * Get the type of content represented, generally defined by a specific use
-	 * and intended audience.
+     * Get the type of content represented, generally defined by a specific use
+     * and intended audience.
      * @return property value
-	 */
-	Optional<String> getContentTypeProperty();
+     */
+    Optional<String> getContentTypeProperty();
 
-	/**
-	 * Set the type of content represented, generally defined by a specific use
-	 * and intended audience.
-	 */
-	void setContentTypeProperty(String contentType);
+    /**
+     * Set the type of content represented, generally defined by a specific use
+     * and intended audience.
+     */
+    void setContentTypeProperty(String contentType);
 
     /**
      * Set the type of content represented, generally defined by a specific use
@@ -95,34 +95,34 @@ public interface PackageProperties {
      */
     void setContentTypeProperty(Optional<String> contentType);
 
-	/**
-	 * Get the date of creation of the resource.
+    /**
+     * Get the date of creation of the resource.
      * @return property value
-	 */
-	Optional<Date> getCreatedProperty();
+     */
+    Optional<Date> getCreatedProperty();
 
-	/**
-	 * Set the date of creation of the resource.
-	 */
-	void setCreatedProperty(String created);
-	
-	/**
-	 * Set the date of creation of the resource.
-	 */
-	void setCreatedProperty(Optional<Date> created);
+    /**
+     * Set the date of creation of the resource.
+     */
+    void setCreatedProperty(String created);
 
-	/**
-	 * Get the entity primarily responsible for making the content of the
-	 * resource.
+    /**
+     * Set the date of creation of the resource.
+     */
+    void setCreatedProperty(Optional<Date> created);
+
+    /**
+     * Get the entity primarily responsible for making the content of the
+     * resource.
      * @return property value
-	 */
-	Optional<String> getCreatorProperty();
+     */
+    Optional<String> getCreatorProperty();
 
-	/**
-	 * Set the entity primarily responsible for making the content of the
-	 * resource.
-	 */
-	void setCreatorProperty(String creator);
+    /**
+     * Set the entity primarily responsible for making the content of the
+     * resource.
+     */
+    void setCreatorProperty(String creator);
 
     /**
      * Set the entity primarily responsible for making the content of the
@@ -132,14 +132,14 @@ public interface PackageProperties {
     void setCreatorProperty(Optional<String> creator);
 
     /**
-	 * Get the explanation of the content of the resource.
-	 */
-	Optional<String> getDescriptionProperty();
+     * Get the explanation of the content of the resource.
+     */
+    Optional<String> getDescriptionProperty();
 
-	/**
-	 * Set the explanation of the content of the resource.
-	 */
-	void setDescriptionProperty(String description);
+    /**
+     * Set the explanation of the content of the resource.
+     */
+    void setDescriptionProperty(String description);
 
     /**
      * Set the explanation of the content of the resource.
@@ -147,16 +147,16 @@ public interface PackageProperties {
      */
     void setDescriptionProperty(Optional<String> description);
 
-	/**
-	 * Get an unambiguous reference to the resource within a given context.
+    /**
+     * Get an unambiguous reference to the resource within a given context.
      * @return property value
-	 */
-	Optional<String> getIdentifierProperty();
+     */
+    Optional<String> getIdentifierProperty();
 
-	/**
-	 * Set an unambiguous reference to the resource within a given context.
-	 */
-	void setIdentifierProperty(String identifier);
+    /**
+     * Set an unambiguous reference to the resource within a given context.
+     */
+    void setIdentifierProperty(String identifier);
 
     /**
      * Set an unambiguous reference to the resource within a given context.
@@ -165,19 +165,19 @@ public interface PackageProperties {
     void setIdentifierProperty(Optional<String> identifier);
 
     /**
-	 * Get a delimited set of keywords to support searching and indexing. This
-	 * is typically a list of terms that are not available elsewhere in the
-	 * properties
+     * Get a delimited set of keywords to support searching and indexing. This
+     * is typically a list of terms that are not available elsewhere in the
+     * properties
      * @return property value
-	 */
-	Optional<String> getKeywordsProperty();
+     */
+    Optional<String> getKeywordsProperty();
 
-	/**
-	 * Set a delimited set of keywords to support searching and indexing. This
-	 * is typically a list of terms that are not available elsewhere in the
-	 * properties
-	 */
-	void setKeywordsProperty(String keywords);
+    /**
+     * Set a delimited set of keywords to support searching and indexing. This
+     * is typically a list of terms that are not available elsewhere in the
+     * properties
+     */
+    void setKeywordsProperty(String keywords);
 
     /**
      * Set a delimited set of keywords to support searching and indexing. This
@@ -187,16 +187,16 @@ public interface PackageProperties {
      */
     void setKeywordsProperty(Optional<String> keywords);
 
-	/**
-	 * Get the language of the intellectual content of the resource.
+    /**
+     * Get the language of the intellectual content of the resource.
      * @return property value
-	 */
-	Optional<String> getLanguageProperty();
+     */
+    Optional<String> getLanguageProperty();
 
-	/**
-	 * Set the language of the intellectual content of the resource.
-	 */
-	void setLanguageProperty(String language);
+    /**
+     * Set the language of the intellectual content of the resource.
+     */
+    void setLanguageProperty(String language);
 
     /**
      * Set the language of the intellectual content of the resource.
@@ -205,14 +205,14 @@ public interface PackageProperties {
     void setLanguageProperty(Optional<String> language);
 
     /**
-	 * Get the user who performed the last modification.
-	 */
-	Optional<String> getLastModifiedByProperty();
+     * Get the user who performed the last modification.
+     */
+    Optional<String> getLastModifiedByProperty();
 
-	/**
-	 * Set the user who performed the last modification.
-	 */
-	void setLastModifiedByProperty(String lastModifiedBy);
+    /**
+     * Set the user who performed the last modification.
+     */
+    void setLastModifiedByProperty(String lastModifiedBy);
 
     /**
      * Set the user who performed the last modification.
@@ -221,47 +221,47 @@ public interface PackageProperties {
     void setLastModifiedByProperty(Optional<String> lastModifiedBy);
 
     /**
-	 * Get the date and time of the last printing.
+     * Get the date and time of the last printing.
      * @return property value
      */
-	Optional<Date> getLastPrintedProperty();
-
-	/**
-	 * Set the date and time of the last printing.
-	 */
-	void setLastPrintedProperty(String lastPrinted);
-	
-	/**
-	 * Set the date and time of the last printing.
-	 */
-	void setLastPrintedProperty(Optional<Date> lastPrinted);
-
-	/**
-	 * Get the date on which the resource was changed.
-     * @return property value
-	 */
-	Optional<Date> getModifiedProperty();
-
-	/**
-	 * Set the date on which the resource was changed.
-	 */
-	void setModifiedProperty(String modified);
-	
-	/**
-	 * Set the date on which the resource was changed.
-	 */
-	void setModifiedProperty(Optional<Date> modified);
+    Optional<Date> getLastPrintedProperty();
 
     /**
-	 * Get the revision number.
-     * @return property value
-	 */
-	Optional<String> getRevisionProperty();
+     * Set the date and time of the last printing.
+     */
+    void setLastPrintedProperty(String lastPrinted);
 
-	/**
-	 * Set the revision number.
-	 */
-	void setRevisionProperty(String revision);
+    /**
+     * Set the date and time of the last printing.
+     */
+    void setLastPrintedProperty(Optional<Date> lastPrinted);
+
+    /**
+     * Get the date on which the resource was changed.
+     * @return property value
+     */
+    Optional<Date> getModifiedProperty();
+
+    /**
+     * Set the date on which the resource was changed.
+     */
+    void setModifiedProperty(String modified);
+
+    /**
+     * Set the date on which the resource was changed.
+     */
+    void setModifiedProperty(Optional<Date> modified);
+
+    /**
+     * Get the revision number.
+     * @return property value
+     */
+    Optional<String> getRevisionProperty();
+
+    /**
+     * Set the revision number.
+     */
+    void setRevisionProperty(String revision);
 
     /**
      * Set the revision number.
@@ -270,15 +270,15 @@ public interface PackageProperties {
     void setRevisionProperty(Optional<String> revision);
 
     /**
-	 * Get the topic of the content of the resource.
+     * Get the topic of the content of the resource.
      * @return property value
-	 */
-	Optional<String> getSubjectProperty();
+     */
+    Optional<String> getSubjectProperty();
 
-	/**
-	 * Set the topic of the content of the resource.
-	 */
-	void setSubjectProperty(String subject);
+    /**
+     * Set the topic of the content of the resource.
+     */
+    void setSubjectProperty(String subject);
 
     /**
      * Set the topic of the content of the resource.
@@ -287,10 +287,10 @@ public interface PackageProperties {
     void setSubjectProperty(Optional<String> subject);
 
     /**
-	 * Get the name given to the resource.
+     * Get the name given to the resource.
      * @return property value
-	 */
-	Optional<String> getTitleProperty();
+     */
+    Optional<String> getTitleProperty();
 
     /**
      * Set the name given to the resource.
@@ -298,16 +298,16 @@ public interface PackageProperties {
     void setTitleProperty(String title);
 
     /**
-	 * Set the name given to the resource.
+     * Set the name given to the resource.
      * @since 4.0.0
-	 */
-	void setTitleProperty(Optional<String> title);
+     */
+    void setTitleProperty(Optional<String> title);
 
     /**
-	 * Get the version number.
+     * Get the version number.
      * @return property value
-	 */
-	Optional<String> getVersionProperty();
+     */
+    Optional<String> getVersionProperty();
 
     /**
      * Set the version number.
