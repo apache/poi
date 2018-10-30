80030d529fc0cb2512b71996d5be0e1d91880ffb
Tim Allison <tallison@apache.org>
2018-10-30 15:45:00 +0000

    bug 62625 -- clean up imports
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1845246 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/poi/poifs/macros/VBAMacroReader.java
+++ src/java/org/apache/poi/poifs/macros/VBAMacroReader.java
@@ -29,7 +29,6 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.PushbackInputStream;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
