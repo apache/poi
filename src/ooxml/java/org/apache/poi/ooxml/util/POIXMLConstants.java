740a756a3f28280e450f97525fd44d79eb9103e5
PJ Fanning <fanningpj@apache.org>
2018-12-08 08:13:36 +0000

    lgtm issues
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1848438 13f79535-47bb-0310-9956-ffa450edef68

--- src/ooxml/java/org/apache/poi/ooxml/util/POIXMLConstants.java
+++ src/ooxml/java/org/apache/poi/ooxml/util/POIXMLConstants.java
@@ -20,6 +20,7 @@ package org.apache.poi.ooxml.util;
 public class POIXMLConstants {
     public static final String FEATURE_LOAD_DTD_GRAMMAR = "http://apache.org/xml/features/nonvalidating/load-dtd-grammar";
     public static final String FEATURE_LOAD_EXTERNAL_DTD = "http://apache.org/xml/features/nonvalidating/load-external-dtd";
+    public static final String FEATURE_DISALLOW_DOCTYPE_DECL = "http://apache.org/xml/features/disallow-doctype-decl";
     public static final String PROPERTY_ENTITY_EXPANSION_LIMIT = "http://www.oracle.com/xml/jaxp/properties/entityExpansionLimit";
     public static final String PROPERTY_SECURITY_MANAGER = "http://apache.org/xml/properties/security-manager";
 }
