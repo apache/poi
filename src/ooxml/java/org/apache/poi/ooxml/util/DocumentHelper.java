854820c42095042cf501f83ff66d8e7c24dbfda1
PJ Fanning <fanningpj@apache.org>
2019-03-20 23:41:29 +0000

    remove unncessary synchronization
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1855956 13f79535-47bb-0310-9956-ffa450edef68

--- src/ooxml/java/org/apache/poi/ooxml/util/DocumentHelper.java
+++ src/ooxml/java/org/apache/poi/ooxml/util/DocumentHelper.java
@@ -182,7 +182,7 @@ public final class DocumentHelper {
     /**
      * Creates a new DOM Document
      */
-    public static synchronized Document createDocument() {
+    public static Document createDocument() {
         return documentBuilderSingleton.newDocument();
     }
 
