a17cdc8458ea8ef2c8ef03ce811d0d7664ea5d38
Andreas Beeker <kiwiwings@apache.org>
2019-10-12 18:08:29 +0000

    add another text height to TestFonts
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1868364 13f79535-47bb-0310-9956-ffa450edef68

--- src/ooxml/testcases/org/apache/poi/sl/TestFonts.java
+++ src/ooxml/testcases/org/apache/poi/sl/TestFonts.java
@@ -38,7 +38,6 @@ import java.util.Map;
 
 import org.apache.poi.POIDataSamples;
 import org.apache.poi.common.usermodel.fonts.FontGroup;
-import org.apache.poi.sl.draw.DrawFactory;
 import org.apache.poi.sl.draw.Drawable;
 import org.apache.poi.sl.usermodel.Slide;
 import org.apache.poi.sl.usermodel.SlideShow;
@@ -75,6 +74,7 @@ public class TestFonts {
             304, // windows 10, 1080p, MS Office 2016, system text scaling 100% instead of default 125%
             306, 308,// Windows 10, 15.6" 3840x2160
             311, 312, 313, 318,
+            338, // Manjaro Linux, 24", 1920x1080(519x292 mm), 94x94 dpi
             348, // Windows 10, 15.6" 3840x2160
             362, // Windows 10, 13.3" 1080p high-dpi
             372, // Ubuntu Xenial, 15", 1680x1050
