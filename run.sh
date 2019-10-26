e2aec1f0c0218aafd153c8723c451f04c89d1463
Dominik Stadler <centic@apache.org>
2019-10-26 06:49:44 +0000

    Add script to run ant locally with Java 1.6/Ant 1.8.x
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1868988 13f79535-47bb-0310-9956-ffa450edef68

---
+++ run.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+export JAVA_HOME=/usr/lib/jvm/java-6-sun
+
+# use this JVM for creating javadocs:
+#export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
+
+export PATH=$JAVA_HOME/bin:$PATH
+
+export FORREST_HOME=/opt/apache/forrest/apache-forrest-0.5.1-bin
+
+export ANT_OPTS="-Xmx1024m -XX:MaxPermSize=256m"
+
+#ls $JAVA_HOME
+java -version
+
+# compile-ooxml-lite test-all testcoveragereport
+#/opt/apache/apache-ant/apache-ant-1.7.1/bin/
+ant \
+	-Dcoverage.enabled=true \
+	-Dorg.apache.poi.util.POILogger=org.apache.poi.util.NullLogger \
+	-Dhalt.on.test.failure=false \
+	$*
+/opt/apache/apache-ant/apache-ant-1.7.1/bin/ant -Dcoverage.enabled=true $*
