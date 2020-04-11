50d52bf8285925ceaf6f93d0204333e4ee7a6747
Andreas Beeker <kiwiwings@apache.org>
2020-04-11 23:16:18 +0000

    Add Java 15 job
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1876407 13f79535-47bb-0310-9956-ffa450edef68

--- jenkins/create_jobs.groovy
+++ jenkins/create_jobs.groovy
@@ -35,6 +35,8 @@ def poijobs = [
         ],
         [ name: 'POI-DSL-1.14', jdk: '1.14', trigger: triggerSundays, skipcigame: true
         ],
+        [ name: 'POI-DSL-1.15', jdk: '1.15', trigger: triggerSundays, skipcigame: true
+        ],
         [ name: 'POI-DSL-IBM-JDK', jdk: 'IBMJDK', trigger: triggerSundays, skipcigame: true
         ],
         [ name: 'POI-DSL-old-Xerces', trigger: triggerSundays,
@@ -55,9 +57,7 @@ def poijobs = [
         [ name: 'POI-DSL-no-scratchpad', trigger: triggerSundays, noScratchpad: true
         ],
         [ name: 'POI-DSL-SonarQube', trigger: 'H 7 * * *', maven: true, sonar: true, skipcigame: true,
-          email: 'kiwiwings@apache.org',
-          // H40 can't access sonar instance
-          slaveAdd: '&&!H40'
+          email: 'kiwiwings@apache.org'
         ],
         [ name: 'POI-DSL-SonarQube-Gradle', trigger: 'H 9 * * *', gradle: true, sonar: true, skipcigame: true,
                 disabled: true // this one does run, but does not actually send data to Sonarqube for some reason, we need to investigate some more
@@ -114,6 +114,7 @@ def jdkMapping = [
         '1.12': 'JDK 12 (latest)',
         '1.13': 'JDK 13 (latest)',
         '1.14': 'JDK 14 (latest)',
+        '1.15': 'JDK 15 (latest)',
         'OpenJDK 1.8': 'OpenJDK 1.8.0_242',
         'IBMJDK': 'IBM 1.8 64-bit (on Ubuntu only)',
 ]
@@ -498,7 +499,7 @@ xmlbeansjobs.each { xjob ->
                 // when using JDK 9/10 for running Ant, we need to provide more modules for the forbidden-api-checks task
                 // on JDK 11 and newer there is no such module any more, so do not add it here
                 env('ANT_OPTS', '--add-modules=java.xml.bind --add-opens=java.xml/com.sun.org.apache.xerces.internal.util=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED')
-            } else if (jdkKey == '1.11' || jdkKey == '1.12' || jdkKey == '1.13' || jdkKey == '1.14') {
+            } else if (jdkKey == '1.11' || jdkKey == '1.12' || jdkKey == '1.13' || jdkKey == '1.14' || jdkKey == '1.15') {
                 env('ANT_OPTS', '--add-opens=java.xml/com.sun.org.apache.xerces.internal.util=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED')
             }
             // will be needed for forbidden-apis-check: env('ANT_HOME', xjob.windows ? 'f:\\jenkins\\tools\\ant\\latest' : '/usr/share/ant')
@@ -592,7 +593,8 @@ Unfortunately we often see builds break because of changes/new machines...''')
                 'JDK 11 (latest)',
                 'JDK 12 (latest)',
                 'JDK 13 (latest)',
-                'JDK 14 (latest)'
+                'JDK 14 (latest)',
+                'JDK 15 (latest)'
         )
         elasticAxis {
             name('Nodes')
