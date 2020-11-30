197cdf6bc7aae5f753abe0ac733d407fa144d8e8
Marius Volkhart <marius@volkhart.com>
2020-11-30 20:45:46 +0100

    Update OPCPackage#getPartsByRelationshipType docs to reflect behavior
    The function never returns null.

--- src/ooxml/java/org/apache/poi/openxml4j/opc/OPCPackage.java
+++ src/ooxml/java/org/apache/poi/openxml4j/opc/OPCPackage.java
@@ -646,13 +646,13 @@ public abstract class OPCPackage implements RelationshipSource, Closeable {
 	 * Retrieve parts by relationship type.
 	 *
 	 * @param relationshipType
-	 *            Relationship type.
+	 *            Relationship type. Must not be {@code null}.
 	 * @return All parts which are the target of a relationship with the
-	 *         specified type, if the method can't retrieve relationships from
-	 *         the package, then return <code>null</code>.
+	 *         specified type. If no such parts are found, the list is empty.
+	 * @throws InvalidOperationException If called on a write-only package.
 	 */
 	public ArrayList<PackagePart> getPartsByRelationshipType(
-			String relationshipType) {
+			String relationshipType) throws InvalidOperationException {
 		if (relationshipType == null) {
 			throw new IllegalArgumentException("relationshipType");
 		}
