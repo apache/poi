793e8b6f6a530870da2d0b62c8f81facd407846a
Andreas Beeker <kiwiwings@apache.org>
2020-12-25 19:19:32 +0000

    try to fix gradle sonarqube job
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1884803 13f79535-47bb-0310-9956-ffa450edef68

--- jenkins/create_jobs.groovy
+++ jenkins/create_jobs.groovy
@@ -401,6 +401,7 @@ poijobs.each { poijob ->
                     switches('-Dsonar.login=${POI_SONAR_TOKEN}')
                     switches('-Dsonar.organization=apache')
                     switches('-Dsonar.projectKey=poi-parent')
+                    switches('-Dsonar.host.url=https://sonarcloud.io')
                     tasks('check')
                     tasks('jacocoTestReport')
                     tasks('sonarqube')
