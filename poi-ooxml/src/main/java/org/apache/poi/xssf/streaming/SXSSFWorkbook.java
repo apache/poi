f05ae0c2c595cf3e039a95d476663007823beb1d
PJ Fanning <fanningpj@apache.org>
2021-10-04 21:05:09 +0000

    [bug-65613] experimental SXSSF writeAvoidingTempFiles
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1893898 13f79535-47bb-0310-9956-ffa450edef68

--- poi-ooxml/src/main/java/org/apache/poi/xssf/streaming/SXSSFWorkbook.java
+++ poi-ooxml/src/main/java/org/apache/poi/xssf/streaming/SXSSFWorkbook.java
@@ -17,7 +17,13 @@
 
 package org.apache.poi.xssf.streaming;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.nio.charset.StandardCharsets;
 import java.util.Enumeration;
 import java.util.HashMap;
