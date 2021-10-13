e64eb4867089ca91b70115eb5d2b680e7fa9d06b
PJ Fanning <fanningpj@apache.org>
2021-10-13 22:34:35 +0000

    add tests
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1894213 13f79535-47bb-0310-9956-ffa450edef68

---
+++ poi-ooxml/src/test/java/org/apache/poi/openxml4j/opc/TestEncryptedTempFilePackagePart.java
@@ -0,0 +1,33 @@
+package org.apache.poi.openxml4j.opc;
+
+import org.apache.poi.openxml4j.OpenXML4JTestDataSamples;
+import org.apache.poi.openxml4j.opc.internal.EncryptedTempFilePackagePart;
+import org.apache.poi.util.IOUtils;
+import org.junit.jupiter.api.Test;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestEncryptedTempFilePackagePart {
+    @Test
+    void testRoundTrip() throws Exception {
+        String text = UUID.randomUUID().toString();
+        String filepath =  OpenXML4JTestDataSamples.getSampleFileName("sample.docx");
+
+        try (OPCPackage p = OPCPackage.open(filepath, PackageAccess.READ)) {
+            PackagePartName name = new PackagePartName("/test.txt", true);
+            EncryptedTempFilePackagePart part = new EncryptedTempFilePackagePart(p, name, "text/plain");
+            try (OutputStream os = part.getOutputStream()) {
+                os.write(text.getBytes(StandardCharsets.UTF_8));
+            }
+            assertEquals(-1, part.getSize());
+            try (InputStream is = part.getInputStream()) {
+                assertEquals(text, new String(IOUtils.toByteArray(is), StandardCharsets.UTF_8));
+            }
+        }
+    }
+}
