ba1a9b5d5939b61d9bb4a1bb7ca44959724b11ee
aspojo <logbug@aliyun.com>
2021-10-26 17:10:07 +0800

    Update BaseTestFormulaEvaluator.java

--- poi/src/test/java/org/apache/poi/ss/usermodel/BaseTestFormulaEvaluator.java
+++ poi/src/test/java/org/apache/poi/ss/usermodel/BaseTestFormulaEvaluator.java
@@ -24,8 +24,6 @@ import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Locale;
 
