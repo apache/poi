34ea6a50e02aceaf4075d4a46fba9145a6f3625c
Dominik Stadler <centic@apache.org>
2021-11-02 13:18:50 +0000

    Check and fix issue when sheets are added while an existing workbook-evaluator is used.
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1894680 13f79535-47bb-0310-9956-ffa450edef68

--- poi-ooxml/src/main/java/org/apache/poi/xssf/usermodel/XSSFEvaluationWorkbook.java
+++ poi-ooxml/src/main/java/org/apache/poi/xssf/usermodel/XSSFEvaluationWorkbook.java
@@ -17,6 +17,9 @@
 
 package org.apache.poi.xssf.usermodel;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.poi.ss.formula.EvaluationCell;
 import org.apache.poi.ss.formula.EvaluationSheet;
 import org.apache.poi.ss.formula.FormulaParser;
@@ -29,8 +32,8 @@ import org.apache.poi.util.Internal;
  */
 @Internal
 public final class XSSFEvaluationWorkbook extends BaseXSSFEvaluationWorkbook {
-    private XSSFEvaluationSheet[] _sheetCache;
-    
+    private final Map<XSSFSheet, XSSFEvaluationSheet> _sheetCache = new HashMap<>();
+
     public static XSSFEvaluationWorkbook create(XSSFWorkbook book) {
         if (book == null) {
             return null;
@@ -48,9 +51,9 @@ public final class XSSFEvaluationWorkbook extends BaseXSSFEvaluationWorkbook {
     @Override
     public void clearAllCachedResultValues() {
         super.clearAllCachedResultValues();
-        _sheetCache = null;
+        _sheetCache.clear();
     }
-    
+
     @Override
     public int getSheetIndex(EvaluationSheet evalSheet) {
         XSSFSheet sheet = ((XSSFEvaluationSheet)evalSheet).getXSSFSheet();
@@ -59,25 +62,22 @@ public final class XSSFEvaluationWorkbook extends BaseXSSFEvaluationWorkbook {
 
     @Override
     public EvaluationSheet getSheet(int sheetIndex) {
-        // Performance optimization: build sheet cache the first time this is called
-        // to avoid re-creating the XSSFEvaluationSheet each time a new cell is evaluated
+		// verify index and let the method in _uBook throw the exception so we report
+		// it the same way as in other places
+		if (sheetIndex < 0 || sheetIndex >= _uBook.getNumberOfSheets()) {
+			// this will throw an exception now as the index is out of bounds
+			_uBook.getSheetAt(sheetIndex);
+		}
+
+        // Performance optimization: build sheet cache for each sheet to avoid re-creating
+		// the XSSFEvaluationSheet each time a new cell is evaluated
         // EvaluationWorkbooks make not guarantee to synchronize changes made to
         // the underlying workbook after the EvaluationWorkbook is created.
-        if (_sheetCache == null) {
-            final int numberOfSheets = _uBook.getNumberOfSheets();
-            _sheetCache = new XSSFEvaluationSheet[numberOfSheets];
-            for (int i=0; i < numberOfSheets; i++) {
-                _sheetCache[i] = new XSSFEvaluationSheet(_uBook.getSheetAt(i));
-            }
-        }
-        if (sheetIndex < 0 || sheetIndex >= _sheetCache.length) {
-            // do this to reuse the out-of-bounds logic and message from XSSFWorkbook
-            _uBook.getSheetAt(sheetIndex);
-        }
-        return _sheetCache[sheetIndex];
+		final XSSFSheet sheet = _uBook.getSheetAt(sheetIndex);
+        return _sheetCache.computeIfAbsent(sheet, rows -> new XSSFEvaluationSheet(sheet));
     }
 
-    @Override    
+    @Override
     public Ptg[] getFormulaTokens(EvaluationCell evalCell) {
         final XSSFCell cell = ((XSSFEvaluationCell)evalCell).getXSSFCell();
         final int sheetIndex = _uBook.getSheetIndex(cell.getSheet());
