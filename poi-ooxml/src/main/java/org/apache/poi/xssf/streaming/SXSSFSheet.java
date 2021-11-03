a46cf63f4f446371a6d499ff3d5397230c75a518
PJ Fanning <fanningpj@apache.org>
2021-11-03 14:00:10 +0000

    javadoc
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1894714 13f79535-47bb-0310-9956-ffa450edef68

--- poi-ooxml/src/main/java/org/apache/poi/xssf/streaming/SXSSFSheet.java
+++ poi-ooxml/src/main/java/org/apache/poi/xssf/streaming/SXSSFSheet.java
@@ -1051,7 +1051,14 @@ public class SXSSFSheet implements Sheet
 
     /**
      * Sets a page break at the indicated row
-     * @param row FIXME: Document this!
+     * Breaks occur above the specified row and left of the specified column inclusive.
+     *
+     * For example, {@code sheet.setColumnBreak(2);} breaks the sheet into two parts
+     * with columns A,B,C in the first and D,E,... in the second. Simuilar, {@code sheet.setRowBreak(2);}
+     * breaks the sheet into two parts with first three rows (rownum=1...3) in the first part
+     * and rows starting with rownum=4 in the second.
+     *
+     * @param row the row to break, inclusive
      */
     @Override
     public void setRowBreak(int row) {
