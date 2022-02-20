b7c2cabf3f222d129689403a41e64eedf043863c
PJ Fanning <fanningpj@apache.org>
2022-02-20 11:27:17 +0000

    change error message to suggest large files will need overrides
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1898250 13f79535-47bb-0310-9956-ffa450edef68

--- poi/src/main/java/org/apache/poi/util/IOUtils.java
+++ poi/src/main/java/org/apache/poi/util/IOUtils.java
@@ -554,7 +554,7 @@ public final class IOUtils {
     private static void throwRFE(long length, int maxLength) {
         throw new RecordFormatException(String.format(Locale.ROOT, "Tried to allocate an array of length %,d" +
                         ", but the maximum length for this record type is %,d.\n" +
-                        "If the file is not corrupt, please open an issue on bugzilla to request \n" +
+                        "If the file is not corrupt or large, please open an issue on bugzilla to request \n" +
                         "increasing the maximum allowable size for this record type.\n"+
                         "As a temporary workaround, consider setting a higher override value with " +
                         "IOUtils.setByteArrayMaxOverride()",
