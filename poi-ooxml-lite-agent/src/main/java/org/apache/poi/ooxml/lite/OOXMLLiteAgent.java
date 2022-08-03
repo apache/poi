554f7298707cd5f2ae0aef8a4c9e215bcd3616ff
PJ Fanning <fanningpj@apache.org>
2022-08-03 10:21:13 +0000

    remove unused imports
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1903208 13f79535-47bb-0310-9956-ffa450edef68

--- poi-ooxml-lite-agent/src/main/java/org/apache/poi/ooxml/lite/OOXMLLiteAgent.java
+++ poi-ooxml-lite-agent/src/main/java/org/apache/poi/ooxml/lite/OOXMLLiteAgent.java
@@ -34,12 +34,9 @@ import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 import net.bytebuddy.agent.builder.AgentBuilder;
-import net.bytebuddy.description.type.TypeDescription;
-import net.bytebuddy.dynamic.DynamicType;
 import net.bytebuddy.implementation.MethodDelegation;
 import net.bytebuddy.implementation.SuperMethodCall;
 import net.bytebuddy.matcher.ElementMatchers;
-import net.bytebuddy.utility.JavaModule;
 import org.apache.xmlbeans.impl.schema.SchemaTypeSystemImpl;
 
 /**
