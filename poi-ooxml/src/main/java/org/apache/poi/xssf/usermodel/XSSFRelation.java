2712abf00ba8b99ec4b445ec619ceb3abb8bd8d3
PJ Fanning <fanningpj@apache.org>
2023-05-22 15:17:22 +0000

    add content types for macrosheets
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1909993 13f79535-47bb-0310-9956-ffa450edef68

--- poi-ooxml/src/main/java/org/apache/poi/xssf/usermodel/XSSFRelation.java
+++ poi-ooxml/src/main/java/org/apache/poi/xssf/usermodel/XSSFRelation.java
@@ -286,13 +286,13 @@ public final class XSSFRelation extends POIXMLRelation {
     );
 
     public static final XSSFRelation MACRO_SHEET_BIN = new XSSFRelation(
-        null,//TODO: figure out what this should be?
+        "application/vnd.ms-excel.macrosheet+xml",
         "http://schemas.microsoft.com/office/2006/relationships/xlMacrosheet",
         "/xl/macroSheets/sheet#.bin"
     );
 
     public static final XSSFRelation INTL_MACRO_SHEET_BIN = new XSSFRelation(
-        null,//TODO: figure out what this should be?
+        "application/vnd.ms-excel.intlmacrosheet+xml",
         "http://schemas.microsoft.com/office/2006/relationships/xlIntlMacrosheet",
         "/xl/macroSheets/sheet#.bin"
     );
