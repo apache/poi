a605449f9e7623c89e5d04fcfd9786431f8e8652
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-06-23 09:22:45 -0400

    spelling: escapement
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- poi-scratchpad/src/main/java/org/apache/poi/hwmf/record/HwmfFont.java
+++ poi-scratchpad/src/main/java/org/apache/poi/hwmf/record/HwmfFont.java
@@ -534,7 +534,7 @@ public class HwmfFont implements FontInfo, GenericRecord {
         final Map<String,Supplier<?>> m = new LinkedHashMap<>();
         m.put("height", this::getHeight);
         m.put("width", this::getWidth);
-        m.put("escapment", this::getEscapement);
+        m.put("escapement", this::getEscapement);
         m.put("weight", this::getWeight);
         m.put("italic", this::isItalic);
         m.put("underline", this::isUnderline);
