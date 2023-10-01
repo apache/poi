bbc98b5cd3d141cd0e05e6e820d10d416d428543
PJ Fanning <fanningpj@apache.org>
2023-10-01 01:34:13 +0000

    commons-io 2.14.0
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1912638 13f79535-47bb-0310-9956-ffa450edef68

--- poi-scratchpad/src/test/java9/module-info.java
+++ poi-scratchpad/src/test/java9/module-info.java
@@ -19,6 +19,7 @@ module org.apache.poi.scratchpad {
     requires transitive org.apache.poi.poi;
     requires java.desktop;
     requires commons.math3;
+    requires org.apache.commons.io;
     requires org.apache.logging.log4j;
 
     uses org.apache.poi.sl.usermodel.MetroShapeProvider;
