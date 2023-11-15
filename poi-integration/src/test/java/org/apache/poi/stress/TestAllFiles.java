676d0fdf3f1ab0ce900a4603138a1ec0d4328427
Dominik Stadler <centic@apache.org>
2023-11-15 10:32:59 +0000

    Bug 68128: Exclude one broken file from the "No-Scratchpad" run
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1913797 13f79535-47bb-0310-9956-ffa450edef68

--- poi-integration/src/test/java/org/apache/poi/stress/TestAllFiles.java
+++ poi-integration/src/test/java/org/apache/poi/stress/TestAllFiles.java
@@ -132,6 +132,7 @@ public class TestAllFiles {
         "diagram/clusterfuzz-testcase-minimized-POIHDGFFuzzer-5947849161179136.vsd",
         "spreadsheet/clusterfuzz-testcase-minimized-POIHSSFFuzzer-5436547081830400.xls",
         "spreadsheet/clusterfuzz-testcase-minimized-POIHSSFFuzzer-4819588401201152.xls",
+        "diagram/clusterfuzz-testcase-minimized-POIVisioFuzzer-4537225637134336.vsd",
 
         // exclude files failing on windows nodes, because of limited JCE policies
         "document/bug53475-password-is-pass.docx",
