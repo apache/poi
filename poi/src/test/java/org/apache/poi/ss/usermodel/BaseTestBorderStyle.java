7e86ff1bb03123b90c9b3f40c8c5ab3355980b13
PJ Fanning <fanningpj@apache.org>
2024-07-01 23:06:36 +0000

    use standard ASF header
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1918804 13f79535-47bb-0310-9956-ffa450edef68

--- poi/src/test/java/org/apache/poi/ss/usermodel/BaseTestBorderStyle.java
+++ poi/src/test/java/org/apache/poi/ss/usermodel/BaseTestBorderStyle.java
@@ -13,10 +13,6 @@
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
    limitations under the License.
-
-   2012 - Alfresco Software, Ltd.
-   Alfresco Software has modified source of this file
-   The details of changes as svn diff can be found in svn at location root/projects/3rd-party/src
 ==================================================================== */
 
 package org.apache.poi.ss.usermodel;
