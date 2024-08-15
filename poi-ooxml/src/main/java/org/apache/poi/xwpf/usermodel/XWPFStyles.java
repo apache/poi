c2945d86f92f668a632acbf20f834e74e2990753
PJ Fanning <fanningpj@apache.org>
2024-08-15 19:45:40 +0000

    [github-672] Support removing XWPF Styles. Thanks to fangd1997. This closes #672
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1919918 13f79535-47bb-0310-9956-ffa450edef68

--- poi-ooxml/src/main/java/org/apache/poi/xwpf/usermodel/XWPFStyles.java
+++ poi-ooxml/src/main/java/org/apache/poi/xwpf/usermodel/XWPFStyles.java
@@ -23,6 +23,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import javax.xml.namespace.QName;
@@ -145,6 +146,41 @@ public class XWPFStyles extends POIXMLDocumentPart {
                         docDefaults.getPPrDefault().getPPr());
             }
         }
+    }
+
+    /**
+     * Gets the underlying CTStyles object for the Styles.
+     *
+     * @return CTStyles object
+     * @since POI 5.3.1
+     */
+    public CTStyles getCtStyles() {
+        return ctStyles;
+    }
+
+    /**
+     * Get the list of {@link XWPFStyle} in the Styles part.
+     *
+     * @since POI 5.3.1
+     */
+    public List<XWPFStyle> getStyles() {
+        return Collections.unmodifiableList(listStyle);
+    }
+
+    /**
+     * Remove the specified style if present.
+     *
+     * @param pos Array position of the style to be removed
+     * @return True if the style was removed.
+     * @since POI 5.3.1
+     */
+    public boolean removeStyle(int pos) {
+        if (pos >= 0 && pos < getNumberOfStyles()) {
+            listStyle.remove(pos);
+            ctStyles.removeStyle(pos);
+            return true;
+        }
+        return false;
     }
 
     /**
