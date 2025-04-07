c6eb5b7b858786e143a9772c99fffd2a34f30d60
Dominik Stadler <centic@apache.org>
2025-04-07 05:45:39 +0000

    Exclude two more files when running tests with "no-scratchpad"
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1924886 13f79535-47bb-0310-9956-ffa450edef68

--- poi-integration/src/test/java/org/apache/poi/stress/TestAllFiles.java
+++ poi-integration/src/test/java/org/apache/poi/stress/TestAllFiles.java
@@ -136,7 +136,9 @@ public class TestAllFiles {
         "spreadsheet/clusterfuzz-testcase-minimized-POIHSSFFuzzer-4977868385681408.xls",
         "spreadsheet/clusterfuzz-testcase-minimized-POIHSSFFuzzer-4651309315719168.xls",
         "document/clusterfuzz-testcase-POIHWPFFuzzer-5696094627495936.doc",
-        "spreadsheet/clusterfuzz-testcase-minimized-POIHSSFFuzzer-4657005060816896.xls"
+        "spreadsheet/clusterfuzz-testcase-minimized-POIHSSFFuzzer-4657005060816896.xls",
+        "diagram/clusterfuzz-testcase-minimized-POIHDGFFuzzer-4913778037489664.vsd",
+        "diagram/clusterfuzz-testcase-minimized-POIHDGFFuzzer-6478389109981184.vsd"
     });
 
     private static final Set<String> EXPECTED_FAILURES = StressTestUtils.unmodifiableHashSet(
