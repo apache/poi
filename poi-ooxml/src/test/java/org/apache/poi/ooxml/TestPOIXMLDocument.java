7e3a404e4de1dcca8c002ee8d7b79403d1d97955
PJ Fanning <fanningpj@apache.org>
2025-04-09 21:31:39 +0000

    tidy up
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1924980 13f79535-47bb-0310-9956-ffa450edef68

--- poi-ooxml/src/test/java/org/apache/poi/ooxml/TestPOIXMLDocument.java
+++ poi-ooxml/src/test/java/org/apache/poi/ooxml/TestPOIXMLDocument.java
@@ -24,7 +24,6 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.ByteArrayInputStream;
 import java.io.File;
