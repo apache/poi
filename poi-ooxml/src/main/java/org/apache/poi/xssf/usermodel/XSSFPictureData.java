f2179098fef96da6569ffd56b00c19c7b4e86944
PJ Fanning <fanningpj@apache.org>
2025-04-10 13:51:20 +0000

    [bug-69628] more changes to getMax methods
    git-svn-id: https://svn.apache.org/repos/asf/poi/trunk@1924993 13f79535-47bb-0310-9956-ffa450edef68

--- poi-ooxml/src/main/java/org/apache/poi/xssf/usermodel/XSSFPictureData.java
+++ poi-ooxml/src/main/java/org/apache/poi/xssf/usermodel/XSSFPictureData.java
@@ -48,7 +48,8 @@ public class XSSFPictureData extends POIXMLDocumentPart implements PictureData {
      * @return the max image size allowed for XSSF pictures
      */
     public static int getMaxImageSize() {
-        return MAX_IMAGE_SIZE;
+        final int ioMaxSize = IOUtils.getByteArrayMaxOverride();
+        return ioMaxSize < 0 ? MAX_IMAGE_SIZE : Math.min(MAX_IMAGE_SIZE, ioMaxSize);
     }
 
     /**
