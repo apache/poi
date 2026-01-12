b09bf904d776924e6ac4784c42ee79b7344a1adb
Dominik Stadler <dominik.stadler@gmx.at>
2026-01-12 08:18:02 +0100

    Fix running integration-tests without scratchpad-libraries

--- poi-integration/src/test/java/org/apache/poi/stress/TestAllFiles.java
+++ poi-integration/src/test/java/org/apache/poi/stress/TestAllFiles.java
@@ -144,7 +144,9 @@ public class TestAllFiles {
         "document/clusterfuzz-testcase-POIHWPFFuzzer-5696094627495936.doc",
         "spreadsheet/clusterfuzz-testcase-minimized-POIHSSFFuzzer-4657005060816896.xls",
         "diagram/clusterfuzz-testcase-minimized-POIHDGFFuzzer-4913778037489664.vsd",
-        "diagram/clusterfuzz-testcase-minimized-POIHDGFFuzzer-6478389109981184.vsd"
+        "diagram/clusterfuzz-testcase-minimized-POIHDGFFuzzer-6478389109981184.vsd",
+        "publisher/clusterfuzz-testcase-minimized-POIHPBFFuzzer-4918886059278336.pub",
+        "publisher/clusterfuzz-testcase-minimized-POIHPBFFuzzer-4918886059278336.pub"
     });
 
     private static final Set<String> EXPECTED_FAILURES = StressTestUtils.unmodifiableHashSet(
