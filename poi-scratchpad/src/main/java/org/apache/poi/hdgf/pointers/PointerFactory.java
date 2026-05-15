bc6aa263e05620ae537d6ddac8399f771b474eb7
metsw24-max <metsw24@gmail.com>
2026-05-15 20:56:25 +0530

    Guard HDGF PointerFactory offset/length uint32 narrowing (#1076)
    * Guard HDGF PointerFactory offset/length uint32 narrowing
    
    * updated
    
    * updated

--- poi-scratchpad/src/main/java/org/apache/poi/hdgf/pointers/PointerFactory.java
+++ poi-scratchpad/src/main/java/org/apache/poi/hdgf/pointers/PointerFactory.java
@@ -20,6 +20,7 @@ import org.apache.poi.hdgf.exceptions.OldVisioFormatException;
 import org.apache.poi.hdgf.streams.PointerContainingStream;
 import org.apache.poi.util.IOUtils;
 import org.apache.poi.util.LittleEndian;
+import org.apache.poi.util.RecordFormatException;
 
 /**
  * Factor class to create the appropriate pointers, based on the version
@@ -45,8 +46,17 @@ public final class PointerFactory {
             p = new PointerV6();
             p.setType(LittleEndian.getInt(data, offset));
             p.setAddress((int)LittleEndian.getUInt(data, offset+4));
-            p.setOffset((int)LittleEndian.getUInt(data, offset+8));
-            p.setLength((int)LittleEndian.getUInt(data, offset+12));
+            // Offset and Length flow into Stream.createStream as the (offset, length)
+            // pair handed to StreamStore / CompressedStreamStore -> IOUtils.safelyClone.
+            // Validate the uint32 values up-front: throw RecordFormatException with the
+            // offending value rather than letting a wrapped int reach the downstream
+            // bounds check (matches the EMF header description fix in PR #1060).
+            final long v6Offset = LittleEndian.getUInt(data, offset+8);
+            final long v6Length = LittleEndian.getUInt(data, offset+12);
+            checkPointerOffset(v6Offset);
+            IOUtils.safelyAllocateCheck(v6Length, Integer.MAX_VALUE);
+            p.setOffset((int)v6Offset);
+            p.setLength((int)v6Length);
             p.setFormat(LittleEndian.getShort(data, offset+16));
 
             return p;
@@ -55,12 +65,23 @@ public final class PointerFactory {
             p.setType(LittleEndian.getShort(data, offset));
             p.setFormat(LittleEndian.getShort(data, offset+2));
             p.setAddress((int)LittleEndian.getUInt(data, offset+4));
-            p.setOffset((int)LittleEndian.getUInt(data, offset+8));
-            p.setLength((int)LittleEndian.getUInt(data, offset+12));
+            final long v5Offset = LittleEndian.getUInt(data, offset+8);
+            final long v5Length = LittleEndian.getUInt(data, offset+12);
+            checkPointerOffset(v5Offset);
+            IOUtils.safelyAllocateCheck(v5Length, Integer.MAX_VALUE);
+            p.setOffset((int)v5Offset);
+            p.setLength((int)v5Length);
 
             return p;
         } else {
             throw new OldVisioFormatException("Visio files with versions below 5 are not supported, yours was " + version);
+        }
+    }
+
+    private static void checkPointerOffset(long unsignedOffset) {
+        if (unsignedOffset > Integer.MAX_VALUE) {
+            throw new RecordFormatException(
+                    "HDGF Pointer offset " + unsignedOffset + " exceeds Integer.MAX_VALUE");
         }
     }
 
